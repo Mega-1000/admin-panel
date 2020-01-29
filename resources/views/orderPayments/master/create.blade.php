e53ef8e9973e11b380d5bdeee4d162905a012dea
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-01-29 13:53:26 +0100

    Change variable from $id to $order->id in breadcrumbs of payment create view

--- resources/views/orderPayments/master/create.blade.php
+++ resources/views/orderPayments/master/create.blade.php
@@ -2,7 +2,7 @@
 @section('app-header')
     <h1 class="page-title">
         <i class="voyager-tag"></i> @lang('order_payments.create')
-        <a style="margin-left: 15px;" href="{{ action('OrdersController@edit', ["id" => $id]) }}"
+        <a style="margin-left: 15px;" href="{{ action('OrdersController@edit', ["id" => $order->id]) }}"
            class="btn btn-info install pull-right">
             <span>@lang('order_payments.list')</span>
         </a>
