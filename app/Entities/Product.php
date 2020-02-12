f81abdaaf7b8827400211e8217eaadedd994706c
Karol Krupinski <karolkk01@gmail.com>
2020-02-12 13:36:48 +0100

    usunięto nadmiarowe pola obiektu

--- app/Entities/Product.php
+++ app/Entities/Product.php
@@ -81,16 +81,6 @@ class Product extends Model implements Transformable
         'comments',
         'value_of_the_order_for_free_transport',
         'show_on_page',
-        'gross_selling_price_basic_unit',
-        'gross_purchase_price_basic_unit_after_discounts',
-        'gross_selling_price_commercial_unit',
-        'gross_purchase_price_commercial_unit_after_discounts',
-        'gross_selling_price_calculated_unit',
-        'gross_purchase_price_calculated_unit_after_discounts',
-        'gross_selling_price_aggregate_unit',
-        'gross_purchase_price_aggregate_unit_after_discounts',
-        'gross_selling_price_the_largest_unit',
-        'gross_purchase_price_the_largest_unit_after_discounts'
     ];
 
     public $customColumnsVisibilities = [
