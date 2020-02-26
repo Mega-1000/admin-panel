9e288a3a007342fcfffb0c4117019eec46831158
Karol Krupinski <karolkk01@gmail.com>
2020-02-26 15:18:31 +0100

    poprawa faktur leży poza tym scopem

--- config/payments-import.php
+++ config/payments-import.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'pdf-application-path' => '/home/mega1000/domains/'.env('APP_URL') . '/public_html/pdf_to_text/app.js',
-    'pdf-file-path' => '/home/mega1000/domains/' . env('APP_URL') . '/public_html/storage/app/',
-    'output-file-path' => '/home/mega1000/domains/'. env('APP_URL') . '/public_html/storage/app/',
+    'pdf-application-path' => '/home/mega1000/domains/mega1000.pro-linuxpl.com/public_html/pdf_to_text/app.js',
+    'pdf-file-path' => '/home/mega1000/domains/mega1000.pro-linuxpl.com/public_html/storage/app/',
+    'output-file-path' => '/home/mega1000/domains/mega1000.pro-linuxpl.com/public_html/storage/app/',
 ];
