0379478f361bd22bf4239a467f1168bc524cfdcf
DawidZwiewka <DawidZwiewka@github.com>
2020-03-11 14:45:35 +0100

    aktualizacja powiązanych produktów

--- app/Jobs/CheckDateOfProductNewPriceJob.php
+++ app/Jobs/CheckDateOfProductNewPriceJob.php
@@ -149,7 +149,14 @@ class CheckDateOfProductNewPriceJob
             $price['gross_selling_price_the_largest_unit'] = number_format($price['net_purchase_price_the_largest_unit_after_discounts'] * ((100 + $product->price->coating) / 100) * 1.23,
                 2, '.', '');
 
-            $this->productPriceRepository->update($price, $product->price->id);
+            $productsRelated = \App\Entities\Product::where('products_related_to_the_automatic_price_change', $product->symbol)->get();
+
+            $ids = [$product->id];
+            foreach ($productsRelated as $productRelated) {
+                $ids[] = $productRelated->id;
+            }
+            
+            \App\Entities\ProductPrice::whereIn('product_id', $ids)->update($price);
         }
     }
 
