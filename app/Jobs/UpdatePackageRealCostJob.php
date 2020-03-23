42d455cc80eb140a18a16941a5a6075abe821e01
bogdanjaczewski <bogdanjaczewski@github.com>
2020-03-23 11:24:43 +0100

    deleting space

--- app/Jobs/UpdatePackageRealCostJob.php
+++ app/Jobs/UpdatePackageRealCostJob.php
@@ -40,7 +40,6 @@ class UpdatePackageRealCostJob implements ShouldQueue {
         if (!empty($pocztexPackages)) {
             $spec = fopen('specyfikacjaPocztex.csv', 'r');
             while (($csvLine = fgetcsv($spec, 1000, ",")) !== FALSE) {
-
                 foreach ($pocztexPackages as $pocztexPackage) {
                     if ($pocztexPackage->letter_number == $csvLine[0]) {
                         $pocztexPackage->real_cost_for_company = $csvLine[17];
