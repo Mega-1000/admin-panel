5667490947fb50e125be75229e5ac98000312a21
bogdanjaczewski <bogdanjaczewski@github.com>
2020-03-23 11:20:59 +0100

    dissecting real cost update into 2 functions

--- app/Jobs/UpdatePackageRealCostJob.php
+++ app/Jobs/UpdatePackageRealCostJob.php
@@ -18,56 +18,71 @@ class UpdatePackageRealCostJob implements ShouldQueue {
     }
 
     public function handle() {
-        $inpostPackages = array();
-        $pocztexPackages = array();
-        $dpdPackages = array();
-        foreach ($this->orderPackageRepository as $package) {
-            if ($package->service_courier_name == 'INPOST') {
-                $inpostPackages[] = $package;
-            } 
-            if ($package->service_courier_name == 'POCZTEX') {
-                $pocztexPackages[] = $package;
-            } 
-            if ($package->service_courier_name == 'DPD') {
-                $dpdPackages[] = $package;
-            } 
-        }
+        $sortedPackages = $this->sortPackages();
+        $inpostPackages = $sortedPackages['INPOST'];
+        $pocztexPackages = $sortedPackages['POCZTEX'];
+        $dpdPackages = $sortedPackages['DPD'];
+        $this->updateCost($inpostPackages, $pocztexPackages, $dpdPackages);
+    }
+
+    public function updateCost($inpostPackages, $pocztexPackages, $dpdPackages) {
         if (!empty($inpostPackages)) {
             $spec = fopen('specyfikacjaInpost.csv', 'r');
             while (($csvLine = fgetcsv($spec, 1000, ";")) !== FALSE) {
                 foreach ($inpostPackages as $inpostPackage) {
-                    if ($inpostPackage->letter_number == $csvLine[2] ){
+                    if ($inpostPackage->letter_number == $csvLine[2]) {
                         $inpostPackage->real_cost_for_company = $csvLine[7];
                         $inpostPackage->save();
                     }
                 }
-            }   
+            }
         }
         if (!empty($pocztexPackages)) {
-            $spec = fopen('specyfikacjaPocztex.csv','r');
+            $spec = fopen('specyfikacjaPocztex.csv', 'r');
             while (($csvLine = fgetcsv($spec, 1000, ",")) !== FALSE) {
-                
+
                 foreach ($pocztexPackages as $pocztexPackage) {
-                    if ($pocztexPackage->letter_number == $csvLine[0] ){
+                    if ($pocztexPackage->letter_number == $csvLine[0]) {
                         $pocztexPackage->real_cost_for_company = $csvLine[17];
                         $pocztexPackage->save();
                     }
                 }
-            }   
+            }
         }
         if (!empty($dpdPackages)) {
-            $spec = fopen('specyfikacjaDpd.csv','r');
+            $spec = fopen('specyfikacjaDpd.csv', 'r');
             while (($csvLine = fgetcsv($spec, 1000, ",")) !== FALSE) {
                 foreach ($dpdPackages as $dpdPackage) {
-                    if ($dpdPackage->letter_number == $csvLine[11] ){
+                    if ($dpdPackage->letter_number == $csvLine[11]) {
                         $dpdPackage->real_cost_for_company = $csvLine[14];
                         $dpdPackage->save();
                     }
                 }
-            }   
+            }
         }
-        
     }
 
+    public function sortPackages() {
+        $inpostPackages = array();
+        $pocztexPackages = array();
+        $dpdPackages = array();
+        foreach ($this->orderPackageRepository as $package) {
+            switch ($package->service_courier_name) {
+                case "INPOST":
+                    $inpostPackages[] = $package;
+                    break;
+                case "POCZTEX":
+                    $pocztexPackages[] = $package;
+                    break;
+                case "DPD":
+                    $dpdPackages[] = $package;
+                    break;
+            }
+        }
+        $sortedPackages['INPOST'] = $inpostPackages;
+        $sortedPackages['POCZTEX'] = $pocztexPackages;
+        $sortedPackages['DPD'] = $dpdPackages;
+        return $sortedPackages;
+    }
 
 }
