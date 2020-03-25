fc797a7a0d9baec4a0de36abd861b090f8adec09
Karol Krupinski <karolkk01@gmail.com>
2020-03-25 11:32:07 +0100

    zmieniono sposob przeliczania kosztow paczki

--- app/Helpers/OrderPriceOverrider.php
+++ app/Helpers/OrderPriceOverrider.php
@@ -22,6 +22,13 @@ class OrderPriceOverrider implements iOrderPriceOverrider
             return $orderItem;
         }
         $prices = $this->overrides[$orderItem->product_id];
+        foreach ($prices as $k => $price) {
+            if (strpos($k, 'gross') !== false) {
+                $tax = 1 + env('VAT');
+                $newKey = str_replace('gross', 'net', $k);
+                $prices[$newKey] = $price / $tax;
+            }
+        }
         foreach (OrderBuilder::getPriceColumns() as $column) {
             if (isset($prices[$column])) {
                 $orderItem->$column = $prices[$column];
