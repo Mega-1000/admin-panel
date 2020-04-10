f50d7c042ebc0607775f1a3ef4de713be7de2c6c
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-04-10 19:25:01 +0200

    [Fix] Warehouse stocks change when specific label appears

--- app/Jobs/WarehouseStocks/ChangeWarehouseStockJob.php
+++ app/Jobs/WarehouseStocks/ChangeWarehouseStockJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs\Orders;
 
+use App\Entities\Product;
 use App\Jobs\DispatchLabelEventByNameJob;
 use App\Jobs\Job;
 use App\Repositories\OrderRepository;
@@ -40,12 +41,16 @@ class ChangeWarehouseStockJob extends Job
         foreach($order->items as $item) {
             $productName = $item->product->symbol;
             preg_match('/([^-]+)/', $productName, $matches);
-            $product = $productRepository->findWhere(['symbol' => $matches[1]])->first();
-            $productStock = $productStockRepository->findWhere(['product_id' => $product->id])->first();
+            $product = Product::withTrashed()->where('symbol', $matches[1])->first();
+            if($product !== null) {
+                $productStock = $productStockRepository->findWhere(['product_id' => $product->id])->first();
+            } else {
+                $productStock = $productStockRepository->findWhere(['product_id' => $item->product->id])->first();
+            }
 
             $productStockPosition = $productStockPositionRepository->findWhere(['product_stock_id' => $productStock->id])->first();
             if(empty($productStockPosition)) {
-                return response()->json(['error' => 'position', 'product' => $product->id, 'productName' => $product->symbol]);
+                return response()->json(['error' => 'position', 'product' => $product ? $product->id : $item->product->id, 'productName' => $product ? $product->symbol : $item->product->symbol]);
             }
 
             $productStockLog = $productStockLogRepository->findWhere([
@@ -63,8 +68,12 @@ class ChangeWarehouseStockJob extends Job
         foreach($order->items as $item) {
             $productName = $item->product->symbol;
             preg_match('/([^-]+)/', $productName, $matches);
-            $product = $productRepository->findWhere(['symbol' => $matches[1]])->first();
-            $productStock = $productStockRepository->findWhere(['product_id' => $product->id])->first();
+            $product = Product::withTrashed()->where('symbol', $matches[1])->first();
+            if($product !== null) {
+                $productStock = $productStockRepository->findWhere(['product_id' => $product->id])->first();
+            } else {
+                $productStock = $productStockRepository->findWhere(['product_id' => $item->product->id])->first();
+            }
             $productStockRepository->update([
                 'quantity' => $productStock->quantity - $item->quantity,
             ], $productStock->id);
