7a440e8c2c4a911871e354812c235dddea128c10
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-04-24 13:17:59 +0200

    Add warehouse payments emails templates

---
+++ resources/views/emails/warehouse-accept-payment.blade.php
@@ -0,0 +1,4 @@
+<div>
+    W związku z pobraniem ze zlecenia {{ $orderId }} prosimy o zaksięgowanie kwoty {{ $amount }} na fakturę dodaną w załączniku.
+    W celu zaksięgowania, proszę
+</div>
---
+++ resources/views/emails/warehouse-invoice-request.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Uwaga - prosimy o wystawienie faktury dla zlecenia {{ $orderId }} w systemie awizacji Mega1000.
+</div>
