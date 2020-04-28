dd772a587eecd455f0eb5ffff0917625426614ea
Karol Krupinski <karolkk01@gmail.com>
2020-04-28 11:59:55 +0200

    rozbudowa systemu etykiet czatu, dodawanie crona z powiadomieniami

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,6 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs;
-use App\Jobs\UpdatePackageRealCostJob;
 
 class Kernel extends ConsoleKernel
 {
@@ -42,7 +41,8 @@ class Kernel extends ConsoleKernel
         $schedule->job(Jobs\JpgGeneratorJob::class)->dailyAt("01:00");
         $schedule->job(Jobs\ImportCsvFileJob::class)->everyMinute();
         $schedule->job(Jobs\ImportOrdersFromSelloJob::class)->cron('0 6,11,17,22 * * *');
-        $schedule->job(UpdatePackageRealCostJob::class)->dailyAt("00:30");
+        $schedule->job(Jobs\UpdatePackageRealCostJob::class)->dailyAt("00:30");
+        $schedule->job(Jobs\CheckForHangedChats::class)->cron('0,15,30,45 7-17 * * 1-5');
     }
 
     /**
