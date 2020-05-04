36ee863737c9a8de428493dab35e8144e08c254e
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-05-04 13:25:36 +0200

    Add customer order data reminder to schedule

---
+++ app/Jobs/CustomerOrderDataReminder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Entities\Label;
+use App\Entities\Order;
+use App\Mail\ConfirmData;
+use App\Mail\DifferentCustomerData;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\DB;
+
+class CustomerOrderDataReminder implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $orders = Order::whereHas('labels', function ($query) {
+            $query->whereIn('id', Label::CUSTOMER_DATA_REMINDER_IDS);
+        })->all();
+
+        foreach($orders as $order) {
+            $noData = DB::table('gt_invoices')->where('order_id', $order->id)->where('gt_invoice_status_id', '13')->first();
+            if (!empty($noData)) {
+                try {
+                    \Mailer::create()
+                        ->to($order->customer->login)
+                        ->send(new DifferentCustomerData('Wybór danych do wystawienia faktury - zlecenie'.$order->id, $order->id, $noData->id));
+                } catch (\Swift_TransportException $e) {
+                    Log::error('Customer order data change email has not been sent due to: ', $e->getMessage());
+                }
+            } else {
+                try {
+                    \Mailer::create()
+                        ->to($order->customer->login)
+                        ->send(new ConfirmData('Wybór danych do wystawienia faktury  - zlecenie'.$order->id, $order->id));
+                } catch (\Swift_TransportException $e) {
+                    Log::error('Customer order data change email has not been sent due to: ', $e->getMessage());
+                }
+            }
+        }
+    }
+}
