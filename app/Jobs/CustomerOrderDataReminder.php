7090ee2afe80b31378474e2a4dba9a08e66b0a3c
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-05-07 11:11:57 +0200

    Add namespace for Log facade

--- app/Jobs/CustomerOrderDataReminder.php
+++ app/Jobs/CustomerOrderDataReminder.php
@@ -12,6 +12,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class CustomerOrderDataReminder implements ShouldQueue
 {
