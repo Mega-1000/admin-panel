c99d59040ae9b8cc74d908d946bbfd914512841c
Karol Krupinski <karolkk01@gmail.com>
2020-05-18 15:46:30 +0200

    usunieto error logi

--- app/Http/Controllers/Api/InvoicesController.php
+++ app/Http/Controllers/Api/InvoicesController.php
@@ -14,17 +14,12 @@ class InvoicesController extends Controller
 
     public function getInvoice(Request $request, $id)
     {
-        error_log('test');
         try {
             $invoice = SubiektInvoices::findOrFail($id);
             $user = Auth::user();
-        error_log($user->id);
-        error_log('test2');
-        error_log($invoice->order->customer_id);
             if ($invoice->order->customer_id != $user->id) {
                 throw new \Exception('wrong user');
             }
-        error_log('test3');
             $item = Storage::path("user-files/invoices/$invoice->ftp_invoice_filename");
             return response()->file($item);
         } catch (\Exception $exception) {
