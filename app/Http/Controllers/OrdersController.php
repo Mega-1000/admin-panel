28df992b82e77917b12d9c47df45099b4840a6e7
Karol Krupinski <karolkk01@gmail.com>
2020-07-06 10:43:20 +0200

    dodano wyswietlanie podobnych zlecen na WZ

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2013,8 +2013,36 @@ class OrdersController extends Controller
             return null;
         });
         $order->lost = $lostFromPack;
+        $notSentYetLabel = Label::NOT_SENT_YET_LABELS_IDS;
+        $batteryId = Label::ORDER_ITEMS_REDEEMED_LABEL;
+        $hasHammerOrBagLabel = $order->labels->filter(function ($label) use ($notSentYetLabel) {
+                return in_array($label->id, $notSentYetLabel);
+            });
+        $isNotProducedYet = $order->labels->filter(function ($label) use ($batteryId) {
+            return $label->id === $batteryId ;
+        })->count() == 0;
+        if ($hasHammerOrBagLabel && $isNotProducedYet) {
+            $history = $order->customer->orders;
+            $similar = $history->reduce(function($acu, $orderh) use ($batteryId, $notSentYetLabel, $order) {
+                error_log(print_r($acu, 1));
+                if ($orderh->id == $order->id) {
+                    return $acu;
+                }
+                $hasChildHammerOrBagLabel = $orderh->labels->filter(function($label) use ($notSentYetLabel) {
+                    return in_array($label->id, $notSentYetLabel);
+                })->count() > 0;
+                $isChildNotProducedYet = $orderh->labels->filter(function ($label) use ($batteryId) {
+                    return $label->id == $batteryId;
+                })->count() == 0;
+                if ($hasChildHammerOrBagLabel && $isChildNotProducedYet) {
+                    $acu []= $orderh->id;
+                }
+                return $acu;
+            }, []);
+        }
         return View::make('orders.print', [
             'order' => $order,
+            'similar' => $similar ?? [],
             'tagHelper' => $tagHelper,
             'showPosition' => $showPosition
         ]);
