dfd99b7d524d1956f11801040f9ab1ad8b2439a4
Marcin Kania <marcin@programmers4u.com>
2020-10-23 14:21:26 +0200

    test redirect

--- tests/Browser/RedirectToAdminTest.php
+++ tests/Browser/RedirectToAdminTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
+class RedirectToAdminTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
@@ -17,7 +17,7 @@ class ExampleTest extends DuskTestCase
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                ->assertSee('Laravel');
+                ->assertPathIs('/admin/login');
         });
     }
 }
