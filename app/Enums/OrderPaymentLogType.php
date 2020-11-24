b79b5818ba1e3059ba42e75e746d22c9e7e6394c
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-11-24 16:39:10 +0100

    Update app/Enums/OrderPaymentLogType.php
    Co-authored-by: paweljar <paweljar@gmail.com>

--- app/Enums/OrderPaymentLogType.php
+++ app/Enums/OrderPaymentLogType.php
@@ -7,6 +7,6 @@ use BenSampo\Enum\Enum;
 final class OrderPaymentLogType extends Enum
 {
     const OORDER_PAYMENT = 'ORDER_PAYMENT';
-    const ClientPayment = 'CLIENT_PAYMENT';
+    const CLIENT_PAYMENT = 'CLIENT_PAYMENT';
     const RETURN_PAYMENT = 'RETURN_PAYMENT';
 }
