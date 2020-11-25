8a20d38bf19dfff362e2cd9836eb5c73e7e6cb29
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-11-25 14:17:51 +0100

    Update app/Enums/UserRole.php
    Co-authored-by: paweljar <paweljar@gmail.com>

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -7,7 +7,7 @@ use BenSampo\Enum\Enum;
 final class UserRole extends Enum
 {
     const SuperAdministrator = 1;
-    const Administrator =   2;
+    const Administrator =  2;
     const Accountant = 3;
     const Consultant = 4;
     const Storekeeper = 5;
