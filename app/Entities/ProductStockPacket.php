c86d3e3a1fdbdbd9245d65aedfec2fd603fc7346
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-11-26 17:09:30 +0100

    Update app/Entities/ProductStockPacket.php
    Co-authored-by: paweljar <paweljar@gmail.com>

--- app/Entities/ProductStockPacket.php
+++ app/Entities/ProductStockPacket.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductStockPacket extends Model
 {
     protected $fillable = [
-        'packet_quantity', 'packet_name', 'packet_product_quantity', 'product_stock_id'
+        'packet_quantity', 
+        'packet_name', 
+        'packet_product_quantity', 
+        'product_stock_id',
     ];
 }
