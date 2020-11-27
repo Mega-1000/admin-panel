54fe7cce63eae13adcfb0b1edf1cefcc77723283
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-11-28 00:54:36 +0100

    Review changes

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -729,7 +729,7 @@ class TasksController extends Controller
      */
     private function markTaskAsProduced($task)
     {
-        $response = [];
+        $response = null;
         if ($task->childs->count()) {
             $task->childs->map(function ($child) {
                 if ($child->order_id) {
