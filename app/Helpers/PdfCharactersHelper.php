d4291667dd8e4b1a9d2521973d9840f7057fa69a
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2020-12-01 14:55:33 +0100

    Use valid font for PDF & change polish characters to view properly cities

---
+++ app/Helpers/PdfCharactersHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Entities\Customer;
+use App\Entities\Employee;
+use App\Entities\Firm;
+
+class PdfCharactersHelper
+{
+    public static function changePolishCharactersToNonAccented($stringToReplace)
+    {
+        $replaceCharactersArray = array('ę' => 'e', 'ć' => 'c', 'ą' => 'a', 'ń' => 'n', 'ł' => 'l', 'ś' => 's', 'Ł' => 'L', 'Ż' => 'Z');
+
+        return strtr($stringToReplace, $replaceCharactersArray);
+    }
+}
