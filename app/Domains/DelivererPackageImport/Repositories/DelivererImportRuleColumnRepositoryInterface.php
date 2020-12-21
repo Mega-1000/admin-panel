4fc2b6301e4187118691f05576d897f483fd220f
Pawel <pawel.2.jarosz.ext@nokia.com>
2020-12-21 22:32:09 +0100

    Fix issues after code review

--- app/Domains/DelivererPackageImport/Repositories/DelivererImportRuleColumnRepositoryInterface.php
+++ app/Domains/DelivererPackageImport/Repositories/DelivererImportRuleColumnRepositoryInterface.php
@@ -9,6 +9,22 @@ use Illuminate\Support\Collection;
 
 interface DelivererImportRuleColumnRepositoryInterface
 {
+    /**
+     * Method should return null if it's not allowed to run for proper repository,
+     * otherwise a body can be implemented and method should find a order in proper db table.
+     *
+     * @param $valueToSearch
+     * @return Collection|null
+     */
     public function findOrder($valueToSearch): ?Collection;
+
+    /**
+     * Method should return null if it's not allowed to run for proper repository,
+     * otherwise a body can be implemented and method should update column we want.
+     *
+     * @param Order $order
+     * @param $valueToUpdate
+     * @return mixed
+     */
     public function updateColumn(Order $order, $valueToUpdate);
 }
