1dc9e9ea690a7068c5b4fad58ea2ffa8b9513ccf
paweljar <paweljar@gmail.com>
2021-01-05 23:46:48 +0100

    feat:[MEG-34] [PCI] Add column ALLEGRO_wartosc_prowizji_od_sprzedazy_towaru to import

--- app/Entities/OrderAllegroCommission.php
+++ app/Entities/OrderAllegroCommission.php
@@ -1,13 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderAllegroCommission extends Model
 {
+    protected $fillable = [
+        'order_id',
+        'amount',
+    ];
 
-    public function order()
+    public function order(): BelongsTo
     {
         return $this->belongsTo(Order::class);
     }
