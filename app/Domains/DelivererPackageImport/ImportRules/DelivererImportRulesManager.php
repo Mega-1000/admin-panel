85980ce5dd7a52579ebb2fee9b9535fbf537a716
Pawel <pawel.2.jarosz.ext@nokia.com>
2021-01-23 02:05:02 +0100

    hotfix: Fix for case when an order was not found

--- app/Domains/DelivererPackageImport/ImportRules/DelivererImportRulesManager.php
+++ app/Domains/DelivererPackageImport/ImportRules/DelivererImportRulesManager.php
@@ -64,9 +64,7 @@ class DelivererImportRulesManager
         }
 
         $order = $this->findOrderByRules($line, clone $this->searchRules);
-
-        $this->importLogger->logInfo("valueUsedForFindOrder: {$this->valueUsedToFindOrder}");
-
+        
         if (is_null($order)) {
             throw new OrderNotFoundException($this->valueUsedToFindOrder);
         }
