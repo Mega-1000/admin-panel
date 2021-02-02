2902b76f36c6f0f77c372cba49cbffb676dbac42
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2021-02-02 23:12:50 +0100

    Update app/Helpers/ProductSymbolCoreExtractor.php
    Co-authored-by: paweljar <paweljar@gmail.com>

--- app/Helpers/ProductSymbolCoreExtractor.php
+++ app/Helpers/ProductSymbolCoreExtractor.php
@@ -16,6 +16,6 @@ class ProductSymbolCoreExtractor
     {
         preg_match(self::PRODUCT_SYMBOL_CORE_REGEX, $productSymbol, $matches);
 
-        return isset($matches[1]) ? $matches[1] : '';
+        return $matches[1] ?? '';
     }
 }
