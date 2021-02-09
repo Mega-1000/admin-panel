e0f09a71f4fb1ef178ad5b6c0d4f8602db2a2e52
Mateusz Rusyn <mateusz.rusyn@gmail.com>
2021-02-09 22:23:55 +0100

    Update app/Repositories/OrderPaymentRepositoryEloquent.php
    Co-authored-by: paweljar <paweljar@gmail.com>

--- app/Repositories/OrderPaymentRepositoryEloquent.php
+++ app/Repositories/OrderPaymentRepositoryEloquent.php
@@ -31,7 +31,7 @@ class OrderPaymentRepositoryEloquent extends BaseRepository implements OrderPaym
      */
     public function getPromisedPayment(int $orderId, string $amount)
     {
-        return parent::findWhere([
+        return $this->findWhere([
             'order_id' => $orderId,
             'amount' => $amount,
             'promise' => OrderPaymentPromiseType::PROMISED,
