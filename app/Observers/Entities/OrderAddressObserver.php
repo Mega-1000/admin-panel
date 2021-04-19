5a83fa84c27dfcf96aca30010131ea17ff935ecc
Mateusz Urbanowicz <urbanowiczmateusz89@gmail.com>
2021-04-19 12:44:42 +0200

    MEG-100-fixed

--- app/Observers/Entities/OrderAddressObserver.php
+++ app/Observers/Entities/OrderAddressObserver.php
@@ -3,7 +3,9 @@
 namespace App\Observers\Entities;
 
 use App\Entities\OrderAddress;
+use App\Jobs\AddLabelJob;
 use App\Jobs\DispatchLabelEventByNameJob;
+use App\Services\OrderAddressService;
 
 class OrderAddressObserver
 {
@@ -21,11 +23,18 @@ class OrderAddressObserver
     {
         $hasMissingDeliveryAddressLabel = $orderAddress->order->labels()->where('label_id', 75)->get();    //brak danych do dostawy
         if (count($hasMissingDeliveryAddressLabel) > 0) {
-            if($orderAddress->type == "DELIVERY_ADDRESS") {
-                if($orderAddress->order->isDeliveryDataComplete()) {
+            if ($orderAddress->type == "DELIVERY_ADDRESS") {
+                if ($orderAddress->order->isDeliveryDataComplete()) {
                     dispatch_now(new DispatchLabelEventByNameJob($orderAddress->order, "added-delivery-address"));
                 }
             }
+        }
+    }
+
+    protected function addLabelIfManualCheckIsRequired(OrderAddress $orderAddress): void
+    {
+        if (!(new OrderAddressService())->addressIsValid($orderAddress)) {
+            dispatch_now(new AddLabelJob($orderAddress->order->id, [184]));
         }
     }
 }
