2cc6a635dc9e9fccedd4768fead141361e785239
Adrian Szlachta <office@adddesign.org>
2021-05-11 17:53:04 +0200

    console log

--- resources/layout/src/main.ts
+++ resources/layout/src/main.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-new */
+
 import Vue from 'vue'
 import App from './App.vue'
 import SetsList from '@/views/Sets/SetsList.vue'
 import SetEdit from '@/views/Sets/SetEdit.vue'
 import store from './store'
+import RenderComponent from '@/helpers/renderComponent'
 
 Vue.config.productionTip = false
 
@@ -11,16 +14,21 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-if (document.querySelector('#setsList')) {
-  new Vue({
-    store,
-    render: h => h(SetsList)
-  }).$mount('#setsList')
-}
-
-if (document.querySelector('#setEdit')) {
-  new Vue({
-    store,
-    render: h => h(SetEdit)
-  }).$mount('#setEdit')
-}
+// if (document.querySelector('#setsList')) {
+//   new Vue({
+//     store,
+//     render: h => h(SetsList)
+//   }).$mount('#setsList')
+// }
+//
+// if (document.querySelector('#setEdit')) {
+//   new Vue({
+//     store,
+//     render: h => h(SetEdit)
+//   }).$mount('#setEdit')
+// }
+RenderComponent.startRender()
+new RenderComponent(App, '#app')
+new RenderComponent(SetsList, '#setsList')
+new RenderComponent(SetEdit, '#setEdit')
+RenderComponent.endRender()
