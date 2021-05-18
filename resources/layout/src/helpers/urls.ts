b0ffcdd7b4c6d76269a8b7547f3ce5568f328909
Adrian Szlachta <office@adddesign.org>
2021-05-18 23:34:33 +0200

    Fix error

--- resources/layout/src/helpers/urls.ts
+++ resources/layout/src/helpers/urls.ts
@@ -1,6 +1,6 @@
 export function getFullUrl (path: string): string {
   let port = ''
-  if (window.location.hostname.search('localhost')) {
+  if (window.location.hostname.search('localhost') > -1) {
     port = ':8000'
   }
   return window.location.protocol + '//' + window.location.hostname + port + '/' + path
