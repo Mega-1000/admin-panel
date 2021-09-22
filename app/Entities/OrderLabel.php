5514e99b13e4da4d05711670db72e05726b09e7d
Anton Gavrilov <spirit.gavrilov@gmail.com>
2021-09-22 12:43:21 +0300

    Send notification

---
+++ app/Entities/OrderLabel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class OrderLabel.
+ *
+ * @package namespace App\Entities;
+ */
+class OrderLabel extends Model implements Transformable
+{
+	use TransformableTrait;
+	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'order_id',
+		'label_id',
+		'added_type'
+	];
+	
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function order()
+	{
+		return $this->belongsTo(Order::class);
+	}
+	
+	public function scopeRedeemed($query) {
+		$query->where('label_id', Label::ORDER_ITEMS_REDEEMED_LABEL);
+	}
+	
+	public function scopeConfirmationSended($query) {
+		$query->where('label_id', 194);
+	}
+}
