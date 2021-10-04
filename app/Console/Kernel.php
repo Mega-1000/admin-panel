27a7760a0ce26334c880d7326c17c3cde6279604
Anton Gavrilov <spirit.gavrilov@gmail.com>
2021-10-04 09:51:30 +0300

    Uncomment crons

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -58,9 +58,9 @@ class Kernel extends ConsoleKernel
         $schedule->job(Jobs\UpdateAllegroDisputes::class)->everyFifteenMinutes();
         $schedule->job(Jobs\GetNewAllegroDisputesJob::class)->everyFifteenMinutes();
         
-	    //$schedule->job(Jobs\Cron\SendFinalProformConfirmationMailsJob::class)->dailyAt("09:00");
-	    //$schedule->job(Jobs\Cron\FinalProformConfirmationAutoApprovementJob::class)->dailyAt("23:00");
-	    //$schedule->job(Jobs\Cron\SendInvoicesMailsJob::class)->dailyAt("23:45");
+	    $schedule->job(Jobs\Cron\SendFinalProformConfirmationMailsJob::class)->dailyAt("09:00");
+	    $schedule->job(Jobs\Cron\FinalProformConfirmationAutoApprovementJob::class)->dailyAt("23:00");
+	    $schedule->job(Jobs\Cron\SendInvoicesMailsJob::class)->dailyAt("23:45");
     }
 
     /**
--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -17,28 +17,12 @@ class DebugController extends Controller
 {
     public function index()
     {
+	    dispatch_now(new SendFinalProformConfirmationMailsJob());
+	    //dispatch_now(new FinalProformConfirmationAutoApprovementJob());
+	    //dispatch_now(new SendInvoicesMailsJob());
+	    
     	if (env('APP_ENV') != 'development') {
     		return;
 	    }
-    	
-    	$oa = OrderAddress::find(1);
-    	//$oa->firstname = 'test';
-    	//$oa->save();
-    	$o = $oa->order;
-    	$l = $o->labels;
-	    
-	    return;
-	    dispatch_now(new SendFinalProformConfirmationMailsJob());
-	    dispatch_now(new FinalProformConfirmationAutoApprovementJob());
-	    dispatch_now(new SendInvoicesMailsJob());
-    	return;
-    	
-	    dispatch(new OrderProformSendMailJob($oa->order, setting('allegro.address_changed_msg')));
-	    
-	    dispatch_now(new ImportOrdersFromSelloJob());
-	    dispatch_now(new FindNewAllegroOrders());
-	
-	    $orderSerivce = new AllegroOrderService();
-	    $orderSerivce->fixOrdersWithInvalidData();
     }
 }
