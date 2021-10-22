c253db823c9f97b8398db6964c9eec4ad171857e
Norbert Grzechnik <grzechniknorbert@gmail.com>
2021-10-22 12:00:33 +0200

    feature/[Backend] Formularz pytan i odpowiedzi

---
+++ app/Entities/OrderAllegro.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class OrderAllegro
+ * @package App\Entities
+ *
+ * @author Norbert Grzechnik <norbert.grzechnik@netro42.digital>
+ */
+class OrderAllegro extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
