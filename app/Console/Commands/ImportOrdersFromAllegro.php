4c809f94fbf7c348d971d41daf982393fe5bed53
Anton Gavrilov <spirit.gavrilov@gmail.com>
2022-06-30 15:04:28 +0300

    Create command

---
+++ app/Console/Commands/ImportOrdersFromAllegro.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\AllegroOrderSynchro;
+use App\Jobs\ImportOrdersFromSelloJob;
+use Illuminate\Console\Command;
+
+class ImportOrdersFromAllegro extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:allegro';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import zamówień z allegro';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        dispatch_now(new AllegroOrderSynchro());
+    }
+}
