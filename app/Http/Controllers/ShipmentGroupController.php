8b2c6b147929484464a50ceaa4c4a3eb88c2dbd4
Norbert Grzechnik <grzechniknorbert@gmail.com>
2022-10-26 23:48:01 +0200

    feature/MEG-210 Implementacja grupy przesyłek.

---
+++ app/Http/Controllers/ShipmentGroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ShipmentGroup;
+use Illuminate\Http\Request;
+
+class ShipmentGroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ShipmentGroup  $shipmentGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ShipmentGroup $shipmentGroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ShipmentGroup  $shipmentGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ShipmentGroup $shipmentGroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ShipmentGroup  $shipmentGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ShipmentGroup $shipmentGroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ShipmentGroup  $shipmentGroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ShipmentGroup $shipmentGroup)
+    {
+        //
+    }
+}
