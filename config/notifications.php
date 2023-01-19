59c6cc72ab19a17e65263b452eea46ee38bca934
Sebastian Machaj <team@blackredstudio.com>
2023-01-19 13:14:06 +0100

    notification config, load const from env

--- config/notifications.php
+++ config/notifications.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'host'       => 's104.linuxpl.com',
-    'username'   => 'awizacje@ephpolska.pl',
-    'password'   => '1!Qaa2@Wss',
-    'port'       => 587,
-    'encryption' => 'tls',
-    'from'       => 'awizacje@ephpolska.pl',
+    'host'       => env('MAIL_NOTIFICATION_HOST', 's104.linuxpl.com'),
+    'username'   => env('MAIL_NOTIFICATION_USERNAME', 'awizacje@ephpolska.pl'),
+    'password'   => env('MAIL_NOTIFICATION_PASSWORD', '1!Qaa2@Wss'),
+    'port'       => env('MAIL_NOTIFICATION_PORT', 587),
+    'encryption' => env('MAIL_NOTIFICATION_ENCRYPTION', 'tls'),
+    'from'       => env('MAIL_NOTIFICATION_FROM', 'awizacje@ephpolska.pl'),
 ];
