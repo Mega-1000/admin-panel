9bf48fd27d9f62dfbb699c82f8e0af1e954271b5
Sebastian Machaj <team@blackredstudio.com>
2023-01-26 10:42:04 +0100

    resolve conflicts

--- routes/web.php
+++ routes/web.php
@@ -597,6 +597,10 @@ Route::group(['prefix' => 'admin'], function () {
 
 Route::get('/dispatch_now-job/order-status-change', 'DispatchJobController@orderStatusChange');
 
+Route::get('/order-offer-pdf/{id}', 'OrderOfferController@getPdf');
+Route::get('/order-proform-pdf/{id}', 'OrderOfferController@getProform');
+Route::get('/dispatch-job/order-status-change', 'DispatchJobController@orderStatusChange');
+
 Route::get('/debug', 'DebugController@index');
 Route::get('/communication/{warehouseId}/{orderId}', 'OrdersMessagesController@communication');
 Route::get('/communication/{orderId}', 'OrdersMessagesController@userCommunication');
