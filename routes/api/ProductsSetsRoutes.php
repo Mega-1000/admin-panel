6f6e0419245d03a70e60b2bfce2330df86accdf9
Prosofts Bartosz Woszczak <bartosz.woszczak@gmail.com>
2023-01-30 22:02:52 +0100

    fix for product sets

--- routes/api/ProductsSetsRoutes.php
+++ routes/api/ProductsSetsRoutes.php
@@ -1,16 +1,16 @@
 <?php
 
-Route::get('/products',  ['as' => 'products', 'uses' => 'Api\SetsController@products']);
-Route::get('/',  ['as' => 'index', 'uses' => 'Api\SetsController@index']);
-Route::get('/{set}',  ['as' => 'set', 'uses' => 'Api\SetsController@set']);
-Route::get('/products/{product}',  ['as' => 'product', 'uses' => 'Api\SetsController@productsStocks']);
-Route::get('/{set}/products/',  ['as' => 'product', 'uses' => 'Api\SetsController@stocksAllSetsProducts']);
-Route::post('/products',  ['as' => 'searchProducts', 'uses' => 'Api\SetsController@products']);
+Route::get('/products', ['as' => 'products', 'uses' => 'Api\SetsController@products']);
+Route::get('/', ['as' => 'index', 'uses' => 'Api\SetsController@index']);
+Route::get('/{set}', ['as' => 'set', 'uses' => 'Api\SetsController@set']);
+Route::get('/products/{product}', ['as' => 'product', 'uses' => 'Api\SetsController@productsStocks']);
+Route::get('/{set}/products/', ['as' => 'set_products', 'uses' => 'Api\SetsController@stocksAllSetsProducts']);
+Route::post('/products', ['as' => 'searchProducts', 'uses' => 'Api\SetsController@products']);
 Route::post('/', ['as' => 'store', 'uses' => 'Api\SetsController@store']);
 Route::post('/{set}/completing/', ['as' => 'completing', 'uses' => 'Api\SetsController@completing']);
 Route::post('/{set}/disassembly/', ['as' => 'disassembly', 'uses' => 'Api\SetsController@disassembly']);
-Route::post('/{set}/products/',  ['as' => 'addProduct', 'uses' => 'Api\SetsController@addProduct']);
+Route::post('/{set}/products/', ['as' => 'addProduct', 'uses' => 'Api\SetsController@addProduct']);
 Route::put('/{set}', ['as' => 'update', 'uses' => 'Api\SetsController@update']);
-Route::put('/{set}/products/{product}',  ['as' => 'editProduct', 'uses' => 'Api\SetsController@editProduct']);
+Route::put('/{set}/products/{product}', ['as' => 'editProduct', 'uses' => 'Api\SetsController@editProduct']);
 Route::delete('/{set}', ['as' => 'delete', 'uses' => 'Api\SetsController@delete']);
-Route::delete('/{set}/products/{product}',  ['as' => 'deleteProduct', 'uses' => 'Api\SetsController@deleteProduct']);
+Route::delete('/{set}/products/{product}', ['as' => 'deleteProduct', 'uses' => 'Api\SetsController@deleteProduct']);
