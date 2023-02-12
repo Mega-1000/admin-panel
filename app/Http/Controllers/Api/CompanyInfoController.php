ac4f1cb6e047931fa2c5c859bc99a64499ad697c
Prosofts Bartosz Woszczak <bartosz.woszczak@gmail.com>
2023-02-12 22:24:32 +0100

    :fire: Update for GUSApi

--- app/Http/Controllers/Api/CompanyInfoController.php
+++ app/Http/Controllers/Api/CompanyInfoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use GusApi\Exception\InvalidUserKeyException;
+use GusApi\Exception\NotFoundException;
 use GusApi\GusApi;
 use GusApi\ReportTypes;
 
@@ -18,20 +19,20 @@ class CompanyInfoController extends Controller
 
         try {
             $gus->login();
-            $reports       = $gus->getByNip($nip);
-            $reportTypeJDG = ReportTypes::REPORT_ACTIVITY_PHYSIC_CEIDG;
-            $reportType    = ReportTypes::REPORT_PUBLIC_LAW;
-            $data          = $gus->getFullReport($reports[0], $reportType);
+            $reports = $gus->getByNip($nip);
+            $reportTypeJDG = ReportTypes::REPORT_PERSON_CEIDG;
+            $reportType = ReportTypes::REPORT_ORGANIZATION;
+            $data = $gus->getFullReport($reports[0], $reportType);
             if (count($data) == 0) {
-                $data         = $gus->getFullReport($reports[0], $reportTypeJDG)[0];
+                $data = $gus->getFullReport($reports[0], $reportTypeJDG)[0];
                 $data['type'] = 'JDG';
             } else {
-                $data         = $gus->getFullReport($reports[0], $reportType)[0];
+                $data = $gus->getFullReport($reports[0], $reportType)[0];
                 $data['type'] = 'SP';
             }
         } catch (InvalidUserKeyException $e) {
             $data['error'] = 'Bad user key';
-        } catch (\GusApi\Exception\NotFoundException $e) {
+        } catch (NotFoundException $e) {
             $data['error'] = 'No data found';
         }
 
