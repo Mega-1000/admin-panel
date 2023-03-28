4a6c49492b2f44bb30b9440f68e38b6f7d9972ed
Antoni Seba <antoniwoj@o2.pl>
2023-03-28 18:19:35 +0200

    Revert

--- app/Jobs/GenerateXmlForNexoJob.php
+++ app/Jobs/GenerateXmlForNexoJob.php
@@ -61,6 +61,13 @@ class GenerateXmlForNexoJob implements ShouldQueue
     {
         $orders = $this->orderRepository->whereHas('labels', function ($query) {
             $query->where('label_id', Label::INVOICE_TO_ISSUE);
+        })->whereHas('labels', function ($query) {
+            $query->where('label_id', Label::ORDER_RECEIVED_INVOICE_TODAY);
+        })->whereHas('labels', function ($query) {
+            $query->where('label_id', Label::ORDER_ITEMS_REDEEMED_LABEL)
+                ->orWhere('label_id', Label::RETURN_ALLEGRO_PAYMENTS);
+        })->whereDoesntHave('labels', function ($query) {
+            $query->where('label_id', Label::XML_INVOICE_GENERATED);
         })->get();
 
         $fileNames = [];
