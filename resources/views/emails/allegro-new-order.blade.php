e64ed018fa546201670c3769b18eff75d40fe078
Sebastian Machaj <team@blackredstudio.com>
2023-04-03 22:47:32 +0200

    fix

--- resources/views/emails/allegro-new-order.blade.php
+++ resources/views/emails/allegro-new-order.blade.php
@@ -1,3 +1,3 @@
 {!! nl2br(setting('allegro.new_allegro_order_msg')) !!}
 Jeżeli masz pytania zapoznaj się z naszym FAQ:<br>
-https://www.mega1000.pl/faq?phone={{$base64Email}}&email={{$base64Phone}}<br>
+https://www.mega1000.pl/faq?phone={{$base64Email}}&email={{$base64Phone}}&showFaq=true<br>
