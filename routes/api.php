da73f131ab4702c89acbce2cf4501db3c5678ae2
Antoni Seba <antoniwoj@o2.pl>
2023-04-03 21:04:39 +0200

    Rest of required interactions

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::middleware('auth:api')->group(function () {
     Route::get('invoices/get/{id}', 'Api\InvoicesController@getInvoice')->name('api.invoices.get');
     Route::post('user/change-password', 'Api\CustomersController@changePassword')->name('api.customers.change-password');
     Route::post('user/update-informations', 'Api\CustomersController@updateInformations')->name('api.customers.update-informations');
+    Route::get('user/get-orders', 'Api\CustomersController@getOrders')->name('api.customers.get-orders');
 
     Route::middleware('staff.api')->group(function () {
         Route::group(['prefix' => 'faqs'], function () {
