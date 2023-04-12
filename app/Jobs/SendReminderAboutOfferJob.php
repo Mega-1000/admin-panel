98b33863a123ebcbc5aae9541bc09861a1098957
Antoni Seba <antoniwoj@o2.pl>
2023-04-12 14:20:56 +0200

    Add reminders

---
+++ app/Jobs/SendReminderAboutOfferJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Entities\Order;
+use App\Facades\Mailer;
+use App\Mail\SendReminderAboutOffer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendReminderAboutOfferJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public Order $order;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->order->reminder_date = null;
+        $this->order->save();
+
+        Mailer::create()
+            ->to($this->order->customer->login)
+            ->send(new SendReminderAboutOffer($this->order->customer));
+    }
+}
