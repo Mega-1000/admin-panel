2bea4304c23165a6681237e05cd6848635675bb6
Antoni Seba <antoniwoj@o2.pl>
2023-04-19 23:09:08 +0200

    update ProductStocksRoutes.php

--- routes/web/ProductStocksRoutes.php
+++ routes/web/ProductStocksRoutes.php
@@ -3,49 +3,49 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('products/stocks')->group(function () {
-    Route::get('products/stocks', 'ProductStocksController@index')->name('product_stocks.index');
-    Route::post('products/stocks/datatable', 'ProductStocksController@datatable')->name('product_stocks.datatable');
-    Route::get('products/stocks/print', 'ProductStocksController@print')->name('product_stocks.print');
-    Route::get('products/stocks/printReport', 'ProductStocksController@printReport')->name('product_stocks.printReport');
-    Route::get('products/stocks/{id}/edit', 'ProductStocksController@edit')->name('product_stocks.edit');
-    Route::put('products/stocks/{id}/update', 'ProductStocksController@update')->name('product_stocks.update');
-    Route::put('products/stocks/{id}/change-status',
+    Route::get('/', 'ProductStocksController@index')->name('product_stocks.index');
+    Route::post('/datatable', 'ProductStocksController@datatable')->name('product_stocks.datatable');
+    Route::get('/print', 'ProductStocksController@print')->name('product_stocks.print');
+    Route::get('/printReport', 'ProductStocksController@printReport')->name('product_stocks.printReport');
+    Route::get('/{id}/edit', 'ProductStocksController@edit')->name('product_stocks.edit');
+    Route::put('/{id}/update', 'ProductStocksController@update')->name('product_stocks.update');
+    Route::put('/{id}/change-status',
         'ProductStocksController@changeStatus')->name('product_stocks.change.status');
-    Route::get('products/stocks/{id}/positions/create',
+    Route::get('/{id}/positions/create',
         'ProductStockPositionsController@create')->name('product_stocks.position.create');
-    Route::get('products/stocks/{id}/positions/datatable',
+    Route::get('/{id}/positions/datatable',
         'ProductStockPositionsController@datatable')->name('product_stocks.position.datatable');
-    Route::post('products/stocks/{id}/positions/store',
+    Route::post('/{id}/positions/store',
         'ProductStockPositionsController@store')->name('product_stocks.position.store');
-    Route::get('products/stocks/{id}/positions/{position_id}/edit',
+    Route::get('/{id}/positions/{position_id}/edit',
         'ProductStockPositionsController@edit')->name('product_stocks.position.edit');
-    Route::put('products/stocks/{id}/positions/{position_id}/update',
+    Route::put('/{id}/positions/{position_id}/update',
         'ProductStockPositionsController@update')->name('product_stocks.position.update');
-    Route::delete('products/stocks/{id}/positions/{position_id}',
+    Route::delete('/{id}/positions/{position_id}',
         'ProductStockPositionsController@destroy')->name('product_stocks.position.destroy');
-    Route::get('products/stocks/{id}/logs/datatable',
+    Route::get('/{id}/logs/datatable',
         'ProductStockLogsController@datatable')->name('product_stocks.logs.datatable');
-    Route::get('products/stocks/{id}/logs/{log_id}/show',
+    Route::get('/{id}/logs/{log_id}/show',
         'ProductStockLogsController@show')->name('product_stocks.logs.show');
-    Route::get('products/stocks/packets/create',
+    Route::get('/packets/create',
         'ProductStockPacketsController@create')->name('product_stock_packets.create');
-    Route::post('products/stocks/packets',
+    Route::post('/packets',
         'ProductStockPacketsController@store')->name('product_stock_packets.store');
-    Route::delete('products/stocks/packets/{packetId}',
+    Route::delete('/packets/{packetId}',
         'ProductStockPacketsController@delete')->name('product_stock_packets.delete');
-    Route::get('products/stocks/packets',
+    Route::get('/packets',
         'ProductStockPacketsController@index')->name('product_stock_packets.index');
     Route::get('orders/{orderId}/packet/{packetId}/use',
         'OrdersController@usePacket')->name('orders.usePacket');
-    Route::post('products/stocks/packets/{packetId}/orderItem/{orderItemId}/assign',
+    Route::post('/packets/{packetId}/orderItem/{orderItemId}/assign',
         'Api\ProductStockPacketsController@assign')->name('product_stock_packets.assign');
-    Route::post('products/stocks/packets/orderItem/{orderItemId}/retain',
+    Route::post('/packets/orderItem/{orderItemId}/retain',
         'Api\ProductStockPacketsController@retain')->name('product_stock_packets.retain');
-    Route::get('products/stocks/packets/product/stock/check',
+    Route::get('/packets/product/stock/check',
         'Api\ProductStockPacketsController@checkProductStockForPacketAssign')->name('product_stock_packets.product.stock.check');
-    Route::get('products/stocks/packets/{packetId}',
+    Route::get('/packets/{packetId}',
         'ProductStockPacketsController@edit')->name('product_stock_packets.edit');
-    Route::put('products/stocks/packets',
+    Route::put('/packets',
         'ProductStockPacketsController@update')->name('product_stock_packets.update');
 
     Route::get('{productStock:product_id}/place-admin-order',
@@ -56,11 +56,11 @@ Route::prefix('products/stocks')->group(function () {
         'ProductStocksController@createAdminOrder')->name('product_stocks.createAdminOrder');
 
     Route::get('place-multiple-admin-orders',
-        'ProductStocksController@placeMultipleAdminSideOrders')->name('product_stocks.placeMultipleAdminOrders');
-    Route::get('place-multiple-admin-orders/calculate',
-        'ProductStocksController@calculateMultipleAdminOrders')->name('product_stocks.calculateMultipleAdminOrders');
-    Route::post('place-multiple-admin-orders/confirm', 'ProductStocksController@createMultipleAdminOrders')
-        ->name('product_stocks.createMultipleAdminOrders');
+            'ProductStocksController@placeMultipleAdminSideOrders')->name('product_stocks.placeMultipleAdminOrders');
+        Route::get('place-multiple-admin-orders/calculate',
+            'ProductStocksController@calculateMultipleAdminOrders')->name('product_stocks.calculateMultipleAdminOrders');
+        Route::post('place-multiple-admin-orders/confirm', 'ProductStocksController@createMultipleAdminOrders')
+            ->name('product_stocks.createMultipleAdminOrders');
 });
 
 
