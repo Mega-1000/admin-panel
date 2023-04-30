4e996c374776385e9785a175185bb3b7eebd1b5e
Antoni Seba <antoniwoj@o2.pl>
2023-04-30 09:28:40 +0200

    Add auto filling data in orders (#502)

---
+++ app/Services/OrderAddressesService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Customer;
+use App\Entities\CustomerAddress;
+use App\Entities\OrderAddress;
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class OrderAddressesService
+{
+    /**
+     * @param string $type
+     * @return void
+     * @throws Exception
+     */
+    private static function checkAddressType(string $type): void
+    {
+        if (!in_array($type, ['INVOICE_ADDRESS', 'DELIVERY_ADDRESS'])) {
+            throw new Exception('invalid_address_type');
+        }
+    }
+
+    /**
+     * @param string $type
+     * @param Customer $customer
+     *
+     * @return Model|HasMany
+     * @throws Exception
+     */
+    private static function getUserAddress(string $type, Customer $customer): CustomerAddress
+    {
+        self::checkAddressType($type);
+
+        return $customer->addresses()->where('type', $type)->first();
+    }
+
+    /**
+     * @param Customer $customer
+     * @param OrderAddress $orderAddress
+     *
+     * @return OrderAddress
+     * @throws Exception
+     */
+    public static function updateOrderAddressFromCustomer(OrderAddress $orderAddress, Customer $customer): OrderAddress
+    {
+        $customerAddress = self::getUserAddress($orderAddress->type, $customer);
+        unset($customerAddress->customer_id);
+
+        $orderAddress->fill($customerAddress->toArray());
+        $orderAddress->save();
+
+        return $orderAddress;
+    }
+}
