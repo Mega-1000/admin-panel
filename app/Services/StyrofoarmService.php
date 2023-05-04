3f37dcbc80b83df3b70fcbeb285d2da834f18365
Antoni Seba <antoniwoj@o2.pl>
2023-05-04 21:27:02 +0200

    Add some changes

--- app/Services/StyrofoarmService.php
+++ app/Services/StyrofoarmService.php
@@ -23,19 +23,6 @@ class StyrofoarmService
      */
     public function getStyrofoarmsByFirms(): array
     {
-//        $products = Products::getStyrofoarmsWithoutVariations();
-//        $firms = [];
-//        $response = [];
-//        //output should be like this:
-//        // Product1: [product_from_firm1, product_from_firm2, product_from_firm3],
-//        // Product2: [product_from_firm1, product_from_firm2, product_from_firm3]
-//
-//        $variations = $this->productService->getVariationsFromProducts($products, PostalCodeLatLons::getLatLonByPostalCode('66-400'));
-//
-//        foreach ($firms as $product => $firm) {
-//            $response[$product] = array_unique($firm);
-//        }
-//
-//        return $firms;
+        $products = Products::getStyrofoarmsWithoutVariations();
     }
 }
