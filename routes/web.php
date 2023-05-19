f2a75de963596c2c08c382836b3913d8140f1d7a
Piotr Kołodziej <pkolodziej@animativ.pl>
2023-05-19 21:42:24 +0200

    cleanup

--- routes/web.php
+++ routes/web.php
@@ -503,12 +503,10 @@ Route::group(['prefix' => 'admin'], function () {
                     Route::post('/produce-break-down', 'TasksController@breakDownTask')->name('breakDownTask');
                     Route::post('/mark-denied/', 'TasksController@deny')->name('deny');
 
-                    Route::get('/adding-task-to-planner', 'TasksController@addingTaskToPlanner')->name('addingTaskToPlanner');
-                    //Route::post('/adding-task-to-planner', 'TasksController@addingTaskToPlanner')->name('addingTaskToPlanner');
+                    Route::post('/adding-task-to-planner', 'TasksController@addingTaskToPlanner')->name('addingTaskToPlanner');
                     Route::post('/store/planner', 'TasksController@saveTaskToPlanner')->name('storePlanner');
 
                     Route::get('/{taskId}/checkQuantityInStock', 'TasksController@checkQuantityInStock')->name('checkQuantityInStock');  
-                    Route::get('/test', 'TasksController@test')->name('test');  
                     
                 });
             Route::prefix('reports')->as('reports.')
