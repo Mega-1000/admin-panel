6ce57e3c47225dc2f62751467c44c5fa98eb3a5d
Antoni Seba <antoniwoj@o2.pl>
2023-06-19 16:13:52 +0200

    Add redirect after importing

--- app/Http/Controllers/ImportAllegroBillingController.php
+++ app/Http/Controllers/ImportAllegroBillingController.php
@@ -6,19 +6,19 @@ use App\Factory\AllegroBilling\ImportAllegroBillingDTOFactory;
 use App\Http\Requests\ImportAllegroBillingRequest;
 use App\Services\ImportAllegroBillingService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+
 class ImportAllegroBillingController extends Controller
 {
     public function __invoke(
         ImportAllegroBillingRequest    $request,
         ImportAllegroBillingDTOFactory $DTOFactory,
         ImportAllegroBillingService    $importAllegroBillingService
-    ): JsonResponse
+    ): RedirectResponse
     {
         $data = $DTOFactory->createFromFile($request->file('file'));
         $importAllegroBillingService->import($data);
 
-        return response()->json([
-            'message' => 'Imported Allegro billing',
-        ]);
+        return redirect()->back()->with('success', 'Imported successfully');
     }
 }
