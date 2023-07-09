861434bb38da60a70da9d5c2559709a0ae011bce
Antoni Seba <antoniwoj@o2.pl>
2023-07-09 23:55:15 +0200

    Add truncating of data at the begining of csv parsing

--- app/Http/Controllers/ControllSubjectInvoiceController.php
+++ app/Http/Controllers/ControllSubjectInvoiceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Factory\ControllSubjectInvoiceDTOFactory;
 use App\Services\ControllSubjectInvoiceService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ControllSubjectInvoiceController extends Controller
 {
@@ -13,6 +14,8 @@ class ControllSubjectInvoiceController extends Controller
         Request $request,
     ): array
     {
+        DB::statemnt('TRUNCATE TABLE controll_subject_invoices');
+
         return $controllSubjectInvoiceService->handle(
             ControllSubjectInvoiceDTOFactory::createFromCsvFile($request->file('file'))
         );
