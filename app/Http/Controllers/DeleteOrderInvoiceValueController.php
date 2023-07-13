e7bd51e1844c7a0acfc50681e86dbd872ca4c0b3
Antoni Seba <antoniwoj@o2.pl>
2023-07-13 16:38:33 +0200

    Update index.blade.php

--- app/Http/Controllers/DeleteOrderInvoiceValueController.php
+++ app/Http/Controllers/DeleteOrderInvoiceValueController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Order;
 use App\Entities\OrderInvoiceValue;
+use App\Repositories\OrderInvoiceValues;
+use App\Repositories\Orders;
+use App\Services\Label\AddLabelService;
 use Illuminate\Http\RedirectResponse;
 
 class DeleteOrderInvoiceValueController extends Controller
 {
     public function __invoke(int $id): RedirectResponse
     {
+        $order = Order::findOrFail(OrderInvoiceValue::findOrFail($id)->order_id);
         OrderInvoiceValue::findOrFail($id)->delete();
+
+        $orderInvoiceValuesSum = OrderInvoiceValues::getSumOfInvoiceValuesByOrder($order);
+        $orderValue = $order->getValue() + Orders::getOrderReturnGoods($order) - Orders::getSumOfWTONPayments($order);
+        $arr = [];
+        if (round($orderInvoiceValuesSum, 2) != round($orderValue, 2)) {
+            AddLabelService::addLabels($order, [231],$arr, []);
+
+            $order->labels()->detach(232);
+            return;
+        }
+
+        AddLabelService::addLabels($order, [232],$arr, []);
+        $order->labels()->detach(231);
 
         return redirect()->back();
     }
