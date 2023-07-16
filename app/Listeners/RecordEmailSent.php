04a4df28ead6f8abe37d4556a39a112d14ae3915
Antoni Seba <antoniwoj@o2.pl>
2023-07-16 22:49:08 +0200

    Update report creation after sending any email

--- app/Listeners/RecordEmailSent.php
+++ app/Listeners/RecordEmailSent.php
@@ -25,7 +25,6 @@ class RecordEmailSent
      */
     public function handle(MessageSent $event): void
     {
-        dd( $event->data['message']->parameters);
         MailReport::create([
             'email' => $event->data['email'],
             'subject' => $event->data['title'],
