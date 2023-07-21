80c42e2ea7e5162905f15693476a38f6644ea478
Antoni Seba <antoniwoj@o2.pl>
2023-07-21 12:58:19 +0200

    Allegro payments make work - hopefully

--- app/Jobs/ImportAllegroPayInJob.php
+++ app/Jobs/ImportAllegroPayInJob.php
@@ -228,19 +228,17 @@ final class ImportAllegroPayInJob implements ShouldQueue
 
         $existingPayment = $order->payments()->where('amount', $payIn['kwota'])->first();
 
-        if (empty($existingPayment)) {
-            $order->payments()->create([
-                'amount' => $payIn['kwota'],
-                'type' => 'CLIENT',
-                'promise' => '',
-                'external_payment_id' => $payIn['identyfikator'],
-                'payer' => $order->customer->login,
-                'operation_date' => Carbon::parse($payIn['data']),
-                'comments' => implode(' ', $payIn),
-                'operation_type' => 'wplata/wyplata allegro',
-                'status' => $declaredSum ? 'Rozliczająca deklarowaną' : null,
-            ]);
-        }
+        $order->payments()->create([
+            'amount' => $payIn['kwota'],
+            'type' => 'CLIENT',
+            'promise' => '',
+            'external_payment_id' => $payIn['identyfikator'],
+            'payer' => $order->customer->login,
+            'operation_date' => Carbon::parse($payIn['data']),
+            'comments' => implode(' ', $payIn),
+            'operation_type' => 'wplata/wyplata allegro',
+            'status' => $declaredSum ? 'Rozliczająca deklarowaną' : null,
+        ]);
     }
 
     /**
