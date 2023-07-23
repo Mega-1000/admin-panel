6964b8a344fa32912ef758106b44a7cf9eae8a6c
budzix-dev <pbudziak.github@gmail.com>
2023-07-24 01:54:45 +0200

    some changes

--- resources/views/allegro-return/index.blade.php
+++ resources/views/allegro-return/index.blade.php
@@ -8,14 +8,33 @@
 
 @section('table')
     @if($order->items)
-        @foreach($order->items as $item)
-            <div class="col-md-6">
-                <h4>
-                    <img src="{!! $item->product->getImageUrl() !!}" style="width: 179px; height: 130px;">
-                    <strong>{{ $loop->iteration }}. </strong>{{ $item->product->name }}
-                    (symbol: {{ $item->product->symbol }})
-                </h4>
+        <form enctype="multipart/form-data" action="{{ action('AllegroReturnPaymentController@store', ['orderId' => $order->id])}}" method="POST" class="form-horizontal">
+            {{ csrf_field() }}
+            <div class="grid">
+                @if(count($existingAllegroReturns) > 0)
+                    <div class="alert alert-warning">
+                        <strong>Uwaga!</strong> Istnieją już zwroty dla tego zamówienia.
+                    </div>
+                @endif
+                @foreach($order->items as $item)
+                    <div style="width: 60%">
+                        <h4>
+                            <img src="{!! $item->product->getImageUrl() !!}" style="width: 179px; height: 130px;">
+                            <strong>{{ $loop->iteration }}. </strong>{{ $item->product->name }}
+                            (symbol: {{ $item->product->symbol }})
+                        </h4>
+                        <hr />
+                    </div>
+                    <div style="width: 40%">
+                        <input type="hidden" name="return[{{$loop->iteration}}][id]"
+                                               @if(count($item->realProductPositions())) @if(isset($order->returnPosition($item->realProductPositions()->first()['id'])->id))value="{{$order->returnPosition($item->realProductPositions()->first()['id'])->id}}" @endif @endif>
+                        <input class="return-check" type="checkbox"
+                                name="return[{{$loop->iteration}}][check]" value="{{$loop->iteration}}"
+                                @if(count($item->realProductPositions()) && $order->returnPosition($item->realProductPositions()[0]['id'])!==null) checked @endif>
+                        Dodaj zwrot
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </form>
     @endif
 @endsection
