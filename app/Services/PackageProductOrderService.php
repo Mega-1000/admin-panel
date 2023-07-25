f33a1da3b35645e99480e9f5ec5c49982f7693b7
Antoni Seba <antoniwoj@o2.pl>
2023-07-25 07:06:26 +0200

    Add package product creation options (#580)

---
+++ app/Services/PackageProductOrderService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Order;
+use App\Entities\Product;
+use App\Factory\OrderBuilderFactory;
+
+class PackageProductOrderService
+{
+    public function store(Order $order, array $data): void
+    {
+        foreach ($data['quantity'] as $key => $quantity) {
+            if ($quantity === 0) {
+                continue;
+            }
+
+            $productArray = Product::find($key)->toArray();
+
+            if ($data['subtract-from-shipping-cost'] === 'on') {
+                $order->shipment_price_for_client -= $productArray['gross_selling_price_commercial_unit'] * $quantity;
+            }
+
+            if ($data['do-not-count-price']) {
+                $productArray['gross_selling_price_commercial_unit'] = 0;
+            }
+
+            OrderBuilderFactory::create()
+                ->assignItemsToOrder(
+                    $order,
+                    [
+                        $productArray +
+                        ['amount' => $quantity]
+                    ],
+                    false,
+                );
+        }
+
+        $order->save();
+    }
+}
--- resources/views/package-product-order/create.blade.php
+++ resources/views/package-product-order/create.blade.php
@@ -9,6 +9,14 @@
 @section('app-content')
     <div class="panel panel-bordered panel-body" style="margin: 0 auto; width: 94%">
         <form action="{{ route('storePackageProductOrder', ['order' => $order->id]) }}" method="post">
+            <div>
+                <label for="name">Odejmij z kosztów transportu</label>
+                <input type="checkbox" name="subtract-from-shipping-cost">
+
+                <label for="name">Nie licz cen</label>
+                <input type="checkbox" name="do-not-count-price">
+            </div>
+
             @csrf
             <label for="product">Produkt</label>
             <div style="display: flex;">
