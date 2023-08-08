0d145ef574877f88a6f043e9b2781d18076a6ffd
budzix-dev <pbudziak.github@gmail.com>
2023-08-08 13:39:13 +0200

    make allegro return work for non-constructed items

--- app/Services/AllegroPaymentsReturnService.php
+++ app/Services/AllegroPaymentsReturnService.php
@@ -46,20 +46,34 @@ class AllegroPaymentsReturnService
 
     public function getOrderItemsWithReturns(Order $order): ?Order
     {
+        if (count($order->items) === 0) {
+            return null;
+        }
+
+        $orderIsConstructed = $order->labels()->where('label_id', Label::ORDER_ITEMS_CONSTRUCTED)->exists();
+
         $hasOrderReturn = false;
 
         foreach ($order->items as $item) {
             $productId = $item->product_id;
             $orderReturn = OrderReturn::with(['product'])->where('product_id', $productId)->where('order_id', $order->id)->orderByDesc('created_at')->first();
             if (empty($orderReturn)) {
-                $item['orderReturn'] = null;
+                if ($orderIsConstructed) {
+                    $item['orderReturn'] = null;
+                    continue;
+                }
+
+                $dummyReturn = new OrderReturn();
+                $dummyReturn->quantity_undamaged = $item->quantity;
+                $dummyReturn->quantity_damaged = 0;
+                $item['orderReturn'] = $dummyReturn;
                 continue;
             }
             $item['orderReturn'] = $orderReturn;
             $hasOrderReturn = true;
         }
 
-       return $hasOrderReturn ? $order : null;
+       return ($hasOrderReturn || !$orderIsConstructed) ? $order : null;
     }
 
     public function returnCommissionsAndReturnFailed(array $lineItemsForCommissionRefund, array $returnsByAllegroId) {
