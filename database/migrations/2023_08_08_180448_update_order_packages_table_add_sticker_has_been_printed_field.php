251d4a232fd89200c8e74b56ddf3748f3244c856
budzix-dev <115490079+budzix-dev@users.noreply.github.com>
2023-08-09 09:02:27 +0200

    confirm to print sticker for second time (#596)

---
+++ database/migrations/2023_08_08_180448_update_order_packages_table_add_sticker_has_been_printed_field.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_packages', function (Blueprint $table) {
+            $table->boolean('sticker_has_been_printed')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_packages', function (Blueprint $table) {
+            $table->dropColumn('sticker_has_been_printed');
+        });
+    }
+};
