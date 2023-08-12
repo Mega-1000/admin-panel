996f9a4b54719bf9ad2408ad34ad313fb1383797
root <root@anna1.f2.webh.me>
2023-08-12 13:59:11 +0200

    update PackageProductOrderService.php

--- app/Services/PackageProductOrderService.php
+++ app/Services/PackageProductOrderService.php
@@ -67,8 +67,9 @@ final class PackageProductOrderService
             ->toArray();
 
         foreach ($orderItems as &$orderItem) {
-            $orderItem['amount'] = $orderItem['quantity'];
-            $orderItem['product_id'] = $orderItem['product']['id'];
+   	    $orderItem['amount'] = 0;
+	    $orderItem['amount'] = $orderItem['quantity'];
+            $orderItem['id'] = $orderItem['product']['id'];
         }
 
         return [$productArray, $orderItems];
