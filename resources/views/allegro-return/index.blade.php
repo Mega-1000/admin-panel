65f9ba310e1f52639bf9b7f07bbd2a274f8a22d8
Antoni Seba <antoniwoj@o2.pl>
2023-08-14 12:29:07 +0200

    fix unwanted form submittion

--- resources/views/allegro-return/index.blade.php
+++ resources/views/allegro-return/index.blade.php
@@ -114,7 +114,7 @@
 @endsection
 
 @section('datatable-scripts')
-    <script src="sweetalert2.all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="/js/helpers/fastResponseSendBox.js"></script>
     <script>
         const save = async (id, ) => {
