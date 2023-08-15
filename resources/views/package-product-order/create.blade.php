fb4eaa65df9cc6d5b72515ac07b9880e347c8520
Antoni Seba <antoniwoj@o2.pl>
2023-08-15 10:32:51 +0200

    Checkboxes texts changes

--- resources/views/package-product-order/create.blade.php
+++ resources/views/package-product-order/create.blade.php
@@ -17,18 +17,20 @@
 @section('app-content')
     <div class="panel panel-bordered panel-body" style="margin: 0 auto; width: 94%">
         <form action="{{ route('storePackageProductOrder', ['order' => $order->id]) }}" method="post">
+            @csrf
             <div>
-                <label for="name">Odejmij z kosztów transportu</label>
+                <label for="name">Odejmij z kosztów transportu (warsość zaznaczonych towarów będzie odjęta od kosztów transpotu w przypadku gdy wartość zaznaczonego towaru jest mniejsza niż koszty transportu ta opcja zadziała)</label>
                 <input type="checkbox" name="subtract-from-shipping-cost">
                 <br>
-                <label for="name">Nie licz cen</label>
+                <label for="name">Nie licz cen (doda towary dodając cenę sprzedaży 0zł)</label>
                 <input type="checkbox" name="do-not-count-price">
                 <br>
-                <label for="name">ceny towarów pakowych 0,01</label>
+                <label for="name">ceny towarów pakowych 0,01 (Wszystkie zaznaczone towary będą posiadały ceny 0.01)</label>
                 <input type="checkbox" name="package-products-price-0-01">
+                <br>
+                <label for="name">ceny sprzedaży standardowe aczkolwiek z innych towarów i końcówkę ewentualnie z transportu</label>
             </div>
 
-            @csrf
             <label for="product">Produkt</label>
             <div class="products-grid">
                 @foreach($packageProducts as $product)
