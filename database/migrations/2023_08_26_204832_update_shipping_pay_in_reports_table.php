7fead72e3fe9a48f818a73ae9d3085a3f71abd8a
Antoni Seba <antoniwoj@o2.pl>
2023-08-26 21:18:35 +0200

    Add shipment reports comments (#611)

---
+++ database/migrations/2023_08_26_204832_update_shipping_pay_in_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('shipping_pay_in_reports', function (Blueprint $table) {
+            $table->text('comments')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- resources/views/shipment-pay-in-report/index.blade.php
+++ resources/views/shipment-pay-in-report/index.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.datatable')
+
+@section('head')
+    @livewireStyles
+@endsection
+
+
 <script src="https://cdn.tailwindcss.com" ></script>
 
 <script defer>
@@ -28,68 +34,76 @@
 
         </div>
     </form>
-
-    <table class="table table-bordered">
-        <tr>
-            <th>
-                ID
-            </th>
-            <th>
-                Numer listu
-            </th>
-            <th>
-                Numer faktury do której dany LP został przydzielony
-            </th>
-            <th>
-                Data nadania otrzymania
-            </th>
-            <th>
-                Nr i d
-            </th>
-            <th>
-                Rzeczywisty koszt transportu brutto
-            </th>
-            <th>
-                Wartość pobrania
-            </th>
-            <th>
-                plik
-            </th>
-            <th>
-                Reszta
-            </th>
-            <th>
-                Rodzaj
-            </th>
-            <th>
-                Data faktury
-            </th>
-            <th>
-                Treść
-            </th>
-            <th>
-                Dopłata
-            </th>
-            <th>
-                Znaleziono
-            </th>
-        </tr>
-        @foreach($report as $entry)
+    <div>
+        <table class="table table-bordered">
+            {{ $report->links() }}
             <tr>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Numer listu
+                </th>
+                <th>
+                    Numer faktury do której dany LP został przydzielony
+                </th>
+                <th>
+                    Data nadania otrzymania
+                </th>
+                <th>
+                    Nr i d
+                </th>
+                <th>
+                    Rzeczywisty koszt transportu brutto
+                </th>
+                <th>
+                    Wartość pobrania
+                </th>
+                <th>
+                    plik
+                </th>
+                <th>
+                    Reszta
+                </th>
+                <th>
+                    Rodzaj
+                </th>
+                <th>
+                    Data faktury
+                </th>
+                <th>
+                    Treść
+                </th>
+                <th>
+                    Dopłata
+                </th>
+                <th>
+                    Znaleziono
+                </th>
+                <th>
+                    Komentarz
+                </th>
+            </tr>
+            @foreach($report as $entry)
                 @foreach(\App\Entities\ShippingPayInReport::getColumns() as $title)
                     <td>
                         {{ $entry->$title }}
                     </td>
                 @endforeach
-            </tr>
-        @endforeach
-    </table>
+
+                <livewire:shipment-pay-in-report-datatable :reportId="$entry->id" />
+
+            @endforeach
+        </table>
+    </div>
 
     <div style="display: block !important;" class="d-flex justify-content-center">
         {{ $report->links() }}
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+    @livewireScripts
 
     <script>
         $('document').ready(() => {
