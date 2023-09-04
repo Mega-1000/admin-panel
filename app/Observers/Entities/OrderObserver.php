9b037e2353c1c2b8ebd57800a3bb60792a801dff
Antoni Seba <antoniwoj@o2.pl>
2023-09-04 22:55:11 +0200

    Update order observer

--- app/Observers/Entities/OrderObserver.php
+++ app/Observers/Entities/OrderObserver.php
@@ -92,7 +92,7 @@ readonly class OrderObserver
             }
         }
 
-        $hasMissingDeliveryAddressLabel = $order->labels()->where('label_id', 75)->get();    //brak danych do dostawy
+        $hasMissingDeliveryAddressLabel = $order->labels()->where('label_id', 75)->get();
 
         if (count($hasMissingDeliveryAddressLabel) > 0) {
             if ($order->isDeliveryDataComplete()) {
@@ -105,6 +105,8 @@ readonly class OrderObserver
         $order->updateQuietly(['packages_values' => $this->orderPackagesCalculator->calculate($order)]);
 
         $fullCost = OrderPackagesCalculator::getFullCost($order);
+
+        $order->updateQuietly(['shipment_price_for_client_automatic' => $fullCost]);
 
         if ($order->shipment_price_for_client > $fullCost) {
             return;
