bd606e3e110297b5de8cb57af9ef3b0f35ee5c86
Antoni Seba <antoniwoj@o2.pl>
2023-09-24 13:24:14 +0200

    Import class fix

--- app/Http/Controllers/TimetablesController.php
+++ app/Http/Controllers/TimetablesController.php
@@ -8,6 +8,7 @@ use App\Repositories\TaskRepository;
 use App\Repositories\WarehouseRepository;
 use App\Services\TaskService;
 use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
