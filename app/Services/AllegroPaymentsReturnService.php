bde4071e1e36060c1c71775cce7ad2ba47a37f69
Antoni Seba <antoniwoj@o2.pl>
2023-09-24 10:19:17 +0200

    debugging

--- app/Services/AllegroPaymentsReturnService.php
+++ app/Services/AllegroPaymentsReturnService.php
@@ -31,7 +31,7 @@ readonly class AllegroPaymentsReturnService
         if (
             in_array(Label::RETURN_ALLEGRO_PAYMENTS, $orderLabels) &&
             !in_array(Label::ORDER_ITEMS_REDEEMED_LABEL, $orderLabels) &&
-            dd(!in_array(50, $orderLabels)) &&
+            !in_array(50, $orderLabels) &&
             false &&
             !self::checkIfOrderHasKwonPayment($order)
         ) {
