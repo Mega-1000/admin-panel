49f51448dc6749ebbdcba4ea91dea9c4f686a192
Antoni Seba <antoniwoj@o2.pl>
2023-09-28 14:43:12 +0200

    Hot fixes

--- app/Jobs/AllegroOrderSynchro.php
+++ app/Jobs/AllegroOrderSynchro.php
@@ -189,6 +189,10 @@ class AllegroOrderSynchro implements ShouldQueue
                 $invoiceAddress['address']['phoneNumber'] = $allegroOrder['buyer']['phoneNumber'];
                 $this->createOrUpdateCustomerAddress($customer, $allegroOrder['buyer']);
 
+                $invoiceAddress = array_key_exists('nip', $invoiceAddress)
+                    ? $invoiceAddress
+                    : $allegroOrder['delivery']['address'];
+
                 $this->createOrUpdateCustomerAddress($customer, $invoiceAddress, CustomerAddress::ADDRESS_TYPE_INVOICE);
 
                 if (array_key_exists('pickupPoint', $allegroOrder['delivery']) && $allegroOrder['delivery']['pickupPoint'] !== null) {
