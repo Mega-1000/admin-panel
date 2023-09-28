0b733aee969788bbfc20ae4269f386f07935cd23
Antoni Seba <antoniwoj@o2.pl>
2023-09-28 15:04:34 +0200

    Hot fixes

--- app/Http/Controllers/CreateTWSOOrdersDTO.php
+++ app/Http/Controllers/CreateTWSOOrdersDTO.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 readonly class CreateTWSOOrdersDTO
 {
     public function __construct(
-        protected ?string $warehousesSymbols = null,
         protected string $clientEmail,
         protected float $purchaseValue,
         protected ?string $consultantDescription,
@@ -13,7 +12,6 @@ readonly class CreateTWSOOrdersDTO
     public static function fromRequest(array $request): self
     {
         return new self(
-            warehousesSymbols: $request['warehousesSymbols'] ?? null,
             clientEmail:  $request['client_email'],
             purchaseValue: $request['purchase_value'],
             consultantDescription: $request['consultant_description'],
