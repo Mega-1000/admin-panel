ab23d7787201bfbfa20d46678e031ab4c2a61b5e
Antoni Seba <antoniwoj@o2.pl>
2023-10-05 15:24:59 +0200

    Big changes in product stock positions informations in products

--- resources/views/orders/single_item.blade.php
+++ resources/views/orders/single_item.blade.php
@@ -15,7 +15,7 @@
     @if(isset($showPosition) && $showPosition)
         @if(count($item->getPositions()))
             @foreach($item->getPositions() as $position)
-                <td class="wz__position">
+                <td class="wz__position" style="width: fit-content">
                     <div style="">
                         {{ $position->lane }} {{ $position->bookstand }} {{ $position->shelf }} {{ $position->position }} - {{ $position->position_quantity }}
                         {!! \App\Services\ProductPositioningService::renderPositioningViewHtml($position) !!}
