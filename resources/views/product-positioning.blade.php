36a075eb1f123f12d30eed952a7a6e480e9db116
Antoni Seba <antoniwoj@o2.pl>
2023-10-05 13:21:31 +0200

    Big changes in product stock positions informations in products

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -52,16 +52,15 @@
         <br>
         <br>
 
+    <div>
+        @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit(); $i >= 0; $i--)
+            <div style="font-weight: bold; font-size: larger">
+                --
+            </div>
+            <br>
+        @endfor
+    </div>
     <div style="display: flex">
-        <div>
-            @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit(); $i >= 0; $i--)
-                <div style="font-weight: bold; font-size: larger">
-                    --
-                </div>
-                <br>
-            @endfor
-        </div>
-
         <div style="display: grid; margin-left: 15px; grid-template-columns: repeat({{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package }}, 1fr); grid-gap: 10px;">
             @for($i = $productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer() - 1; $i >= 0; $i--)
                 <div style="padding: 10px; border: 1px black solid"></div>
