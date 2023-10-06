dd75231ca91373ebf83b81031cf810a65c6f380b
Antoni Seba <antoniwoj@o2.pl>
2023-10-06 12:04:08 +0200

    Big changes in product stock positions informations in products

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -88,12 +88,10 @@
     {{ $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() }} ,
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package }} *
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package != 0 ? floor($productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer() / $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package) : 0 }},
-    // reszta z dzielenia
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package != 0 ? $productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer() % $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package : 0}}
 
 
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width }} *
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width != 0 ? floor(($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() * $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width + $productPositioningDTO->getQuantityOfTradeItemsInStartedRowInStartedLayerInStartedGlobalUnit()) / $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width ) : 0 }},
-    // reszta z dzielenia
     {{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width != 0 ? $productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() * $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width + $productPositioningDTO->getQuantityOfTradeItemsInStartedRowInStartedLayerInStartedGlobalUnit() % $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width : 0 }}
 {{--</div>--}}
