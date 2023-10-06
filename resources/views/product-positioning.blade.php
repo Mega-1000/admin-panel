ece0ecc0d61d26f0f0cc1a4f6f67834f1982ccb7
Antoni Seba <antoniwoj@o2.pl>
2023-10-06 13:39:25 +0200

    Big changes in product stock positions informations in products

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -52,17 +52,18 @@
         <br>
         <br>
 
+    <div>
         <div>
-            <div>
-                @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
-                    <div style="font-weight: bold; font-size: larger">
-                        --
-                    </div>
-                    <br>
-                @endfor
-            </div>
+            @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
+                <div style="font-weight: bold; font-size: larger">
+                    --
+                </div>
+                <br>
+            @endfor
+        </div>
 
-            <div style="display: flex; margin-left: 20px;">
+        <div style="display: flex" style="margin-left: 20px;">
+            <div style="display: grid; margin-left: 15px; grid-template-columns: repeat(3, 1fr); grid-gap: 10px;">
                 @for($i = $productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer() - 1; $i >= 0; $i--)
                     <div style="padding: 10px; border: 1px black solid"></div>
                 @endfor
@@ -71,13 +72,16 @@
                     <div style="padding: 10px; border: 1px black solid; border-radius: 100%"></div>
                 @endif
             </div>
-            {{($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() * $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width + $productPositioningDTO->getQuantityOfTradeItemsInStartedRowInStartedLayerInStartedGlobalUnit()) - 1}}
+        </div>
+        {{($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() * $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width + $productPositioningDTO->getQuantityOfTradeItemsInStartedRowInStartedLayerInStartedGlobalUnit()) - 1}}
 
-            <div style="display: flex;">
+        <div style="display: flex">
+            <div style="display: grid; margin-left: 15px; grid-template-columns: repeat({{ $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width }}, 1fr); grid-gap: 10px;">
                 @for($i = ($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() * $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width + $productPositioningDTO->getQuantityOfTradeItemsInStartedRowInStartedLayerInStartedGlobalUnit()) - 1; $i >= 0; $i--)
                     <div style="padding: 10px; border: 1px black solid"></div>
                 @endfor
             </div>
         </div>
+    </div>
 
 </div>
