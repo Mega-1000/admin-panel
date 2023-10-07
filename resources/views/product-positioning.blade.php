31b1f54619da98193ba63d0273137dd188ce47b7
Antoni Seba <antoniwoj@o2.pl>
2023-10-07 10:24:50 +0200

    Temp fix of email settings ediditon

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -24,14 +24,18 @@
                 @while($maxNumberOfSquares > 0)
                     <tr>
                         @for ($j = 0; $j <= $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package - 1; $j++)
-                            @if($maxNumberOfSquares <= 0)
+                            @if($maxNumberOfSquares == 0)
+                                <td style="padding: 10px; border: 1px black solid; border-radius: 100%"></td>
+                                @break
+                            @endif
+
+                            @if($maxNumberOfSquares < 0)
                                 @break
                             @endif
                             <td style="padding: 10px; border: 1px black solid;"></td>
 
                             @php($maxNumberOfSquares--)
                         @endfor
-                        <td style="padding: 10px; border: 1px black solid; border-radius: 100%"></td>
                     </tr>
                 @endwhile
             </tbody>
