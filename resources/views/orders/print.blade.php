d1ca37146165e50915a1d5cd7862a710238af87f
Antoni Seba <antoniwoj@o2.pl>
2023-10-07 11:37:08 +0200

    Temp fix of email settings ediditon

--- resources/views/orders/print.blade.php
+++ resources/views/orders/print.blade.php
@@ -11,7 +11,7 @@
             padding: 10px;
             background: transparent;
             position: relative;
-            font: 9px Arial, Helvetica, sans-serif;
+            font-family: DejaVu Sans;
         }
 
         h2 {
