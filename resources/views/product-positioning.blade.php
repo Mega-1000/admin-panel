9372c952aab18c99a2b73b4e754753c4b3564010
Antoni Seba <antoniwoj@o2.pl>
2023-10-07 12:58:50 +0200

    Temp fix of email settings ediditon

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -72,16 +72,14 @@
     <tbody>
     <tr>
         <td>
-            @if($productPositioningDTO->getQuantityOfCompleteLayersOfTradeItemsInLargestUnitInGlobalUnit() != 0)
-                <div style="align-self: flex-start;">
-                    @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
-                        <div style="font-weight: bold; font-size: larger">
-                            --
-                        </div>
-                        <br>
-                    @endfor
-                </div>
-            @endif
+            <div style="align-self: flex-start;">
+                @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
+                    <div style="font-weight: bold; font-size: larger">
+                        --
+                    </div>
+                    <br>
+                @endfor
+            </div>
 
             @php
                 $maxNumberOfSquares =
@@ -93,23 +91,7 @@
             @endphp
         </td>
         <td>
-            <table style="align-self: flex-start;">
-                <thead></thead>
-                <tbody>
-                @while($maxNumberOfSquares > 0)
-                    <tr>
-                        @for ($j = 0; $j <= $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width; $j++)
-                            <td style="padding: 10px; border: 1px black solid;"></td>
-                            @php
-                                $maxNumberOfSquares--
-                            @endphp
-                        @endfor
-                    </tr>
-                @endwhile
-                </tbody>
-            </table>
         </td>
     </tr>
     </tbody>
 </table>
-@endif
