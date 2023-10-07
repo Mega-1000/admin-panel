7b0dba423edd78474122f051645de4862b76bdc0
Antoni Seba <antoniwoj@o2.pl>
2023-10-07 11:30:47 +0200

    Temp fix of email settings ediditon

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -1,44 +1,53 @@
- <div style="display: flex; flex-direction: row;">
+<div style="display: flex; flex-direction: row;">
 
-        @php
-            $maxNumberOfSquares = $productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer();
-            $borderRadius = $maxNumberOfSquares != 0 ? false : true;
-        @endphp
+     <table>
+         <tbody>
+             <tr>
 
-     @if($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() != 0)
-         <div style="align-self: flex-start; display: inline">
-             @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
-                 <div style="font-weight: bold; font-size: larger">
-                     --
-                 </div>
-                 <br>
-             @endfor
-         </div>
-     @endif
-        <table style="align-self: flex-start; display: inline">
-            <thead></thead>
-            <tbody>
-            @while($maxNumberOfSquares > 0)
-                <tr>
-                    @for ($j = 0; $j <= $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package - 1; $j++)
-                        @if($maxNumberOfSquares <= 0)
+                @if($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() != 0)
+                    <div style="align-self: flex-start;">
+                        @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
+                            <div style="font-weight: bold; font-size: larger">
+                                --
+                            </div>
+                            <br>
+                        @endfor
+                    </div>
+                @endif
+
+                @php
+                    $maxNumberOfSquares = $productPositioningDTO->getQuantityOfCompleteGlobalUnitsInStartedLayer();
+                    $borderRadius = $maxNumberOfSquares != 0 ? false : true;
+                @endphp
+             </tr>
+             <tr>
+                <table style="align-self: flex-start;">
+                    <thead></thead>
+                    <tbody>
+                    @while($maxNumberOfSquares > 0)
+                        <tr>
+                            @for ($j = 0; $j <= $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package - 1; $j++)
+                                @if($maxNumberOfSquares <= 0)
+                                    <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
+                                    @php($borderRadius = true)
+                                    @break
+                                @endif
+
+                                <td style="padding: 10px; border: 1px black solid;"></td>
+                                @php($maxNumberOfSquares--)
+                            @endfor
+                        </tr>
+                    @endwhile
+                    @if(!$borderRadius)
+                        <tr>
                             <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
-                            @php($borderRadius = true)
-                            @break
-                        @endif
-
-                        <td style="padding: 10px; border: 1px black solid;"></td>
-                        @php($maxNumberOfSquares--)
-                    @endfor
-                </tr>
-            @endwhile
-            @if(!$borderRadius)
-                <tr>
-                    <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
-                </tr>
-            @endif
-            </tbody>
-        </table>
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
+         </tr>
+         </tbody>
+     </table>
 
         <table style="margin-top: 10px; align-self: flex-start;">
             <tbody>
