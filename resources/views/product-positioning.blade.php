f04354cc645bc68bb616cb95e0b067c5da689b1a
Antoni Seba <antoniwoj@o2.pl>
2023-10-07 10:57:04 +0200

    Temp fix of email settings ediditon

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -1,7 +1,7 @@
 <div style="width: 1000px;">
     <div style="display: flex; flex-direction: row;">
         @if($productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit() != 0)
-            <div style="flex: none;">
+            <div style="align-self: flex-start;">
                 @for($i = $productPositioningDTO->getQuantityOfCompleteLayersOfGlobalUnitsInGlobalUnit() - 1; $i >= 0; $i--)
                     <div style="font-weight: bold; font-size: larger">
                         --
@@ -16,37 +16,37 @@
             $borderRadius = $maxNumberOfSquares != 0 ? false : true;
         @endphp
 
-        <table style="flex: none;">
+        <table style="align-self: flex-start;">
             <thead></thead>
             <tbody>
             @while($maxNumberOfSquares > 0)
                 <tr>
                     @for ($j = 0; $j <= $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_width_in_global_package - 1; $j++)
                         @if($maxNumberOfSquares <= 0)
-                            <td style="padding: 10px; border: 1px black solid; border-radius: 100%; flex: none;"></td>
+                            <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
                             @php($borderRadius = true)
                             @break
                         @endif
 
-                        <td style="padding: 10px; border: 1px black solid; flex: none;"></td>
+                        <td style="padding: 10px; border: 1px black solid;"></td>
                         @php($maxNumberOfSquares--)
                     @endfor
                 </tr>
             @endwhile
             @if(!$borderRadius)
                 <tr>
-                    <td style="padding: 10px; border: 1px black solid; border-radius: 100%; flex: none;"></td>
+                    <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
                 </tr>
             @endif
             </tbody>
         </table>
 
-        <table style="margin-top: 10px; flex: none;">
+        <table style="margin-top: 10px; align-self: flex-start;">
             <tbody>
             @for ($i = 0; $i < $productPositioningDTO->getQuantityOfCompleteRowsOfTradeItemsInStartedLayerInStartedGlobalUnit(); $i++)
                 <tr>
                     @for ($j = 0; $j < $productPositioningDTO->getProduct()->packing->number_of_trade_units_in_package_width; $j++)
-                        <td style="padding: 10px; border: 1px black solid; flex: none;"></td>
+                        <td style="padding: 10px; border: 1px black solid;"></td>
                     @endfor
                 </tr>
             @endfor
