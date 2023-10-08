4be9011c0816ef23ee6e65a0bd646ce8bf2fcee3
Antoni Seba <antoniwoj@o2.pl>
2023-10-08 21:43:00 +0200

    Try to fix product stock edition

--- app/Http/Controllers/ProductStocksController.php
+++ app/Http/Controllers/ProductStocksController.php
@@ -72,7 +72,7 @@ class ProductStocksController extends Controller
     public function edit(int $id): Application|Factory|\Illuminate\Contracts\View\View
     {
         $productStocks = ProductStock::where('product_id', $id)->first();
-        $similarProducts = $this->productService->checkForSimilarProducts($id);
+        $similarProducts = collect();
         $visibilitiesLogs = ColumnVisibility::getVisibilities(ColumnVisibility::getModuleId('product_stock_logs'));
 
         foreach ($visibilitiesLogs as $row) {
