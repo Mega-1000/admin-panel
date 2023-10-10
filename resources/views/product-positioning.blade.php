57828c34fa8ab370b491f48ac4ab876e54ddae97
Antoni Seba <antoniwoj@o2.pl>
2023-10-10 17:07:21 +0200

    Comment out unnescessary thinks

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -41,53 +41,29 @@
     </tbody>
 </table>
 
-<!--         public float $IJHWOZ,
-        public float $IJHWOG,
-        public float $IOHWOP1,
-        public float $IJHNKWWOZ,
-        public float $IJZNKWWOG,
-        public float $IWJNWPWOZ,
-        public float $IPHWOZPD,
-        public float $IPHWOZPS,
-        public float $IJZPDWOG,
-        public float $IJZPSWOG,
-        public float $IOHKSPWZIP1NPWW1WOH,
-        public float $IKWJZWOG,
-        public float $IPJZNRWWOG,
-        public float $IJHWROZNRWZWJG,
-        public float $IKROZPDWRWOG,
-        public float $IKOZWRRNRWWOG,
-        public float $IPWJHWROZWOG,
-        public float $IKRPDOHNRWWRIZBRWWOG,
-        public float $IOHWRRWROZWRWWOG,
-        public float $IKWW1WROZWRWWROG,
-        public float $IKOP1WRWWW1WOG,
-        public float $IOHWROP1WRWWOG,
-        public bool $isZero = false,
-        public Product $product, -->
+{{--IJHWOZ: {{ $productPositioningDTO->IJHWOZ }}<br>--}}
+{{--IJHWOG: {{ $productPositioningDTO->IJHWOG }}<br>--}}
+{{--IOHWOP1: {{ $productPositioningDTO->IOHWOP1 }}<br>--}}
+{{--IJHNKWWOZ: {{ $productPositioningDTO->IJHNKWWOZ }}<br>--}}
+{{--IJZNKWWOG: {{ $productPositioningDTO->IJZNKWWOG }}<br>--}}
 
-IJHWOZ: {{ $productPositioningDTO->IJHWOZ }}<br>
-IJHWOG: {{ $productPositioningDTO->IJHWOG }}<br>
-IOHWOP1: {{ $productPositioningDTO->IOHWOP1 }}<br>
-IJHNKWWOZ: {{ $productPositioningDTO->IJHNKWWOZ }}<br>
-IJZNKWWOG: {{ $productPositioningDTO->IJZNKWWOG }}<br>
+{{--IWJNWPWOZ: {{ $productPositioningDTO->IWJNWPWOZ }}<br>--}}
+{{--IPHWOZPD: {{ $productPositioningDTO->IPHWOZPD }}<br>--}}
+{{--IPHWOZPS: {{ $productPositioningDTO->IPHWOZPS }}<br>--}}
+{{--IJZPDWOG: {{ $productPositioningDTO->IJZPDWOG }}<br>--}}
+{{--IJZPSWOG: {{ $productPositioningDTO->IJZPSWOG }}<br>--}}
 
-IWJNWPWOZ: {{ $productPositioningDTO->IWJNWPWOZ }}<br>
-IPHWOZPD: {{ $productPositioningDTO->IPHWOZPD }}<br>
-IPHWOZPS: {{ $productPositioningDTO->IPHWOZPS }}<br>
-IJZPDWOG: {{ $productPositioningDTO->IJZPDWOG }}<br>
-IJZPSWOG: {{ $productPositioningDTO->IJZPSWOG }}<br>
+{{--IOHKSPWZIP1NPWW1WOH: {{ $productPositioningDTO->IOHKSPWZIP1NPWW1WOH }}<br>--}}
+{{--IKWJZWOG: {{ $productPositioningDTO->IKWJZWOG }}<br>--}}
+{{--IPJZNRWWOG: {{ $productPositioningDTO->IPJZNRWWOG }}<br>--}}
+{{--IJHWROZNRWZWJG: {{ $productPositioningDTO->IJHWROZNRWZWJG }}<br>--}}
+{{--IKROZPDWRWOG: {{ $productPositioningDTO->IKROZPDWRWOG }}<br>--}}
 
-IOHKSPWZIP1NPWW1WOH: {{ $productPositioningDTO->IOHKSPWZIP1NPWW1WOH }}<br>
-IKWJZWOG: {{ $productPositioningDTO->IKWJZWOG }}<br>
-IPJZNRWWOG: {{ $productPositioningDTO->IPJZNRWWOG }}<br>
-IJHWROZNRWZWJG: {{ $productPositioningDTO->IJHWROZNRWZWJG }}<br>
-IKROZPDWRWOG: {{ $productPositioningDTO->IKROZPDWRWOG }}<br>
-
-IKOZWRRNRWWOG: {{ $productPositioningDTO->IKOZWRRNRWWOG }}<br>
-IPWJHWROZWOG: {{ $productPositioningDTO->IPWJHWROZWOG }}<br>
-IKRPDOHNRWWRIZBRWWOG: {{ $productPositioningDTO->IKRPDOHNRWWRIZBRWWOG }}<br>
-IOHWRRWROZWRWWOG: {{ $productPositioningDTO->IOHWRRWROZWRWWOG }}<br>
-IKWW1WROZWRWWROG: {{ $productPositioningDTO->IKWW1WROZWRWWROG }}<br>
+{{--IKOZWRRNRWWOG: {{ $productPositioningDTO->IKOZWRRNRWWOG }}<br>--}}
+{{--IPWJHWROZWOG: {{ $productPositioningDTO->IPWJHWROZWOG }}<br>--}}
+{{--IKRPDOHNRWWRIZBRWWOG: {{ $productPositioningDTO->IKRPDOHNRWWRIZBRWWOG }}<br>--}}
+{{--IOHWRRWROZWRWWOG: {{ $productPositioningDTO->IOHWRRWROZWRWWOG }}<br>--}}
+{{--IKWW1WROZWRWWROG: {{ $productPositioningDTO->IKWW1WROZWRWWROG }}<br>--}}
 
 
+{{--IKOZWRRNRWWOG--}}
