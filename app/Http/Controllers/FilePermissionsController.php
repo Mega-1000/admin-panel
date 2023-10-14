e6cae02804b5badaf531d2ffd7831d0cde2f6437
Antoni Seba <antoniwoj@o2.pl>
2023-10-14 10:34:20 +0200

    Handle errors

--- app/Http/Controllers/FilePermissionsController.php
+++ app/Http/Controllers/FilePermissionsController.php
@@ -18,6 +18,13 @@ class FilePermissionsController extends Controller
         $process = new Process(['chmod', '-R', '777', '/var/www/admin-mega/']);
         $process->run();
 
+        if (!$process->isSuccessful()) {
+            return redirect()->back()->with([
+                'message' => 'Failed to change file permissions: ' . $process->getErrorOutput(),
+                'alert-type' => 'error',
+            ]);
+        }
+
         return redirect()->back()->with([
             'message' => 'Ponownie ustawiono uprawnienia do plików.',
             'alert-type' => 'success',
