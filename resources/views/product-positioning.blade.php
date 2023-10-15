23ad22f7d988689193745eec8d18e55a84119ec5
Antoni Seba <antoniwoj@o2.pl>
2023-10-15 15:12:35 +0200

    Change product packing to product packet

--- resources/views/product-positioning.blade.php
+++ resources/views/product-positioning.blade.php
@@ -73,12 +73,26 @@
         </tbody>
     </table>
 
-    @php
-        $maxNumberOfSquares = ($productPositioningDTO->IKRPDOHNRWWRIZBRWWOG * $productPositioningDTO->IPHWOZPS + $productPositioningDTO->IOHWRRWROZWRWWOG);
-        $borderRadius = false;
-    @endphp
 
+    <table style="align-self: flex-start;">
+        <thead></thead>
+        <tbody>
+        @while($maxNumberOfSquares > 0)
+            <tr>
+                @for ($j = 0; $j <= $productPositioningDTO->IPHWOZPS - 1; $j++)
+                    @if($maxNumberOfSquares <= 0)
+                        <td style="padding: 10px; border: 1px black solid; border-radius: 100%;"></td>
+                        @php($borderRadius = true)
+                        @break
+                    @endif
 
+                    <td style="padding: 10px; border: 1px black solid;"></td>
+                    @php($maxNumberOfSquares--)
+                @endfor
+            </tr>
+        @endwhile
+        </tbody>
+    </table>
 </div>
 
 IJHWOZ: {{ $productPositioningDTO->IJHWOZ }}<br>
