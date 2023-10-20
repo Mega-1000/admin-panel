65ec3de8de35d786e07bf05046d85fc21e439b0f
Antoni Seba <antoniwoj@o2.pl>
2023-10-20 13:37:13 +0200

    Low order quantity alert fixes

--- app/Services/LowOrderQuantityAlertService.php
+++ app/Services/LowOrderQuantityAlertService.php
@@ -34,7 +34,6 @@ class LowOrderQuantityAlertService
 //                }
 
                 $columnName = $alert->column_name;
-                dd($item->product->$columnName, explode($alert->item_names, ','));
                 if (
                     in_array(
                         $item->product->$columnName,
