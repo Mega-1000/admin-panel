c5e126ea4f6ae5a2f09da7218ae580c3af27bc63
Antoni Seba <antoniwoj@o2.pl>
2023-10-27 17:50:30 +0200

    Add 249 column to be filled

--- app/Jobs/ImportCsvFileJob.php
+++ app/Jobs/ImportCsvFileJob.php
@@ -597,6 +597,7 @@ class ImportCsvFileJob implements ShouldQueue
             'number_of_trade_units_in_length_in_global_package' => $line[68],
             'number_of_trade_units_in_width_in_global_package' => $line[69],
             'number_of_trade_items_in_p1' => $line[70],
+            'allegro_gross_selling_price_after_all_additional_costs' => $line[249],
         ];
 
         foreach ($array as $key => $value) {
