f64c8695632c2682eb9b690bf71e1f814708a986
Antoni Seba <antoniwoj@o2.pl>
2023-10-28 08:21:42 +0200

    make mails dispatched with delay

--- app/Services/ProductPacketService.php
+++ app/Services/ProductPacketService.php
@@ -78,17 +78,17 @@ class ProductPacketService
 //                }
 //                }
 
-                if (str_contains($dataArray['price'], '(-)')) {
-                    $parts = explode('(-)', $dataArray['price']);
-                    $priceAdjustment = isset($parts[1]) ? (float)$parts[1] : 0;
-                    $productToAddArray['gross_selling_price_commercial_unit'] -= $priceAdjustment;
-                }
-
-                if (str_contains($dataArray['price'], '(+)')) {
-                    $parts = explode('(+)', $dataArray['price']);
-                    $priceAdjustment = isset($parts[1]) ? (float)$parts[1] : 0;
-                    $productToAddArray['gross_selling_price_commercial_unit'] += $priceAdjustment;
-                }
+//                if (str_contains($dataArray['price'], '(-)')) {
+//                    $parts = explode('(-)', $dataArray['price']);
+//                    $priceAdjustment = isset($parts[1]) ? (float)$parts[1] : 0;
+//                    $productToAddArray['gross_selling_price_commercial_unit'] -= $priceAdjustment;
+//                }
+//
+//                if (str_contains($dataArray['price'], '(+)')) {
+//                    $parts = explode('(+)', $dataArray['price']);
+//                    $priceAdjustment = isset($parts[1]) ? (float)$parts[1] : 0;
+//                    $productToAddArray['gross_selling_price_commercial_unit'] += $priceAdjustment;
+//                }
 
                 $productToAddArray['amount'] = min(
                     $dataArray['quantity'] * $dataArray['quantity_of_packet_in_order'],
