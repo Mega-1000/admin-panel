2d39bdfc4558b3e948c2fdaf7bcc1458de699129
Antoni Seba <antoniwoj@o2.pl>
2023-11-09 06:56:36 +0100

    DD

--- app/Services/FormActionService.php
+++ app/Services/FormActionService.php
@@ -65,11 +65,11 @@ class FormActionService
         $allegroPaymentService = app(AllegroPaymentService::class);
 
         $allegroPaymentId = $order->allegro_payment_id;
-        dd($allegroPaymentId);
         $allegroOrder = $allegroOrderService->getOrderByPaymentId($allegroPaymentId);
 
         $returnsByAllegroId = AllegroReturnPaymentHelper::createReturnsByAllegroId($allegroOrder, []);
 
+        dd($returnsByAllegroId, $allegroOrder, $allegroPaymentId);
         list($lineItemsForPaymentRefund, $lineItemsForCommissionRefund) = AllegroReturnPaymentHelper::createLineItemsFromReturnsByAllegroId($returnsByAllegroId);
 
         $data = new AllegroReturnDTO(
