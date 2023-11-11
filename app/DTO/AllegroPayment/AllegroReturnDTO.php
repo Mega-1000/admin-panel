bdfe56410c7b86bf702ca2ddcdb5b70116855ef8
Antoni Seba <antoniwoj@o2.pl>
2023-11-11 06:40:49 +0100

    Add delivery value to refund

--- app/DTO/AllegroPayment/AllegroReturnDTO.php
+++ app/DTO/AllegroPayment/AllegroReturnDTO.php
@@ -2,6 +2,8 @@
 
 namespace App\DTO\AllegroPayment;
 
+use App\Entities\Order;
+
 class AllegroReturnDTO
 {
     /**
@@ -20,6 +22,9 @@ class AllegroReturnDTO
         if (empty($this->reason)) {
             $this->reason = 'zwrot';
         }
+
+        $order = Order::where('allegro_form_id', $this->paymentId)->first();
+
         return [
             'payment' => [
                 'id' => $this->paymentId
@@ -28,7 +33,13 @@ class AllegroReturnDTO
             'lineItems' => array_map(function (AllegroReturnItemDTO $item) {
                 return $item->toAllegroRefundArray();
             }, $this->lineItems),
-            'message' => 'zwrot'
+            'message' => 'zwrot',
+            'delivery' => [
+                'value' => [
+                    'amount' => $order->shipment_price_for_client,
+                    'currency' => 'PLN'
+                ],
+            ],
         ];
     }
 }
