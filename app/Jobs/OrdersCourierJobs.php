e343db8ab54fe4448d07b6595f616f7f6331511a
Antoni Seba <antoniwoj@o2.pl>
2023-11-12 08:55:13 +0100

    Update form actions

--- app/Jobs/OrdersCourierJobs.php
+++ app/Jobs/OrdersCourierJobs.php
@@ -234,7 +234,7 @@ class OrdersCourierJobs extends Job implements ShouldQueue
                 'deliveryMethodId' => json_decode($this->data['allegro_delivery_method_id'])[0],
             ]
         ];
-//        dd($this->data);
+        dd($this->data);
 
         $allegroApiService = new AllegroApiService();
         $result = $allegroApiService->request('POST', 'https://api.allegro.pl/shipment-management/shipments/create-commands', $data);
