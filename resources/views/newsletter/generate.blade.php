a24922281287c5ad03a349701d93de99e00b70e5
Antoni Seba <antoniwoj@o2.pl>
2023-11-12 11:29:27 +0100

    Newsletter changes

--- resources/views/newsletter/generate.blade.php
+++ resources/views/newsletter/generate.blade.php
@@ -6,16 +6,14 @@
         <div class="p-4 border rounded-lg shadow-lg">
             <img src="{{ $product?->url_for_website }}" class="w-full h-48 object-cover rounded-lg">
             <div class="mt-4">
-                <h3 class="text-lg sm:text-xl md:text-2xl font-semibold">{{ $product?->name }}</h3>
-                <p class="text-gray-600">{{ $product?->description }}</p>
-                <p class="text-gray-600">{{ $product?->symbol }}</p>
-
-                <br>
-                <br>
-
                 <div class="text-2xl">
                     Cena: {{ $product->price->allegro_gross_selling_price_after_all_additional_costs }} zł
                 </div>
+                <h3 class="text-lg sm:text-xl md:text-2xl font-semibold">{{ $product?->name }}</h3>
+
+                <p class="text-gray-600">{{ $product?->description }}</p>
+
+                <p class="text-gray-600">{{ $product?->symbol }}</p>
 
                 <button class="px-4 py-2 bg-blue-500 rounded text-white mt-4">
                     Zobacz aukcję
