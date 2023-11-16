536af3df088670bd75ed178da207e5dc9b73f6aa
Antoni Seba <antoniwoj@o2.pl>
2023-11-16 07:33:55 +0100

    Add labels attachment

--- app/Entities/OrderPackage.php
+++ app/Entities/OrderPackage.php
@@ -125,7 +125,7 @@ class OrderPackage extends Model implements Transformable
     public function getPathToSticker(): string
     {
         $paths = [
-            'INPOST' => "/inpost/stickers/sticker$this->letter_number.pdf",
+            'INPOST' => "/storage/inpost/stickers/sticker$this->letter_number.pdf",
             'ALLEGRO-INPOST' => "/inpost/stickers/sticker$this->letter_number.pdf",
             'DPD' => "/dpd/stickers/sticker$this->letter_number.pdf",
             'POCZTEX' => "/storage/pocztex/protocols/protocol$this->sending_number.pdf",
