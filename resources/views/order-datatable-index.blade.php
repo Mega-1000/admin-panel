b4d78dd553b61ac31178f6e4a8759e447e5c8005
Antoni Seba <antoniwoj@o2.pl>
2023-12-05 14:21:33 +0100

    Add invoice generation in new grid

--- resources/views/order-datatable-index.blade.php
+++ resources/views/order-datatable-index.blade.php
@@ -30,6 +30,28 @@
 @section('table')
     <livewire:order-datatable.order-datatable-index />
 
+    <div class="modal fade" tabindex="-1" id="manual_label_selection_to_add_modal" role="dialog">
+        <div class="modal-dialog" style="width: 90%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager::generic.close') }}"><span
+                            aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Etykiety do dodania</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="labels_to_add_after_removal_modal">@lang('labels.form.labels_to_add_after_removal')</label>
+                        <select class="form-control text-uppercase" id="labels_to_add_after_removal_modal" name="labels_to_add_after_removal_modal[]" size="6">
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Anuluj</button>
+                    <button type="button" class="btn btn-success" id="labels_to_add_after_removal_modal_ok" data-dismiss="modal">Zatwierdź</button>
+                </div>
+            </div>
+        </div>
+    </div>
     @livewireScripts
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -208,13 +230,13 @@
             });
         }
         const labelActionMapping = {
-            45: () => showSelectWarehouseTemplate(),
+            45: showSelectWarehouseTemplate,
         }
 
         const removeLabel = (labelId, orderId) => {
             const action = labelActionMapping[labelId];
             if (action) {
-                action();
+                action($('#manual_label_selection_to_add_modal').modal('show'), orderId);
                 return;
             }
 
