0b6e341f44b5f43c56c2d290d1d294029018a07d
Antoni Seba <antoniwoj@o2.pl>
2023-12-11 18:39:41 +0100

    Fix data injected to view

--- resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
@@ -1,6 +1,8 @@
-@foreach($order['chat']['messages'] as $message)
-    <hr>
-    {{ $message['message'] }}
-    <br>
-    <hr>
-@endforeach
+@if(array_key_exists('chat', $order) && array_key_exists('messages', $order['chat']))
+    @foreach($order['chat']['messages'] as $message)
+        <hr>
+        {{ $message['message'] }}
+        <br>
+        <hr>
+    @endforeach
+@endif
