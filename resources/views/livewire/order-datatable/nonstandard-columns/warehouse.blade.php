8ee8319d2aabaf3bbe61d09a87de3ca51cb00834
Antoni Seba <antoniwoj@o2.pl>
2023-12-11 18:25:22 +0100

    Fix data injected to view

--- resources/views/livewire/order-datatable/nonstandard-columns/warehouse.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/warehouse.blade.php
@@ -1,2 +1,4 @@
 
-{{ $data['warehouse']['symbol'] ? '<a class="warehouse-symbol" href="/admin/warehouses/' + $data['warehouse']['symbol'] + '/editBySymbol">' + $data['warehouse']['symbol'] + '</a>' : '' }}
+@if($data['warehouse']['symbol'])
+    {{ $data['warehouse']['symbol'] ? '<a class="warehouse-symbol" href="/admin/warehouses/' + $data['warehouse']['symbol'] + '/editBySymbol">' + $data['warehouse']['symbol'] + '</a>' : '' }}
+@endif
