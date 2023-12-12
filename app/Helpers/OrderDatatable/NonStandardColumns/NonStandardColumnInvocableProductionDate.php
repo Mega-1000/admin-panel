4818474693f4c590ba35c55b6d4fab93b438590a
Antoni Seba <antoniwoj@o2.pl>
2023-12-12 20:27:10 +0100

    New col in main new grid

--- app/Helpers/OrderDatatable/NonStandardColumns/NonStandardColumnInvocableProductionDate.php
+++ app/Helpers/OrderDatatable/NonStandardColumns/NonStandardColumnInvocableProductionDate.php
@@ -4,7 +4,7 @@ namespace App\Helpers\OrderDatatable\NonStandardColumns;
 
 class NonStandardColumnInvocableProductionDate extends AbstractNonStandardColumnInvocable
 {
-    protected string $view = 'orderdatatable.nonstandard-columns.production-date';
+    protected string $view = 'order-datatable.nonstandard-columns.production-date';
 
     protected function getData(array $order): array
     {
