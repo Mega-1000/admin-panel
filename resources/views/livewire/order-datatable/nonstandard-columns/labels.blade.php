1aca28f30f93298e11169b7b46cb3f9550f77b7e
Antoni Seba <antoniwoj@o2.pl>
2023-12-13 15:21:09 +0100

    Add label name popup

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -49,28 +49,3 @@
         display: block;
     }
 </style>
-
-
-
-<div>
-    @if($labelGroupName === 'info dodatkowe')
-        <button onclick="uploadFile({{ $order['id'] }})">
-            Dodaj
-        </button>
-
-        @foreach($order['files'] as $file)
-            <a href="{{ route('orders.getFile', ['id' => $order['id'], 'file_id' => $file['hash']]) }}" target="_blank">
-                {{ $file['file_name'] }}
-            </a>
-        @endforeach
-    @endif
-
-    @foreach(!empty($order['labels']) && count($order['labels']) > 0 ? array_filter($order['labels'], function ($label) use($labelGroupName) { return $label['label_group']['name'] === $labelGroupName; }) : [] as $label)
-        <span
-            style="cursor: pointer"
-            onclick="removeLabel({{ $label['id'] }}, {{ $order['id'] }})"
-        >
-        <i class="{{ $label['icon_name'] }}" style="font-size: 24px; background-color: {{ $label['color'] }}; padding: 5px"></i>
-    </span>
-    @endforeach
-</div>
