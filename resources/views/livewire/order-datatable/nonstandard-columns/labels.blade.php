8d1387f81715e065bbc161e9af84b57c0f639202
Antoni Seba <antoniwoj@o2.pl>
2023-12-13 15:20:05 +0100

    Add label name popup

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -13,7 +13,7 @@
 
     <div class="label-container">
         @foreach(!empty($order['labels']) && count($order['labels']) > 0 ? array_filter($order['labels'], function ($label) use($labelGroupName) { return $label['label_group']['name'] === $labelGroupName; }) : [] as $label)
-            <span class="label-wrapper" style="cursor: pointer" onmouseover="showLabelName(this, '{{ $label['name'] }}')" onmouseout="hideLabelName(this)">
+            <span onclick="removeLabel({{ $label['id'] }}, {{ $order['id'] }})" class="label-wrapper" style="cursor: pointer" onmouseover="showLabelName(this, '{{ $label['name'] }}')" onmouseout="hideLabelName(this)">
                 <i class="{{ $label['icon_name'] }}" style="font-size: 24px; background-color: {{ $label['color'] }}; padding: 5px"></i>
                 <div class="label-popup">{{ $label['name'] }}</div>
             </span>
@@ -49,3 +49,28 @@
         display: block;
     }
 </style>
+
+
+
+<div>
+    @if($labelGroupName === 'info dodatkowe')
+        <button onclick="uploadFile({{ $order['id'] }})">
+            Dodaj
+        </button>
+
+        @foreach($order['files'] as $file)
+            <a href="{{ route('orders.getFile', ['id' => $order['id'], 'file_id' => $file['hash']]) }}" target="_blank">
+                {{ $file['file_name'] }}
+            </a>
+        @endforeach
+    @endif
+
+    @foreach(!empty($order['labels']) && count($order['labels']) > 0 ? array_filter($order['labels'], function ($label) use($labelGroupName) { return $label['label_group']['name'] === $labelGroupName; }) : [] as $label)
+        <span
+            style="cursor: pointer"
+            onclick="removeLabel({{ $label['id'] }}, {{ $order['id'] }})"
+        >
+        <i class="{{ $label['icon_name'] }}" style="font-size: 24px; background-color: {{ $label['color'] }}; padding: 5px"></i>
+    </span>
+    @endforeach
+</div>
