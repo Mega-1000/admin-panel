15703a2c9b89d8408952d8104a50134cf662b3a6
Antoni Seba <antoniwoj@o2.pl>
2023-12-23 10:01:20 +0100

    Add recalculation of labels based on time period

--- app/Http/Controllers/RecalculateLabelsInOrdersBasedOnPeriod.php
+++ app/Http/Controllers/RecalculateLabelsInOrdersBasedOnPeriod.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Entities\Order;
 use App\Http\Requests\RecalculateLabelsInOrdersBasedOnPeriodRequest;
 use App\Services\OrderPaymentLabelsService;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 
 class RecalculateLabelsInOrdersBasedOnPeriod extends Controller
@@ -15,7 +16,7 @@ class RecalculateLabelsInOrdersBasedOnPeriod extends Controller
 
     public function __invoke(RecalculateLabelsInOrdersBasedOnPeriodRequest $request): RedirectResponse
     {
-        $orders = Order::query()->whereBetween('created_at', [$request->get('date-from'), $request->get('date-to')])->get();
+        $orders = Order::query()->whereBetween('created_at', [Carbon::create($request->get('date-from')), Cabon::create($request->get('date-to'))])->get();
 dd($orders);
         foreach ($orders as $order) {
             $this->orderPaymentLabelsService->calculateLabels($order);
