c23d806eb36d714a9504fa38315d03e0a4487b72
Antoni Seba <antoniwoj@o2.pl>
2023-12-27 10:35:02 +0100

    Changes in filtering flow in main grid

--- app/Http/Livewire/Traits/WithChecking.php
+++ app/Http/Livewire/Traits/WithChecking.php
@@ -43,10 +43,8 @@ trait WithChecking
      */
     public function addLabelsForCheckedOrders(int $labelId): void
     {
-
-        dd($labelId, $this->checked);
-        $arr = [];
         foreach ($this->checked as $id) {
+            $arr = [];
             AddLabelService::addLabels(Order::find($id), [$labelId], $arr, []);
         }
 
