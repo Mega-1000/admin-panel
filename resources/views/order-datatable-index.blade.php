9b87a38917c603b671b1f72b644723fdec194c40
Antoni Seba <antoniwoj@o2.pl>
2024-01-03 17:10:22 +0100

    Final refactoring of main grid traits

--- resources/views/order-datatable-index.blade.php
+++ resources/views/order-datatable-index.blade.php
@@ -183,12 +183,10 @@
 
         const resetFilters = () => {
             const filterInputs = document.querySelectorAll('#filter');
-
+            // do not trigger event of "input"
             filterInputs.forEach(input => {
                 input.value = '';
             });
-
-            Livewire.emit('resetFilters');
         }
 
         const selectAllOrders = () => {
