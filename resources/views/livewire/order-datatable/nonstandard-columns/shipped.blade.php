346f985b4adbf3085320a4c03829204c1be53607
Antoni Seba <antoniwoj@o2.pl>
2024-01-13 08:53:21 +0100

    Work on duble letter number fix

--- resources/views/livewire/order-datatable/nonstandard-columns/shipped.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/shipped.blade.php
@@ -4,6 +4,7 @@
     $value = collect($data)->firstWhere('status', 'SENDING');
 @endphp
 @php
+dd($value['letter_number']);
     $html = '';
     foreach ($data as $key => $value) {
         $isProblem = abs(($value['real_cost_for_company'] ?? 0) - ($value['cost_for_company'] ?? 0)) > 2;
