90377335453560785e21462255f7251a435fe30f
Antoni Seba <antoniwoj@o2.pl>
2024-01-18 20:38:40 +0100

    Make filters user dependent

--- app/Services/OrderPaymentLabelsService.php
+++ app/Services/OrderPaymentLabelsService.php
@@ -57,6 +57,7 @@ readonly class OrderPaymentLabelsService
 
         $sumOfGrossValues = $totalProductPrice + $additional_service + $additional_cod_cost + $shipment_price_client;
 
+        dd($sumOfGrossValues, $depositPaidData['returnedValue'], $depositPaidData['balance'], $depositPaidData['knownPayments']);
         if (
             $sumOfGrossValues + $depositPaidData['returnedValue'] - $depositPaidData['balance'] - $depositPaidData['knownPayments'] == 0 &&
             $order->payments->count() > 0
