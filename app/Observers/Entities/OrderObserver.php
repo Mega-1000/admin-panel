142fbf9390ece70548afabc8563342a6ede704ee
Antoni Seba <antoniwoj@o2.pl>
2024-01-19 07:22:55 +0100

    Make filters user dependent

--- app/Observers/Entities/OrderObserver.php
+++ app/Observers/Entities/OrderObserver.php
@@ -105,7 +105,34 @@ readonly class OrderObserver
         }
 
         $this->orderPaymentLabelsService->calculateLabels($order);
-        $order->labels()->detach(39);
+        $arr = [];
+
+        $additional_service = $order->additional_service_cost ?? 0;
+        $additional_cod_cost = $order->additional_cash_on_delivery_cost ?? 0;
+        $shipment_price_client = $order->shipment_price_for_client ?? 0;
+        $totalProductPrice = 0;
+
+        foreach ($order->items as $item) {
+            $price = $item->gross_selling_price_commercial_unit ?: $item->net_selling_price_commercial_unit ?: 0;
+            $quantity = $item->quantity ?? 0;
+            $totalProductPrice += $price * $quantity;
+        }
+
+        $depositPaidData = $this->orderDepositPaidCalculator->calculateDepositPaidOrderData($order);
+
+        $sumOfGrossValues = $totalProductPrice + $additional_service + $additional_cod_cost + $shipment_price_client;
+
+        if (
+            round($sumOfGrossValues) + round($depositPaidData['returnedValue']) - round($depositPaidData['balance']) - round($depositPaidData['wtonValue']) == 0.0 &&
+            $order->payments->count() > 0
+        ) {
+            $order = Order::find($order->id);
+            $LpArray = [];
+//            RemoveLabelService::removeLabels($order, [39], $LpArray, [], Auth::user()->id);
+        } else {
+            AddLabelService::addLabels($order, [39], $arr, [], Auth::user()?->id);
+        }
+
     }
 
     public function labelsAttached(Order $order): void
