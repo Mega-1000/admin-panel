bdf42960cc2860cfa6e81089a6b5f82de8371149
Antoni Seba <antoniwoj@o2.pl>
2024-01-19 06:48:07 +0100

    Make filters user dependent

--- app/Services/OrderPaymentLabelsService.php
+++ app/Services/OrderPaymentLabelsService.php
@@ -62,7 +62,6 @@ readonly class OrderPaymentLabelsService
             round($sumOfGrossValues) + round($depositPaidData['returnedValue']) - round($depositPaidData['offerFinanceBalance']) - round($depositPaidData['wtonValue']) == 0.0 &&
             $order->payments->count() > 0
         ) {
-            dd('oko');
             $order->labels()->detach(39);
         } else {
             AddLabelService::addLabels($order, [39], $arr, [], Auth::user()?->id);
