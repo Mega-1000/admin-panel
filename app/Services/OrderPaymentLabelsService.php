aaac5bd5d8be1b5d2fedb623f22d5f5407586a6a
Antoni Seba <antoniwoj@o2.pl>
2024-01-19 06:52:44 +0100

    Make filters user dependent

--- app/Services/OrderPaymentLabelsService.php
+++ app/Services/OrderPaymentLabelsService.php
@@ -57,13 +57,11 @@ readonly class OrderPaymentLabelsService
 
         $sumOfGrossValues = $totalProductPrice + $additional_service + $additional_cod_cost + $shipment_price_client;
 
-//        dd(round($sumOfGrossValues) + round($depositPaidData['returnedValue']) - round($depositPaidData['offerFinanceBalance']) - round($depositPaidData['wtonValue']));
         if (
             round($sumOfGrossValues) + round($depositPaidData['returnedValue']) - round($depositPaidData['offerFinanceBalance']) - round($depositPaidData['wtonValue']) == 0.0 &&
             $order->payments->count() > 0
         ) {
             $order->labels()->detach(39);
-            dd('label detached', $order, $order->labels()->detach(39));
         } else {
             AddLabelService::addLabels($order, [39], $arr, [], Auth::user()?->id);
         }
