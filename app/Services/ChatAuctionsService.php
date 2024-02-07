29dadde4ec0356f25d6a583249144567bc6248e7
Antoni Seba <antoniwoj@o2.pl>
2024-02-07 16:10:01 +0100

    Fix saving of orders

--- app/Services/ChatAuctionsService.php
+++ app/Services/ChatAuctionsService.php
@@ -137,7 +137,7 @@ readonly class ChatAuctionsService
         return ChatAuction::whereHas('firms', function ($query) use ($firm) {
             $query->where('firm_id', $firm->id);
         })
-        ->with(['offers', 'offers.firm', 'chat.order.customer'])
+        ->with(['offers', 'offers.firm', 'chat.order.customer.addresses'])
         ->get()
         ->each(function (ChatAuction $auction) use ($firm) {
             $auction->editPricesLink = route('auctions.offer.create', ['token' => ChatAuctionFirm::where('chat_auction_id', $auction->id)
