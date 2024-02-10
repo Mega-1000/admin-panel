ae81b68f407f646b297edca5a111b7d3c1b95c2c
Antoni Seba <antoniwoj@o2.pl>
2024-02-10 19:18:38 +0200

    Zip code bluring

--- app/Services/ProductsService.php
+++ app/Services/ProductsService.php
@@ -227,7 +227,7 @@ class ProductsService
                     $radius = $query->distance;
                     $warehouse = Warehouse::find($query->id);
 
-                    if ($radius > $warehouse->radius) {
+                    if ($radius > $warehouse->radius && $product?->variation_group === 'styropiany') {
                         $product->blured = true;
                     } else {
                         $product->blured = false;
