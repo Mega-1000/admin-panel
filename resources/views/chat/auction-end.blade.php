7d757ec9441bbec74dc435065a8150f3a36f505e
Antoni Seba <antoniwoj@o2.pl>
2024-02-14 07:30:37 +0200

    Make 12 employee attached to order by default

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -256,7 +256,7 @@
 
                                 foreach ($items as $item) {
                                     $variation = App\Entities\Product::where('product_group', $item->product_group)->where('product_name_supplier', $firm->firm->symbol)->first();
-                                    $prices[] = $variation?->price->basic_price_gross;
+                                    $prices[] = $variation?->price->net_purchase_price_basic_unit_after_discounts;
                                     dd($variation->price);
                                 }
                                 @endphp
