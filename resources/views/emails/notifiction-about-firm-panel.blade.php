0a3e1c739f9817c1c1b749bb25315b0a51832ab2
Antoni Seba <antoniwoj@o2.pl>
2024-02-14 20:30:54 +0100

    Add mail for company with panel data

--- resources/views/emails/notifiction-about-firm-panel.blade.php
+++ resources/views/emails/notifiction-about-firm-panel.blade.php
@@ -1,28 +1,46 @@
 Mamy przyjemność poinformować, że Twoja firma została udanie zintegrowana z naszym systemem przetargowym. Dzięki temu zyskujesz bezpośredni dostęp do platformy, na której znajdziesz wszystkie aktualne przetargi dotyczące styropianów, a także będziesz mógł zarządzać swoimi ofertami w sposób jeszcze bardziej efektywny.
-
+<br>
+<br>
 Co zyskujesz dzięki dostępowi do naszego systemu?
-
+<br>
+<br>
 Przeglądanie Przetargów: Bezpośredni dostęp do listy przetargów, co umożliwia szybkie wyszukiwanie i filtrację interesujących Cię ofert.
+<br>
+<br>
 Zarządzanie Ofertami: Możliwość zmiany cen w swoich ofertach przetargowych w dowolnym momencie, co daje większą elastyczność w dostosowaniu strategii cenowej.
+<br>
 Widok Tabelaryczny: Przejrzysta prezentacja wszystkich ważnych informacji, w tym dat rozpoczęcia i zakończenia przetargów, co ułatwia planowanie i podejmowanie decyzji.
+<br>
 Monitorowanie Wyników: Szybki dostęp do informacji, czy Twoja oferta wygrała przetarg, co pozwoli Ci na lepsze zrozumienie rynku i dostosowanie swoich strategii biznesowych.
+<br>
 Jak uzyskać dostęp?
+<br>
+<br>
 
 Aby skorzystać z pełnej funkcjonalności naszego systemu, proszę kliknąć poniższy link i zalogować się przy użyciu dostarczonych danych autoryzacyjnych:
+<br>
+<br>
 <a href="https://mega1000.pl/firms/przetargi?firmToken={{ $firm->access_token }}">
     https://mega1000.pl/firms/przetargi?firmToken={{ $firm->access_token }}
 </a>
-
+<br>
+<br>
 Zachęcamy do zapoznania się z naszym przewodnikiem użytkownika oraz często zadawanymi pytaniami (FAQ), które pomogą Ci w pełni wykorzystać możliwości platformy.
 
+<br>
+<br>
+
 W przypadku jakichkolwiek pytań lub potrzeby wsparcia, nasz zespół wsparcia klienta jest do Twojej dyspozycji. Możesz się z nami skontaktować, wysyłając e-mail na adres: antoniwoj@o2.pl lub dzwoniąc pod numer: antoniwoj@o2.pl.
-
+<br>
+<br>
 Cieszymy się na współpracę i jesteśmy przekonani, że nasz system przetargowy znacząco przyczyni się do rozwoju Twojego biznesu.
-
+<br>
+<br>
 Z wyrazami szacunku,
-
-Antoni Seba
-Programista
-Elektroniczna platforma handlowa polska sp. z o.o
-antoniwoj@o2.pl
-+48 576 205 389
+<br>
+<br>
+Antoni Seba <br>
+Programista <br>
+Elektroniczna platforma handlowa polska sp. z o.o <br>
+antoniwoj@o2.pl <br>
++48 576 205 389 <br>
