6b26e00bf0ea3cb5aad9ca7925434e88234b1fe1
Antoni Seba <antoniwoj@o2.pl>
2024-02-14 07:18:46 +0200

    Make 12 employee attached to order by default

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -260,6 +260,8 @@
                                 }
                                 @endphp
 
+                                {{ $prices }}
+
                                 @foreach($prices as $price)
                                     <td>
                                         {{ $price }} Zł
