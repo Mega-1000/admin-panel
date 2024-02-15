1f146585364eb8506987c50d8bda6e3409c63676
Antoni Seba <antoniwoj@o2.pl>
2024-02-15 13:03:10 +0100

    Add mail for company with panel data

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -194,7 +194,7 @@ class AuctionsController extends Controller
 
     public function sendNotificationAboutFirmPanel(Firm $firm): RedirectResponse
     {
-        $employees = $firm->employees()->whereHas('roles', function (Blueprint $q) {
+        $employees = $firm->employees()->whereHas('employeeRoles', function (Blueprint $q) {
             $q->where('name', 'zmiana cen');
         })->get();
 
