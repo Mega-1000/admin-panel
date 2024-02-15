e705ff9e5100e02d8494c15356a43b578253f8bc
Antoni Seba <antoniwoj@o2.pl>
2024-02-15 08:22:49 +0100

    Add mail for company with panel data

--- resources/views/emails/notifiction-about-firm-panel.blade.php
+++ resources/views/emails/notifiction-about-firm-panel.blade.php
@@ -1,4 +1,5 @@
-Mamy przyjemność poinformować, że Twoja firma została udanie zintegrowana z naszym systemem przetargowym. Dzięki temu zyskujesz bezpośredni dostęp do platformy, na której znajdziesz wszystkie aktualne przetargi dotyczące styropianów, a także będziesz mógł zarządzać swoimi ofertami w sposób jeszcze bardziej efektywny.
+Mamy przyjemność poinformować, że Twoja firma została udanie zintegrowana z naszym systemem przetargowym.
+<br>Dzięki temu zyskujesz bezpośredni dostęp do platformy, na której znajdziesz wszystkie aktualne przetargi dotyczące styropianów, a także będziesz mógł zarządzać swoimi ofertami w sposób jeszcze bardziej efektywny.
 <br>
 <br>
 Co zyskujesz dzięki dostępowi do naszego systemu?
@@ -6,12 +7,12 @@ Co zyskujesz dzięki dostępowi do naszego systemu?
 <br>
 Przeglądanie Przetargów: Bezpośredni dostęp do listy przetargów, co umożliwia szybkie wyszukiwanie i filtrację interesujących Cię ofert.
 <br>
-<br>
 Zarządzanie Ofertami: Możliwość zmiany cen w swoich ofertach przetargowych w dowolnym momencie, co daje większą elastyczność w dostosowaniu strategii cenowej.
 <br>
 Widok Tabelaryczny: Przejrzysta prezentacja wszystkich ważnych informacji, w tym dat rozpoczęcia i zakończenia przetargów, co ułatwia planowanie i podejmowanie decyzji.
 <br>
 Monitorowanie Wyników: Szybki dostęp do informacji, czy Twoja oferta wygrała przetarg, co pozwoli Ci na lepsze zrozumienie rynku i dostosowanie swoich strategii biznesowych.
+<br>
 <br>
 Jak uzyskać dostęp?
 <br>
@@ -30,7 +31,7 @@ Zachęcamy do zapoznania się z naszym przewodnikiem użytkownika oraz często z
 <br>
 <br>
 
-W przypadku jakichkolwiek pytań lub potrzeby wsparcia, nasz zespół wsparcia klienta jest do Twojej dyspozycji. Możesz się z nami skontaktować, wysyłając e-mail na adres: antoniwoj@o2.pl lub dzwoniąc pod numer: antoniwoj@o2.pl.
+W przypadku jakichkolwiek pytań lub potrzeby wsparcia, nasz zespół wsparcia klienta jest do Twojej dyspozycji. Możesz się z nami skontaktować, wysyłając e-mail na adres: styropian@ephpolska.pl lub dzwoniąc pod numer: +48 507 925 963.
 <br>
 <br>
 Cieszymy się na współpracę i jesteśmy przekonani, że nasz system przetargowy znacząco przyczyni się do rozwoju Twojego biznesu.
@@ -38,9 +39,4 @@ Cieszymy się na współpracę i jesteśmy przekonani, że nasz system przetargo
 <br>
 Z wyrazami szacunku,
 <br>
-<br>
-Antoni Seba <br>
-Programista <br>
-Elektroniczna platforma handlowa polska sp. z o.o <br>
-antoniwoj@o2.pl <br>
-+48 576 205 389 <br>
+Elektroniczna Platforma Handlowa Polska sp. z o.o <br>
