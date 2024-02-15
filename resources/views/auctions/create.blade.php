bc41a1be15500c26798f278befa73dd60e0cd75e
Antoni Seba <antoniwoj@o2.pl>
2024-02-15 17:35:23 +0100

    Add mail for company with panel data

--- resources/views/auctions/create.blade.php
+++ resources/views/auctions/create.blade.php
@@ -26,8 +26,10 @@
 
         <label for="base-input" class="block mb-2 text-sm font-medium mt-2">Podział procentowy cena/jakość</label>
 
+        Cena
         <input type="text" id="quality-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Cena" value="50" name="price">
 
+        Jakość
         <input type="text" id="price-input" class="mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Jakość" value="50" name="quality">
 
         <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
