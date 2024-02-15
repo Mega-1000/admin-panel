91fb3d9d15d471a1c809a74c6ff209ea0af3e5b7
Antoni Seba <antoniwoj@o2.pl>
2024-02-15 09:42:14 +0100

    Add mail for company with panel data

--- resources/views/auctions/pre-data-prices-table.blade.php
+++ resources/views/auctions/pre-data-prices-table.blade.php
@@ -1,4 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
 
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.chat_name') }}</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        .container {
+            width: 40%;
+            margin: auto;
+        }
+
+        @media screen and (max-width: 768px) {
+            .container {
+                width: 100%;
+            }
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        td {
+            vertical-align: middle;
+        }
+
+        #chat-container {
+            margin-top: 50px;
+        }
+
+        th.asc::after {
+            content: " ↓"; /* Change to arrow SVG or symbol as needed */
+        }
+
+        th.desc::after {
+            content: " ↑"; /* Change to arrow SVG or symbol as needed */
+        }
+    </style>
+</head>
 <table>
     <thead>
     <tr>
@@ -16,9 +81,6 @@
                     $name = implode(' ', $words);
                 @endphp
                 {{ $name }}
-                <button class="btn btn-primary">
-                    Sortuj
-                </button>
             </th>
         @endforeach
     </tr>
