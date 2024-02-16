3d194e915824b5ffbc8755922fe48e7f8fe873d8
Antoni Seba <antoniwoj@o2.pl>
2024-02-16 17:13:18 +0100

    Add mail for company with panel data

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -270,10 +270,9 @@ class AuctionsController extends Controller
             $group = AuctionsHelper::getTrimmedProductGroupName($product);
 
             if (!in_array($group, $productGroups)) {
+                $productGroups[] = $group;
                 continue;
             }
-
-            $productGroups[] = $group;
         }
 
         return response()->json($productGroups);
