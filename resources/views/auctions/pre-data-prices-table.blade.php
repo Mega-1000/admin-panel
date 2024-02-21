c1d74d60f6dea99bd71b87390d24accc1b175d4c
Antoni Seba <antoniwoj@o2.pl>
2024-02-21 10:53:07 +0100

    Styrofoams advanced UI

--- resources/views/auctions/pre-data-prices-table.blade.php
+++ resources/views/auctions/pre-data-prices-table.blade.php
@@ -62,10 +62,45 @@
         th.desc::after {
             content: " ↑"; /* Change to arrow SVG or symbol as needed */
         }
+
+        .loader {
+         position: fixed;
+         z-index: 9999;
+         top: 0;
+         left: 0;
+         width: 100%;
+         height: 100%;
+         background: rgba(255, 255, 255, 0.9);
+         display: flex;
+         justify-content: center;
+         align-items: center;
+        }
+
+        .loader::after {
+        content: "";
+        width: 50px;
+        height: 50px;
+        border: 5px solid #f3f3f3;
+        border-top: 5px solid #3498db;
+        border-radius: 50%;
+        animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+        }
+
+        /* Hide the loader once the page is loaded */
+        .loaded .loader {
+        display: none;
+        }
     </style>
 </head>
 
 <body>
+<div id="loader" class="loader"></div>
+
 <div class="container">
     <table>
         <thead>
