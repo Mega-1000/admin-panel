5a56afda3b1927f4dca17689a5d4087a94a0a4cd
Antoni Seba <antoniwoj@o2.pl>
2024-02-22 12:02:26 +0100

    Styrofoams advanced UI

--- resources/views/chat/chat_body.blade.php
+++ resources/views/chat/chat_body.blade.php
@@ -1,15 +1,12 @@
 <div class="panel panel-default" style="max-height: calc(100vh - 200px); overflow-y: auto;">
     <div class="panel-body chat-panel" style="border: 2px solid #000; border-radius: 7px;">
-        @php(dd($chatMessages))
-        @if ( isset($chatMessages) )
-            @foreach ($chatMessages as $message)
-                @if( $userType == MessagesHelper::TYPE_USER || isset($assignedMessagesIds[$message->id] ))
-                    @if ($userType != MessagesHelper::TYPE_USER && $message->area != 0)
-                        @continue
-                    @endif
-                    @include ('chat/single_message', ['message' => $message])
+        @foreach ($chatMessages as $message)
+            @if( $userType == MessagesHelper::TYPE_USER || isset($assignedMessagesIds[$message->id] ))
+                @if ($userType != MessagesHelper::TYPE_USER && $message->area != 0)
+                    @continue
                 @endif
-            @endforeach
-        @endif
+                @include ('chat/single_message', ['message' => $message])
+            @endif
+        @endforeach
     </div>
 </div>
