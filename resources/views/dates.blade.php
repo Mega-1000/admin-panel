7491590916f47a9ff0acda44b2091fece499fab1
Antoni Seba <antoniwoj@o2.pl>
2024-02-24 11:26:34 +0100

    Dates updating

--- resources/views/dates.blade.php
+++ resources/views/dates.blade.php
@@ -21,7 +21,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modifyDateModalLabel">Modify Date</h5>
+                <h5 class="modal-title" id="modifyDateModalLabel">Modyfikuj daty</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
