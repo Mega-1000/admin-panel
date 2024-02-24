a7d01ec54e72790e4d3bb7a9e3bfbdf0122b1b3c
Antoni Seba <antoniwoj@o2.pl>
2024-02-24 20:42:20 +0100

    Styrofoams advanced UI

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -77,7 +77,7 @@
                             <div id="auction-instructions" style="display: none; color: white; font-weight: bold; font-size: large; border-radius: 15px; padding: 20px; background-color: #0c0c0c">
                             <p>Poniżej tabela cen brutto produktów z tej oferty których istnieje możliwość dostarczenia na wskazany kod pocztowy.</p>
                                 <iframe src="{{ route('displayPreDataPricesTableForOrder', $chat->id) }}" height="600px; width: 100%" width="600"></iframe>
-                            <p>Możesz rozpocząć przetarg klikając w ten przycisk. Poprosimy firmy o indywidualną wycenę twojego zamówienia.</p>
+                            <p>Jeśli chcesz poprosić firmy o indywidualną wycenę twojego zapytania naciśnij przycisk rozpocznij przetarg.</p>
                                 <button class="btn btn-primary" id="dimiss-info">Ok</button>
                             </div>
                         @else
