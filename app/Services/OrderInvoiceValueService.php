138300508357fd84cdeb0f2173cd25b094947252
Antoni Seba <antoniwoj@o2.pl>
2024-02-26 14:31:08 +0100

    Styrofoams advanced UI

--- app/Services/OrderInvoiceValueService.php
+++ app/Services/OrderInvoiceValueService.php
@@ -15,8 +15,8 @@ final class OrderInvoiceValueService
      */
     public static function createFromDTO(ControllSubjectInvoiceDTO $dto, Order $order): OrderInvoiceValue
     {
-        dd($dto->number);
-        if (OrderInvoiceValue::where('invoice_number', $dto->number)->exists()) {
+        dd($dto);
+        if (OrderInvoiceValue::where('invoice_number', $dto->number)->exists() && ) {
             return OrderInvoiceValue::where('invoice_number', $dto->number)->first();
         }
 
