ae950aae07933cfb4be5e28a04659178be4401e3
Antoni Seba <antoniwoj@o2.pl>
2024-02-28 16:35:29 +0100

    Styrofoams advanced UI

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -256,28 +256,30 @@ class AuctionsController extends Controller
             $q->where('variation_group', 'styropiany');
         })->get();
 
-        foreach ($firms as $key => $firm) {
-            $raw = DB::selectOne(
-                'SELECT w.id, pc.latitude, pc.longitude, 1.609344 * SQRT(
-                    POW(69.1 * (pc.latitude - :latitude), 2) +
-                    POW(69.1 * (:longitude - pc.longitude) * COS(pc.latitude / 57.3), 2)) AS distance
-                    FROM postal_code_lat_lon pc
-                         JOIN warehouse_addresses wa on pc.postal_code = wa.postal_code
-                         JOIN warehouses w on wa.warehouse_id = w.id
-                    WHERE w.firm_id = :firmId AND w.status = \'ACTIVE\'
-                    ORDER BY distance
-                limit 1',
-                [
-                    'latitude' => $coordinatesOfUser->latitude,
-                    'longitude' => $coordinatesOfUser->longitude,
-                    'firmId' => $firm->id
-                ]
-            );
+        if ($coordinatesOfUser) {
+            foreach ($firms as $key => $firm) {
+                $raw = DB::selectOne(
+                    'SELECT w.id, pc.latitude, pc.longitude, 1.609344 * SQRT(
+                        POW(69.1 * (pc.latitude - :latitude), 2) +
+                        POW(69.1 * (:longitude - pc.longitude) * COS(pc.latitude / 57.3), 2)) AS distance
+                        FROM postal_code_lat_lon pc
+                             JOIN warehouse_addresses wa on pc.postal_code = wa.postal_code
+                             JOIN warehouses w on wa.warehouse_id = w.id
+                        WHERE w.firm_id = :firmId AND w.status = \'ACTIVE\'
+                        ORDER BY distance
+                    limit 1',
+                    [
+                        'latitude' => $coordinatesOfUser->latitude,
+                        'longitude' => $coordinatesOfUser->longitude,
+                        'firmId' => $firm->id
+                    ]
+                );
 
-            $radius = $raw?->distance;
+                $radius = $raw?->distance;
 
-            if ($radius ?? 0 > $firm->warehouses()->first()->radius) {
-                $firms->forget($key);
+                if ($radius ?? 0 > $firm->warehouses()->first()->radius) {
+                    $firms->forget($key);
+                }
             }
         }
 
