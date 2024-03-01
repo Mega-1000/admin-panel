c2c6e8b0703061f017ffdc56d0ee0f25d2fcca32
Antoni Seba <antoniwoj@o2.pl>
2024-03-01 15:10:26 +0100

    Styrofoams advanced UI

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -354,7 +354,9 @@ class AuctionsController extends Controller
         foreach ($products as $product) {
             $product = Product::find($product->productId);
             // prices are taken from offersorder_item_id
-            $offer = ChatAuctionOffer::where('firm_id', $product->firm->id)->where('order_item_id', $order->items()->where('product_id', $product->id)->first()->id)->first();
+            $offer = ChatAuctionOffer::where('firm_id', $product->firm->id)
+                ->where('order_item_id', $order->items()->where('product_id', $product->id)->first()->id)
+                ->first();
             dd($offer);
             $orderBuilder->assignItemsToOrder($order, [
                 $product->toArray() + ['amount' => 1] ,
