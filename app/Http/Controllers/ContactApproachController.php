6b6001b7bcfda18d45bcec6babf2f618a40c49ee
Antoni Seba <antoniwoj@o2.pl>
2024-03-01 18:02:37 +0100

    Styrofoams advanced UI

--- app/Http/Controllers/ContactApproachController.php
+++ app/Http/Controllers/ContactApproachController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Entities\ContactApproach;
+use App\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -10,11 +11,17 @@ class ContactApproachController extends Controller
 {
     public function store(Request $request): JsonResponse
     {
+        if (User::where('phone', $request->get('phone_number'))->exists() || ContactApproach::where('phone_number', $request->get('phone_number'))->exists()) {
+            return response()->json([
+                'success' => false,
+            ]);
+        }
+
         return response()->json(ContactApproach::create([
             'phone_number' => $request->get('phone_number'),
             'referred_by_user_id' => $request->get('referred_by_user_id'),
             'done' => false,
-        ]));
+        ]) + ['success' => true]);
     }
 
     public function getApproachesByUser(int $userId): JsonResponse
