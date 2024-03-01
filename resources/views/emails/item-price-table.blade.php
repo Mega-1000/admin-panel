34e8dd09366621bcba8645b7dcf2c2e0292fa921
Antoni Seba <antoniwoj@o2.pl>
2024-03-01 09:17:11 +0100

    Styrofoams advanced UI

--- resources/views/emails/item-price-table.blade.php
+++ resources/views/emails/item-price-table.blade.php
@@ -44,129 +44,90 @@
     <Tr>
         <Th>Ceny jednostkowa brutto:</Th>
         <Th>
-            <input disabled
-                   value="{{number_format($item->net_purchase_price_commercial_unit_after_discounts * 1.23, 2)}}"
-            />
+            {{number_format($item->net_purchase_price_commercial_unit_after_discounts * 1.23, 2)}}
         </Th>
         <Th>
-            <input disabled
-                   value="{{number_format($item->net_purchase_price_basic_unit_after_discounts * 1.23, 2)}}"
-            />
+           {{number_format($item->net_purchase_price_basic_unit_after_discounts * 1.23, 2)}}
         </Th>
         <Th>
-            <input disabled
-                   value="{{number_format($item->net_purchase_price_calculated_unit_after_discounts  * 1.23, 2)}}"
-            />
+            {{number_format($item->net_purchase_price_calculated_unit_after_discounts  * 1.23, 2)}}
         </Th>
         @if($bw > 0)
-
             <Th>
-                <input disabled
-                       value="{{number_format($item->net_purchase_price_aggregate_unit_after_discounts * 1.23, 2)}}"
-                />
+                {{number_format($item->net_purchase_price_aggregate_unit_after_discounts * 1.23, 2)}}
             </Th>
         @endif
         @if($bx > 0 && $item->net_purchase_price_the_largest_unit_after_discounts > 0)
             <Th>
-                <input disabled
-                       value="{{number_format($item->net_purchase_price_the_largest_unit_after_discounts * 1.23, 2)}}"
-                />
+                {{number_format($item->net_purchase_price_the_largest_unit_after_discounts * 1.23, 2)}}
             </Th>
         @endif
     </Tr>
     <Tr>
         <Th>Ilość</Th>
         <Th>
-            <input disabled
-                   value="{{$item->quantity}}"
-            />
+            {{$item->quantity}}
         </Th>
         <Th>
-            <input disabled
-                   value={{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack, 3)}}
-            />
+            {{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack, 3)}}
         </Th>
         <Th>
-            <input disabled
-                   value={{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption, 3)}}
-            />
+            {{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption, 3)}}
         </Th>
         @if($bw > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack, 3)}}
-                />
+               {{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack, 3)}}
             </Th>
         @endif
         @if($bx > 0 && $item->net_purchase_price_the_largest_unit_after_discounts > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit, 3)}}
-                />
+                {{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit, 3)}}
             </Th>
         @endif
     </Tr>
     <Tr>
         <Th>Wartość Netto</Th>
         <Th>
-            <input disabled
-                   value="{{$item->price}}"
-            />
+           {{$item->price}}
         </Th>
         <Th>
-            <input disabled
-                   value={{$item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack * $item->net_purchase_price_basic_unit_after_discounts}}
-            />
+           {{$item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack * $item->net_purchase_price_basic_unit_after_discounts}}
         </Th>
         <Th>
-            <input disabled
-                   value={{$item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption * $item->net_purchase_price_calculated_unit_after_discounts}}
-            />
+           {{$item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption * $item->net_purchase_price_calculated_unit_after_discounts}}
         </Th>
         @if($bw > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack * $item->net_purchase_price_aggregate_unit_after_discounts, 2)}}
-                />
+                {{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack * $item->net_purchase_price_aggregate_unit_after_discounts, 2)}}
             </Th>
         @endif
         @if($bx > 0 && $item->net_purchase_price_the_largest_unit_after_discounts > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit * $item->net_purchase_price_the_largest_unit_after_discounts, 2)}}
-                />
+               {{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit * $item->net_purchase_price_the_largest_unit_after_discounts, 2)}}
             </Th>
         @endif
     </Tr>
     <Tr>
         <Th>Wartość Brutto</Th>
         <Th>
-            <input disabled
-                   value="{{number_format($item->price * 1.23, 2)}}"
-            />
+           {{number_format($item->price * 1.23, 2)}}
+        </Th>
+        <Th>
+           {{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack * round(1.23 * $item->net_purchase_price_basic_unit_after_discounts, 2), 2)}}
         </Th>
         <Th>
             <input disabled
-                   value={{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack * round(1.23 * $item->net_purchase_price_basic_unit_after_discounts, 2), 2)}}
-            />
-        </Th>
-        <Th>
-            <input disabled
-                   value={{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption * round(1.23 * $item->net_purchase_price_calculated_unit_after_discounts, 2), 2)}}
+                {{number_format($item->quantity * $item->product->packing->numbers_of_basic_commercial_units_in_pack / $item->product->packing->unit_consumption * round(1.23 * $item->net_purchase_price_calculated_unit_after_discounts, 2), 2)}}
             />
         </Th>
         @if($bw > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack * round(1.23 * $item->net_purchase_price_aggregate_unit_after_discounts, 2), 2)}}
-                />
+               {{number_format($item->quantity * $item->product->packing->number_of_sale_units_in_the_pack * round(1.23 * $item->net_purchase_price_aggregate_unit_after_discounts, 2), 2)}}
             </Th>
         @endif
         @if($bx > 0 && $item->net_purchase_price_the_largest_unit_after_discounts > 0)
             <Th>
-                <input disabled
-                       value={{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit * round(1.23 * $item->net_purchase_price_the_largest_unit_after_discounts, 2), 2)}}
-                />
+                {{number_format($item->quantity * $item->product->packing->number_of_trade_items_in_the_largest_unit * round(1.23 * $item->net_purchase_price_the_largest_unit_after_discounts, 2), 2)}}
             </Th>
         @endif
     </Tr>
