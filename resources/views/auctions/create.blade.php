7b527544d90be69dac866a348984171edf089637
Antoni Seba <antoniwoj@o2.pl>
2024-03-16 08:15:25 +0100

    Styrofoams advanced UI

--- resources/views/auctions/create.blade.php
+++ resources/views/auctions/create.blade.php
@@ -24,7 +24,7 @@
         <label for="base-input" class="block mb-2 text-sm font-medium mt-2">Wstępny termin dostawy (Przynajmniej 2 dni robocze od zakończenia przetargu)</label>
         <input type="date" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Wstępny termin dostawy (Przynajmniej 2 dni robocze od zakończenia przetargu)" name="date_of_delivery">
 
-        <label for="base-input" class="block mb-2 text-sm font-medium mt-2">Podział procentowy cena/jakość</label>
+        <label for="base-input" class="block mb-2 text-sm font-medium mt-6">Podział procentowy cena/jakość</label>
 
         Cena
         <input type="text" id="quality-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Cena" value="50" name="price">
