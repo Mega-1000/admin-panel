0be6578d21cf4c7e2d505457caf90e3518ce0dce
Antoni Seba <antoniwoj@o2.pl>
2024-03-16 09:18:10 +0100

    Styrofoams advanced UI

--- resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
@@ -1,7 +1,7 @@
 @if(array_key_exists('chat', $data) && !empty($data['chat']) && array_key_exists('messages', $data['chat']))
     @foreach($data['chat']['messages'] as $message)
         <hr>
-        {{ $message['message'] }}
+        {{ $message['message'] }} - {{ $message['created_at'] }}
         <br>
         <hr>
     @endforeach
