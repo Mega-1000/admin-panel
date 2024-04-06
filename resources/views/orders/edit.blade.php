1ff34973cf4679c3cecf37b2cce977f8a3ddd1a4
Antoni Seba <antoniwoj@o2.pl>
2024-04-06 14:53:50 +0200

    NOF make great again

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1046,11 +1046,7 @@
                             </tr>
                             @foreach($productsVariation[$item->product->id] as $variation)
                                 <tr class="row-{{$variation['id']}}">
-                                    @php
-                                        $product = App\Entities\Product::find($variation['id']);
-                                    @endphp
-                                    @php($dateOfPriceChange = \Carbon\Carbon::create($product->date_of_price_change))
-                                    <td style="{{ $dateOfPriceChange->lessThan(\Carbon\Carbon::now()) ? 'color: red;' : '' }}">
+                                    <td style="">
                                         {{$variation['name']}}
                                     </td>
                                     <td>
