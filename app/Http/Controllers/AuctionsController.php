18f16856cb10e7d9a4530b06fd01d1d5542f7501
Antoni Seba <antoniwoj@o2.pl>
2024-04-07 13:38:47 +0200

    NOF make great again

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -387,7 +387,6 @@ class AuctionsController extends Controller
         $items = collect($productService->getVariations($order));
         $firms = collect();
 
-        dd($items);
         foreach($items as $item) {
             foreach ($item as $i) {
                 $i = Product::find($i['id']);
