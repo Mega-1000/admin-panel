56c9f2dddaccf316b2125fac301ad35efe2e05e2
Antoni Seba <antoniwoj@o2.pl>
2024-04-07 09:37:51 +0200

    NOF make great again

--- resources/views/auctions/pre-data-prices-table.blade.php
+++ resources/views/auctions/pre-data-prices-table.blade.php
@@ -184,7 +184,7 @@
                                     @php($dateOfPriceChange = \Carbon\Carbon::create($product->date_of_price_change))
                                     <a href="https://mega1000.pl/single-product/{{ $id }}/no-layout"
                                        style="{{ $dateOfPriceChange->lessThan(\Carbon\Carbon::now()) ? 'color: red;' : '' }}">
-                                        {{ round($price / 23, 2) }}
+                                        {{ round($price / 1.23, 2) }}
                                     </a>
                                 @else
                                     Brak
