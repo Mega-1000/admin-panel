569ee908b168072f036463d750f15956d42c53bf
Antoni Seba <antoniwoj@o2.pl>
2024-04-09 18:32:14 +0200

    NOF make great again

--- resources/views/emails/requestNewPricesMail.blade.php
+++ resources/views/emails/requestNewPricesMail.blade.php
@@ -1,7 +1,7 @@
 Dzień dobry,
 <br>
 <br>
-Zwracamy się z prośbą wysłania aktualnych cenników styropianu {{ $this->message }} na adres email styropiany@ephpolska.pl oraz wysyłanie wszystkich nowych cenników na ten adres.
+Zwracamy się z prośbą wysłania aktualnych cenników styropianu {{ $message }} na adres email styropiany@ephpolska.pl oraz wysyłanie wszystkich nowych cenników na ten adres.
 <br>
 <br>
 Elektroniczna Platforma Handlowa Polska
