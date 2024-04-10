b4b502041603fc620c0cba08842c3080967ff767
Antoni Seba <antoniwoj@o2.pl>
2024-04-10 13:23:28 +0200

    NOF make great again

--- resources/views/livewire/order-datatable/nonstandard-columns/actions.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/actions.blade.php
@@ -1,22 +1,22 @@
-<button wire:click="setOrderToMove({{ $order['id'] }})" class="btn btn-secondary w-100 edit">
+<button wire:click="setOrderToMove({{ $order['id'] }})" class="btn btn-secondary w-100">
     Rozpocznij przenoszenie
 </button>
 
-<button wire:click="moveDataToOrder({{ $order['id'] }})" class="btn btn-primary w-100 edit" id="finalize-order-moving">
+<button wire:click="moveDataToOrder({{ $order['id'] }})" class="btn btn-primary w-100" id="finalize-order-moving">
     Zakończ przenoszenie
 </button>
 
-<a class="btn btn-sm btn-primary w-100 edit" href="{{ route('orders.edit', $order['id']) }}">
+<a class="btn btn-sm btn-primary" href="{{ route('orders.edit', $order['id']) }}">
     Edytuj
 </a>
 
 <form action="/admin/orders/{{$order['id']}}" method="POST">
     @method('delete')
     @csrf
-    <button class="btn btn-danger btn-sm edit" id="O">Usuń</button>
+    <button class="btn btn-danger btn-sm" id="O">Usuń</button>
 </form>
 
-<a href="/admin/orderReturn/{{ $order['id'] }}" class="btn btn-sm btn-danger edit">
+<a href="/admin/orderReturn/{{ $order['id'] }}" class="btn btn-sm btn-danger">
     <i class="glyphicon glyphicon-share-alt"></i>
     <span class="hidden-xs hidden-sm">Zwrot</span>
 </a>
@@ -27,12 +27,12 @@
     $messagesHelper->chatId = \App\Entities\Order::find($order['id'])?->chat?->id;
     $token = $messagesHelper->getChatToken($order['id'], auth()->id());
 @endphp
-<a href="/chat/{{ $token }}" target="_blank" class="btn btn-sm btn-primary edit">
+<a href="/chat/{{ $token }}" target="_blank" class="btn btn-sm btn-primary">
     <span class="hidden-xs hidden-sm">Chat</span>
 </a>
 
 @if((Auth::user()->role_id == 1 || Auth::user()->role_id == 2) && Auth::user()->id === User::ORDER_DELETE_USER)
-    <button class="btn btn-sm btn-danger delete delete-record" onclick="deleteRecord(' + {{ $order['id'] }} + ')">
+    <button class="btn btn-sm btn-danger delete-record" onclick="deleteRecord(' + {{ $order['id'] }} + ')">
         <i class="voyager-trash"></i>
         <span class="hidden-xs hidden-sm"> @lang('voyager.generic.delete')</span>
     </button>
