3c3583998236446e114b2ba5c66b654d12eb4a8a
Antoni Seba <antoniwoj@o2.pl>
2024-04-10 17:13:17 +0200

    NOF make great again

--- resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
@@ -1,5 +1,9 @@
 @if(array_key_exists('chat', $data) && !empty($data['chat']) && array_key_exists('messages', $data['chat']))
-    @foreach($data['chat']['messages'] as $message)
+    @php
+        $latestMessages = collect($data['chat']['messages'])->sortByDesc('created_at')->take(3);
+    @endphp
+
+    @foreach($latestMessages as $message)
         <hr>
         {{ $message['message'] }} - {{ explode('.', $message['created_at'])[0] }}
         <br>
