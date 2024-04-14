db38bd7f6612797b31807ebe07a5f8e982a3596d
Antoni Seba <antoniwoj@o2.pl>
2024-04-14 21:00:08 +0200

    Make recalc from csv work

--- resources/views/emails/notification-for-auction-offer-for-firms-mail.blade.php
+++ resources/views/emails/notification-for-auction-offer-for-firms-mail.blade.php
@@ -1,15 +1,21 @@
+@php
+    $token = \App\Entities\ChatAuctionFirm::where('chat_auction_id', $chatAuctionOffer->chatAuction->id)->where('firm_id', $chatAuctionOffer->firm->id)->first()->token
+@endphp
+
+
 Informujemy że w ofercie numer {{ $chatAuctionOffer->chatAuction->chat->order->id }}  zostało dokonane przebicie najniższej ceny.
 <br>
+z
 <br>
 Prosimy wejść na poniższy link aby zobaczyć na ten moment zaproponowaną najniższą ceną.
 <br>
 <br>
-<a href="https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->firm->access_token }}">
+<a href="https://admin.mega1000.pl/auctions/offer/create/{{ $token }}">
     Link do oferty
 </a>
 <br>
 <br>
-W razie problemów prosimy o wklejenie następującego linku w okno przeglądarki: https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->auctionFirm->firm->token }}
+W razie problemów prosimy o wklejenie następującego linku w okno przeglądarki: https://admin.mega1000.pl/auctions/offer/create/{{ $token }}
 <br>
 <br>
 Z pozdrowieniami
