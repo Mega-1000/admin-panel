701479b12db04bf870e76a046d05fe7a03a61f77
Antoni Seba <antoniwoj@o2.pl>
2024-04-14 20:40:05 +0200

    Make recalc from csv work

--- resources/views/emails/notification-for-auction-offer-for-firms-mail.blade.php
+++ resources/views/emails/notification-for-auction-offer-for-firms-mail.blade.php
@@ -4,12 +4,12 @@ Informujemy że w ofercie numer {{ $chatAuctionOffer->chatAuction->chat->order->
 Prosimy wejść na poniższy link aby zobaczyć na ten moment zaproponowaną najniższą ceną.
 <br>
 <br>
-<a href="https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->auctionFirm->token }}">
+<a href="https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->auctionFirm->firm->access_token }}">
     Link do oferty
 </a>
 <br>
 <br>
-W razie problemów prosimy o wklejenie następującego linku w okno przeglądarki: https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->auctionFirm->token }}
+W razie problemów prosimy o wklejenie następującego linku w okno przeglądarki: https://admin.mega1000.pl/auctions/offer/create/{{ $chatAuctionOffer->auctionFirm->firm->token }}
 <br>
 <br>
 Z pozdrowieniami
