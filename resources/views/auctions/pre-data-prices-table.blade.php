9c7ef10d6416458b31bdbe00ae2b43bf84d81c2b
Antoni Seba <antoniwoj@o2.pl>
2024-04-23 20:23:40 +0200

    Make recalc from csv work

--- resources/views/auctions/pre-data-prices-table.blade.php
+++ resources/views/auctions/pre-data-prices-table.blade.php
@@ -75,16 +75,6 @@
         th.sortable {
             cursor: pointer; /* Zmiana kursora na wskaźnik podczas najechania */
         }
-
-        th.asc::after {
-            content: " ↓"; /* Wskaźnik sortowania rosnącego */
-            color: green;
-        }
-
-        th.desc::after {
-            content: " ↑"; /* Wskaźnik sortowania malejącego */
-            color: red;
-        }
     </style>
 </head>
 
@@ -131,13 +121,7 @@
 
                     @endphp
                     @foreach($suffixes as $suffix)
-                        <th title="Kliknij aby sortować">
-                            {{ $suffix }}
-                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
-                            </svg>
-
-                        </th>
+                        <th title="Kliknij aby sortować">{{ $suffix }}</th>
                     @endforeach
                 @endforeach
             </tr>
