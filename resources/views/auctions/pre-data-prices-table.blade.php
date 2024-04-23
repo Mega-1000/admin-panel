307d9e63bdc765e4e178a26e46ed8af204ee4385
Antoni Seba <antoniwoj@o2.pl>
2024-04-23 20:22:57 +0200

    Make recalc from csv work

--- resources/views/auctions/pre-data-prices-table.blade.php
+++ resources/views/auctions/pre-data-prices-table.blade.php
@@ -131,7 +131,13 @@
 
                     @endphp
                     @foreach($suffixes as $suffix)
-                        <th title="Kliknij aby sortować">{{ $suffix }}</th>
+                        <th title="Kliknij aby sortować">
+                            {{ $suffix }}
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
+                            </svg>
+
+                        </th>
                     @endforeach
                 @endforeach
             </tr>
@@ -221,20 +227,6 @@
             });
 
             $('th').click(function(){
-                var $this = $(this);
-                var columnIndex = $this.index();
-                var sortState = $this.data('sortState');
-
-                // Usuń wskaźniki sortowania z innych kolumn
-                $this.siblings().removeClass('asc desc');
-
-                // Przełącz stan sortowania dla tej kolumny
-                if (sortState === 'asc') {
-                    $this.data('sortState', 'desc').addClass('desc').removeClass('asc');
-                } else {
-                    $this.data('sortState', 'asc').addClass('asc').removeClass('desc');
-                }
-
                 var table = $(this).parents('table').eq(0);
                 var columnIndex = $(this).index();
                 var sortState = $(this).data('sortState');
