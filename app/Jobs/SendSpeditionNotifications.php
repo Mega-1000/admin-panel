0e085e9a138f97d01187ab1f4061ba9df51048c2
Antoni Seba <antoniwoj@o2.pl>
2024-04-28 09:45:56 +0200

    Make recalc from csv work

--- app/Jobs/SendSpeditionNotifications.php
+++ app/Jobs/SendSpeditionNotifications.php
@@ -38,7 +38,10 @@ class SendSpeditionNotifications implements ShouldQueue
      */
     public function handle(): void
     {
-        $orders = DB::table('order_labels')->where(['label_id' => 5])->get();
+        $orders = DB::table('order_labels')
+            ->where('label_id', 5)
+            ->whereDate('created_at', '>=', Carbon::now()->subMonths(3))
+            ->get();
 
         foreach ($orders as $order) {
             $order = Order::find($order->order_id);
