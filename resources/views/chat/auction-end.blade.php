8f2b398b3383532ef56ffb785600885b691052ff
Antoni Seba <antoniwoj@o2.pl>
2024-05-10 12:48:25 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -190,7 +190,7 @@
                                 @endphp
 
                                 @if($offer)
-                                    {{ $auction->offers()->where('firm_id', $firm->firm->id)->where('order_item_id', $product->id)->orderBy('created_at', 'desc')->first()->basic_price_gross }}
+                                    {{ $auction->offers()->where('firm_id', $firm->firm->id)->where('order_item_id', $product->id)->orderBy('created_at', 'asc')->first()->basic_price_gross }}
 
                                     <input type="checkbox" class="offer-checkbox" id="offer-checkbox{{ $offer->id }}" data-product-id="{{ $product->id }}" data-variation-id="{{ $offer->id }}">
 
