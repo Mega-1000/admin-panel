9c6ee55abeea94eb3516f3dfde8f4821eca1130f
Antoni Seba <antoniwoj@o2.pl>
2024-05-10 15:14:06 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -232,15 +232,15 @@
 
                            @php
                                $prices = [];
-                               $items = isset($auction) ? $auction?->chat?->order?->items?->pluck('product') : $order?->items?->pluck('product');
+                               $items = isset($auction) ? $auction?->chat?->order?->items : $order?->items;
                                $totalCost = 0;
 
                                foreach ($items as $item) {
-                                   $variation = App\Entities\Product::where('product_group', $item->product_group)->where('product_name_supplier', $symbol)->first();
+                                   $variation = App\Entities\Product::where('product_group', $item->product->product_group)->where('product_name_supplier', $symbol)->first();
 
                                    if ($variation) {
                                        $prices[] = $variation;
-                                       $totalCost += $variation?->price->net_special_price_basic_unit * $item->quantity;
+                                       $totalCost += $variation?->price->net_special_price_basic_unit * $item->product->quantity;
                                        dd($item);
                                    }
                                }
