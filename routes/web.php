d8dc40c5d8752b609e824ba3b2a0ec3d7f06607e
Antoni Seba <antoniwoj@o2.pl>
2024-05-11 13:37:45 +0200

    Make recalc from csv work

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Entities\ContactApproach;
 use App\Http\Controllers\ContactApproachController;
 use App\Http\Controllers\EmployeesController;
 use App\Http\Controllers\OrderDatatableColumnsManagementController;
@@ -821,4 +822,4 @@ Route::post('/form/{actionName}/{order}', [FormController::class, 'executeAction
 Route::get('newsletter/generate/{category}', [NewsletterController::class, 'generate'])->name('newsletter.generate');
 
 Route::get('polecenia', [ContactApproachController::class, 'index'])->name('contact-aproach.index');
-Route::get('set-aproach-as-done', [ContactApproachController::class, 'setDone'])->name('set-aproach-as-done');
+Route::get('set-aproach-as-done/{ContactApproach}', [ContactApproachController::class, 'setDone'])->name('set-aproach-as-done');
