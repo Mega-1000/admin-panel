c36f7ccdb366f6add382eba5d819a6c470500234
Antoni Seba <antoniwoj@o2.pl>
2024-05-12 12:15:05 +0200

    Make recalc from csv work

--- resources/views/chat/pricing_table.blade.php
+++ resources/views/chat/pricing_table.blade.php
@@ -89,10 +89,13 @@
 
     <input class="price gross_purchase_price_commercial_unit hidden" name="commercial_price_gross"
            value="{{$productPrices['commercial_price_gross'] ?? number_format($product->net_purchase_price_commercial_unit_after_discounts * 1.23, 2)}}"/>
-    <input class="price gross_purchase_price_basic_unit" name="basic_price_gross"
+
+    <input class="price gross_purchase_price_basic_unit hidden" name="basic_price_gross"
            value="{{ $productPrices['basic_price_gross'] ?? number_format($product->net_purchase_price_basic_unit_after_discounts * 1.23, 2)}}"/>
+
     <input class="price gross_purchase_price_calculated_unit hidden" name="calculated_price_gross"
            value="{{ $productPrices['calculated_price_gross'] ?? number_format($product->net_purchase_price_calculated_unit_after_discounts * 1.23, 2)}}"/>
+
     <input class="price gross_purchase_price_aggregate_unit hidden" name="aggregate_price_gross"
            value="{{ $productPrices['aggregate_price_gross'] ?? number_format($product->net_purchase_price_aggregate_unit_after_discounts * 1.23, 2)}}"/>
 @endif
