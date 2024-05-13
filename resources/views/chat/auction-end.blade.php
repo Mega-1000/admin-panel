54b7dd43214b699b983d5de6f00944bfa5d9642d
Antoni Seba <antoniwoj@o2.pl>
2024-05-13 11:59:25 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -210,7 +210,7 @@
                                    </span>
 
                                    @php
-                                       $totalCost += $auction->offers()->where('firm_id',$firm->firm->id)->where('order_item_id', $product->id)->orderBy('created_at', 'asc')->first()->basic_price_gross * $product->quantity;
+                                       $totalCost += $auction->offers()->where('firm_id',$firm->firm->id)->where('product_id', $product->id)->orderBy('created_at', 'asc')->first()->basic_price_gross * $product->quantity  ;
                                    @endphp
                                @else
                                    No offer
