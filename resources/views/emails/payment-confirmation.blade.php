9a3d1a66bddbe8f24199231ae5b0a00625c6b7ac
Antoni Seba <antoniwoj@o2.pl>
2024-05-13 19:51:18 +0200

    Make recalc from csv work

--- resources/views/emails/payment-confirmation.blade.php
+++ resources/views/emails/payment-confirmation.blade.php
@@ -1,7 +1,7 @@
 Dzień dobry,
 <br>
 <br>
-Pod następującym linkiem dostępne jest potwierdzenie przelewu wykonanego przez nas: {{ $cofirmation->file_url }}
+Pod następującym linkiem dostępne jest potwierdzenie przelewu wykonanego przez nas: {{ $this->confirmation->file_url }}
 <br>
 <br>
 Prosimy o potwierdzenie otrzymania przelewu klikając <a href="{{  }}">TUTAJ</a>
