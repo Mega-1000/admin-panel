262d609d80ed5fb7afbd0afd9866d3c8987357df
Antoni Seba <antoniwoj@o2.pl>
2024-05-14 15:40:00 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/warehouse.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/warehouse.blade.php
@@ -1,3 +1,4 @@
 @if(array_key_exists('warehouse', $data) && !empty($data['warehouse']))
+    eeee
     {!! $data['warehouse']['symbol'] ? '<a class="warehouse-symbol" href="/admin/warehouses/' . $data['warehouse']['symbol'] . '/editBySymbol">' . $data['warehouse']['symbol'] . '</a>' : '' !!}
 @endif
