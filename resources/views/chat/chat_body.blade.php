e78505f11430c23a976f7610d29f050f59516e04
Antoni Seba <antoniwoj@o2.pl>
2024-05-15 09:21:00 +0200

    Make recalc from csv work

--- resources/views/chat/chat_body.blade.php
+++ resources/views/chat/chat_body.blade.php
@@ -1,8 +1,14 @@
 <div class="panel panel-default" style="max-height: calc(100vh - 200px); overflow-y: auto;">
     <div class="panel-body chat-panel" style="border: 2px solid #000; border-radius: 7px;">
+        {{ dd($chatMessages) }}
         @if ( isset($chatMessages) )
             @foreach ($chatMessages as $message)
-                @include ('chat/single_message', ['message' => $message])
+                @if( $userType == MessagesHelper::TYPE_USER || isset($assignedMessagesIds[$message->id] ))
+                    @if ($userType != MessagesHelper::TYPE_USER && $message->area != 0)
+                        @continue
+                    @endif
+                    @include ('chat/single_message', ['message' => $message])
+                @endif
             @endforeach
         @endif
     </div>
