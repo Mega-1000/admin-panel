f83e0236b2cfc3e2fa326ac5dff42304f9c0b396
Antoni Seba <antoniwoj@o2.pl>
2024-05-15 17:18:17 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -30,7 +30,7 @@
         <h6>Załącz potwierdzenie przelewu</h6>
         <form action="{{ route('store-payment-confirmation', $order['id']) }}" method="post" enctype="multipart/form-data">
             @csrf
-            <input type="file" name="file" style="width: 10px">
+            <input type="file" name="file" style="width: 50px">
 
             <button class="btn btn-primary">
                 Wyślij plik
