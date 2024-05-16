870f9bee2a154eef7c22f03919269d229b6fb20c
Antoni Seba <antoniwoj@o2.pl>
2024-05-16 08:28:59 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -213,7 +213,7 @@
                                    </span>
 
                                    @php
-                                       $totalCost += $offer->basic_price_gross * $product->quantity;
+                                       $totalCost += collect($offers)->min('basic_price_gross') * $product->quantity;
                                    @endphp
                                @else
                                    No offer
