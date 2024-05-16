22812e6f64e27d7a27845896f5c1d2607c9385c0
Antoni Seba <antoniwoj@o2.pl>
2024-05-16 08:39:34 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -214,7 +214,7 @@
 
                                    @php
                                        $totalCost += collect($offers)->min('basic_price_gross') *
-                                       \App\Entities\OrderItem::where('order_id', $auction->chat->order->id)->whereHas('products', function ($q) use ($product) { $q->where('product_group', $product->product_group);})->first();
+                                       \App\Entities\OrderItem::where('order_id', $auction->chat->order->id)->whereHas('product', function ($q) use ($product) { $q->where('product_group', $product->product_group);})->first();
                                    @endphp
                                @else
                                    No offer
