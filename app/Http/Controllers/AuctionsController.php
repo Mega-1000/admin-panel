35488803b68ee74404c9459a5fa9246af83be8f4
Antoni Seba <antoniwoj@o2.pl>
2024-05-17 21:05:47 +0200

    Make recalc from csv work

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -167,11 +167,7 @@ class AuctionsController extends Controller
 
         foreach ($pricingData as $k => $item) {
             $product = Product::find($k)?->parentProduct ?? Product::find($k);
-            $fialItemsToUpdate = $firm
-                ->chatAuction
-                ->chat
-                ->order
-                ->items()
+            $fialItemsToUpdate = OrderItem::where('order_id', $firm->chatAuction->chat->order->id)
                 ->whereHas('product', function ($q) use ($product) { $q->where('parent_id', $product->id); })
                 ->orWhereHas('product', function ($q) use ($product) { $q->where('product_group', $product->product_group); })
                 ->get();
