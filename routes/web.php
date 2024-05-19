857150bfa021acf6e5d05b87b0c58b06d6562f0d
Antoni Seba <antoniwoj@o2.pl>
2024-05-19 18:25:47 +0200

    Make recalc from csv work

--- routes/web.php
+++ routes/web.php
@@ -393,6 +393,9 @@ Route::group(['prefix' => 'admin'], function () {
         Route::post('orders/find-page/{id}', 'OrdersController@findPage')->name('orders.findPage');
         Route::post('orders/find-by-dates', 'OrdersController@findByDates')->name('orders.findByDates');
         Route::delete('orders/{id}/', 'OrdersController@destroy')->name('orders.destroy');
+        Route::put('orders/{id}/update', [
+            'uses' => 'OrdersController@update',
+        ])->name('orders.update');
         Route::put('orders/{id}/updateSelf', [
             'uses' => 'OrdersController@updateSelf',
         ])->name('orders.updateSelf');
