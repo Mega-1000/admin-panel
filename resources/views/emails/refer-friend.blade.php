c13c2887c77067cb8062f79610c50cd1c82b36cc
Antoni Seba <antoniwoj@o2.pl>
2024-05-21 08:35:19 +0200

    Make recalc from csv work

---
+++ resources/views/emails/refer-friend.blade.php
@@ -0,0 +1,16 @@
+Dzień dobry,
+<br>
+<br>
+Zauważyliśmy, że niedawno złożyłeś zapytanie ofertowe na styropian.
+<br>
+<br>
+Chcieliśmy cię poinformować, że możesz zdobyć 30zł za każde pierwsze zamówienie wykonane przez użytkownika w naszym systemie.
+<br>
+<br>
+Brzmi interesująco? Pod linkiem: <a href="https://mega1000.pl/polec-znajomego">https://mega1000.pl/polec-znajomego</a> po zalogowaniu jest dostępny panel poleceń gdzie możesz wpisać numery telefonu osób zainteresowanych zakupem.
+<br>
+<br>
+<br>
+Z Pozdrowieniami
+<br>
+Zespół EPH Polska
