31a4f20ae6f1e174185d6f6f96e79b995520fcce
Antoni Seba <antoniwoj@o2.pl>
2024-05-23 17:55:31 +0200

    Make recalc from csv work

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -749,7 +749,7 @@
 
                 // Check if dateFrom is at least 10 hours less than dateTo
                 const tenHoursInMillis = 10 * 60 * 60 * 1000;
-                if (alert(dateToObj - dateFromObj) < tenHoursInMillis) {
+                if (dateFrom - dateTo < tenHoursInMillis) {
                     showAlert('danger', 'Data początkowa musi być co najmniej 10 godzin wcześniejsza niż data końcowa.');
                     $('#modifyDateModal').modal('hide');
                     return;
