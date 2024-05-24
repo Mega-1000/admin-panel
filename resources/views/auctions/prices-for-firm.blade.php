3e079d5ba75c76713f3639cb4f87b93dd71d43be
Antoni Seba <antoniwoj@o2.pl>
2024-05-24 09:27:00 +0200

    Make recalc from csv work

--- resources/views/auctions/prices-for-firm.blade.php
+++ resources/views/auctions/prices-for-firm.blade.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.chat_name') }}</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+
 @if($firms->count() == 0)
     <div class="text-center">
         <h1>Tu za nie długo zaczną wyświetlać się wyniki twojego przetargu.</h1>
