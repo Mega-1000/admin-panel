fa06c1cfba78ae4ed3888ace3a3c5750c46662b8
Antoni Seba <antoniwoj@o2.pl>
2024-05-24 10:48:50 +0200

    Make recalc from csv work

--- app/Repositories/ChatAuctionOffers.php
+++ app/Repositories/ChatAuctionOffers.php
@@ -44,7 +44,19 @@ class ChatAuctionOffers
      */
     public static function getFirmsForAuctionOfferForEmailRemider(ChatAuctionOffer $chatAuctionOffer): \Illuminate\Support\Collection
     {
-        return ChatAuctionOffer::query()->join('firms', 'chat_auction_offers.firm_id', '=', 'firms.id')->where('send_notification', true)->where('chat_auction_id', $chatAuctionOffer->chat_auction_id)->whereHas('product', function ($q) use ($chatAuctionOffer) {$q->where('product_group', $chatAuctionOffer->product->product_group)->where('additional_info1', $chatAuctionOffer->product->additional_info1);})->select('chat_auction_offers.*', 'firms.email')->groupBy('firms.email')->where('commercial_price_net', '>', $chatAuctionOffer->commercial_price_net)->get();
+        return ChatAuctionOffer::query()
+            ->join('firms', 'chat_auction_offers.firm_id', '=', 'firms.id')
+            ->where('send_notification', true)
+            ->where('chat_auction_id', $chatAuctionOffer->chat_auction_id)
+            ->whereHas('product', function ($q) use ($chatAuctionOffer) {
+                $q->where('product_group', $chatAuctionOffer->product->product_group)
+                    ->where('additional_info1', $chatAuctionOffer->product->additional_info1);
+            })
+            ->select('chat_auction_offers.*', 'firms.email')
+            ->groupBy('firms.email')
+            ->where('commercial_price_net', '<', $chatAuctionOffer->commercial_price_net)
+            ->where('firm_id', '!=', $chatAuctionOffer->firm_id)
+            ->get();
     }
 
 
