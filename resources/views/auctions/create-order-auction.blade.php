856d092e399fee92d38d33daf7c0c13de84aea6c
Antoni Seba <antoniwoj@o2.pl>
2024-05-24 15:06:27 +0200

    Make recalc from csv work

--- resources/views/auctions/create-order-auction.blade.php
+++ resources/views/auctions/create-order-auction.blade.php
@@ -20,7 +20,7 @@
             @foreach($item as $product)
                 <span>
                     Nazwa produktu: {{ $product->name }} Ilość: {{ $product->quantity }} Cena:
-                    {{ \App\Entities\ChatAuctionOffer::where('product_id', $product->id)->first()->commercial_price_gross ?? $product->basic_price_gross }}
+                    {{ \App\Entities\ChatAuctionOffer::where('product_id', $product->id)->first()?->commercial_price_gross ?? $product->basic_price_gross }}
                     @if($item->count() > 1) <input type="checkbox"> @endif
                 </span>
             @endforeach
