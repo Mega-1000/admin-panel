ccca811ade80c156b71d5f8be99f1114e4fad849
Antoni Seba <antoniwoj@o2.pl>
2024-05-25 12:14:56 +0200

    test

--- app/Helpers/ClauteHelper.php
+++ app/Helpers/ClauteHelper.php
@@ -2,48 +2,37 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\Request;
+use Anthropic\Anthropic;
+
+
 class ClauteHelper
 {
-    public function getResponse(): mixed
+    protected $anthropic;
+
+    public function __construct()
     {
-        $url = 'https://api.anthropic.com/v1/messages';
-        $apiKey = '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';
-        $model = 'claude-instant-1.2';
-        $maxTokens = 2048;
-        $temperature = 0.6;
-        $messages = [
-            [
-                'content' => 'You are a travel advisor that will deliver a detailed itinerary based on the information provided by the user during th  season. I am traveling to Denver, CO.'
-            ]
+        $headers = [
+            'anthropic-version' => '2023-06-01',
+            'anthropic-beta' => 'messages-2023-12-15',
+            'content-type' => 'application/json',
+            'x-api-key' => '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'
         ];
-        $data = [
-            'model' => $model,
-            'max_tokens' => $maxTokens,
-            'temperature' => $temperature,
-            'messages' => $messages
-        ];
-        $jsonData = json_encode($data);
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => $jsonData,
-            CURLOPT_HTTPHEADER => [
-                'x-api-key: ' . $apiKey,
-                'anthropic-version: 2023-06-01',
-                'content-type: application/json'
-            ],
-        ]);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        $responseData = json_decode($response, true);
-        dd($responseData);
-        return $responseData['content'][0]['text'];
+
+        $this->anthropic = Anthropic::factory()
+            ->withHeaders($headers)
+            ->make();
+    }
+
+    /**
+     * For sending server event
+     */
+    private function send($event, $data): void
+    {
+        echo "event: {$event}\n";
+        echo 'data: ' . $data;
+        echo "\n\n";
+        ob_flush();
+        flush();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -844,3 +844,72 @@ Route::get('mark-chat-as-finished/{chat}', [MessagesController::class, 'markChat
 
 Route::get('/make-order/{firm:symbol}/{order}', [AuctionsController::class, 'makeOrder'])->name('create-order-from-auction');
 Route::post('/submit-order/{order}', [AuctionsController::class, 'submitOrder']);
+
+Route::get('claute', function (\Illuminate\Http\Client\Request $request) {
+    $claute = app(\App\Helpers\ClauteHelper::class);
+        $question = $request->query('question');
+        return response()->stream(
+            function () use (
+                $question
+            ) {
+                $result_text = "";
+                $last_stream_response = null;
+
+                $model = 'claude-3-opus-20240229';
+                $max_tokens = 4096;
+                $systemMessage = 'You are a helpfull assistant. Answer as concisely as possible.';
+                $temperature = 1;
+                $messages = [
+                    [
+                        'role' => 'user',
+                        'content' => $question
+                    ]
+                ];
+
+                $headers = [
+                    'anthropic-version' => '2023-06-01',
+                    'anthropic-beta' => 'messages-2023-12-15',
+                    'content-type' => 'application/json',
+                    'x-api-key' => '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'
+                ];
+
+                $claute = Anthropic::factory()
+                    ->withHeaders($headers)
+                    ->make();
+
+                $stream = $claute->chat()->createStreamed([
+                    'model' => $model,
+                    'temperature' => $temperature,
+                    'max_tokens' => $max_tokens,
+                    'system' => $systemMessage,
+                    'messages' => $messages,
+                ]);
+
+
+                foreach ($stream as $response) {
+                    $text = $response->choices[0]->delta->content;
+                    if (connection_aborted()) {
+                        break;
+                    }
+                    $data = [
+                        'text' => $text,
+                    ];
+                    $this->send("update", json_encode($data));
+                    $result_text .= $text;
+                    $last_stream_response = $response;
+                }
+
+                $this->send("update", "<END_STREAMING_SSE>");
+
+                logger($last_stream_response->usage->toArray());
+            },
+            200,
+            [
+                'Cache-Control' => 'no-cache',
+                'Connection' => 'keep-alive',
+                'X-Accel-Buffering' => 'no',
+                'Content-Type' => 'text/event-stream',
+            ]
+        );
+});
+
