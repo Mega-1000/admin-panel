89e605e11fb29d26fd089da5fe6b7e872806aec9
Antoni Seba <antoniwoj@o2.pl>
2024-05-25 12:52:20 +0200

    Make recalc from csv work

--- app/Http/Controllers/Api/MessagesController.php
+++ app/Http/Controllers/Api/MessagesController.php
@@ -58,7 +58,7 @@ class MessagesController extends Controller
                 "model" => "claude-3-opus-20240229",
                 "max_tokens" => 1024,
                 "messages" => [
-                    ["role" => "system", "content" => 'You are part of my larvel system. You have to detect if user wants to add employee of company to the chat if so provide me json response like this
+                    ["role" => "user", "content" => 'You are part of my larvel system. You have to detect if user wants to add employee of company to the chat if so provide me json response like this
 { "AddCompany": "COMPANY NAME", "NoticeForUser": "change it to message for user", }
 if user wants to add some compoany wich is not in list provide response like this { "NoticeForUser": "change it to message for user", }
 There are only these companies: "IZOTERM" "POLSTYR" "SWISSPOR"
