15b3787ea84b5705c7482ce21d5fda3d0fdd2e69
Antoni Seba <antoniwoj@o2.pl>
2024-05-27 16:09:19 +0200

    Make recalc from csv work

---
+++ database/migrations/2024_05_27_160759_update_employees_table_add_multiple_zip_codes.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->string('zip_code_2')->nullable()->change();
+            $table->string('zip_code_3')->nullable()->change();
+            $table->string('zip_code_4')->nullable()->change();
+            $table->string('zip_code_5')->nullable()->change();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
