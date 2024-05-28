96e6d2a8425beb3e729eca76c6f0d28b3601b578
Antoni Seba <antoniwoj@o2.pl>
2024-05-28 16:06:43 +0200

    Make recalc from csv work

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -233,14 +233,15 @@ class MessagesController extends Controller
 
             $allEmployeesFromRelatedOrders = $this->productService->getUsersFromVariations($order);
             $emails = $chat->users->pluck('email');
-            $allEmployeesFromRelatedOrders->each(fn (&$employee) =>  $employee->finalRadius = LocationHelper::getDistanceOfClientToEmployee($employee, $order->customer));
+            $allEmployeesFromRelatedOrders->each(fn (&$employee) =>  $employee->finalRadius = $employee->id == 80 ? LocationHelper::getDistanceOfClientToEmployee($employee, $order->customer) : '');
+
 
 
             $allEmployeesFromRelatedOrders = $allEmployeesFromRelatedOrders
                 ->groupBy('firm_id')
                 ->map(function ($group) {
                 return $group->reduce(function ($carry, $item) {
-                    return ($carry === null || $item->finalRadius < $carry->finalRadius) ? $item : $carry;
+                    return ($carry === null || $item->finalRadius > $carry->finalRadius) ? $item : $carry;
                 });
             });
         }
