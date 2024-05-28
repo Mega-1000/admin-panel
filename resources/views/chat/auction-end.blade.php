68e09a97fd77474e1e06f1b4f9d7832b5a0da6d8
Antoni Seba <antoniwoj@o2.pl>
2024-05-28 07:28:18 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -202,11 +202,13 @@
                                        foreach ($allProductsToBeDisplayed as $product) {
                                            if ($auction->offers->where('firm_id', $firm->firm->id)->where('product_id', $product->id)->first()) {
                                                $offers[] = $auction->offers()
-                                                ->whereHas('product', function ($q) use ($product) {
-                                                    $q->where('parent_id', $product->parent_id);
-                                                })
+                                                ->where('product_id', $product->id)
                                                 ->orderBy('basic_price_net', 'desc')
                                                 ->first();
+
+                                               //                       ->whereHas('product', function ($q) use ($product) {
+//                                                    $q->where('parent_id', $product->parent_id);
+//                                                })
                                            }
                                        }
 
