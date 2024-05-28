a91c28cff73bad66b09861df9286edf28ab9c400
Antoni Seba <antoniwoj@o2.pl>
2024-05-28 10:53:14 +0200

    Make recalc from csv work

--- resources/views/auctions/create-order-auction.blade.php
+++ resources/views/auctions/create-order-auction.blade.php
@@ -146,7 +146,7 @@
         })
             .then(async (data) => {
                 await Swal.fire('Sukces', 'Pomyślnie złożono zamówienie. Zostaniesz przekierowany do banku', 'success')
-                window.location.href = 'https://mega1000.pl/payment?token={{ $order->token }}&total={{ $order->getValue() }}'
+                window.location.href = `https://mega1000.pl/payment?token={{ $order->token }}&total=${totalPrice + 50}`
             })
             .catch(error => {
                 console.error('Error:', error);
