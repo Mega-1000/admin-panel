29d6a5df467dd312a1a296322235d40edbf7e552
Antoni Seba <antoniwoj@o2.pl>
2024-05-28 06:50:14 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -211,7 +211,7 @@
                                    @endphp
 
                                    @if(!empty($offers))
-                                       {{ inplode(' ', $offers) }}
+                                       {{ implode(' ', $offers) }}
                                        @foreach($offers as $offer)
                                            {{ \App\Entities\Product::find($offer->product_id)->additional_info1 }}:
                                            {{ round($offer->basic_price_net * 1.23, 2) }}
