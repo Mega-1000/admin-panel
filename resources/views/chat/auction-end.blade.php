59f3b4c80e066eeda42ddad95c8da62efdc78bed
Antoni Seba <antoniwoj@o2.pl>
2024-06-04 14:09:16 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -191,12 +191,10 @@
 
                                     $offers = [];
                                     foreach ($allProductsToBeDisplayed as $product) {
-                                        if ($auction->offers->where('firm_id', $firm->firm->id)->where('product_id', $product->id)->first()) {
                                             $offers[] = \App\Entities\ChatAuctionOffer::whereHas('product', function ($q) use ($product) {$q->where('parent_id', $product->parent_id);})
                                                 ->where('chat_auction_id', $auction->id)
                                                 ->orderBy('basic_price_net', 'asc')
                                                 ->first();
-                                        }
                                     }
 
                                     usort($offers, function($a, $b) {
