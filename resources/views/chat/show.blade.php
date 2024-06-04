0be8fa7580b6ed349337f1f862e5e5943f74c0c8
Antoni Seba <antoniwoj@o2.pl>
2024-06-04 22:05:01 +0200

    Make recalc from csv work

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -171,7 +171,7 @@
                                 </form>
                             @endif
                         @endif
-                        @if(!empty($chat->auctions->first()) && !$order->auction_order_placed)
+                        @if((!empty($chat->auctions->first()) && !$order->auction_order_placed) || $userType == MessagesHelper::TYPE_EMPLOYEE)
                             <br>
                             <a class="btn btn-primary" href="{{ route('auctions.end', ['auction' => $chat->auctions->first()->id]) }}">
                                 Zobacz wyniki przetargu
@@ -183,7 +183,13 @@
                                 Zamówienie zostało złożone i wysłane do fabryki
                             </h1>
                         @endif
-                        @if(($userType === MessagesHelper::TYPE_USER && $chat->auctions->count() > 0 && $chat->auctions->first()?->confirmed === 0) || $userType == MessagesHelper::TYPE_EMPLOYEE)
+                        @if(
+                            (
+                                $userType === MessagesHelper::TYPE_USER
+                                && $chat->auctions->count() > 0 && $chat->auctions->first()?->confirmed === 0
+                            )
+                             || $userType == MessagesHelper::TYPE_EMPLOYEE
+                         )
                             <form method="post" action="{{ route('auctions.confirm', ['auction' => $chat->auctions->first()->id]) }}">
                                 @csrf
                                 <button class="btn btn-success">
