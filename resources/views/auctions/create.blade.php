50f7cbe9d9dacbc4a0236fd6db5aaeec2246c630
Antoni Seba <antoniwoj@o2.pl>
2024-06-11 13:44:07 +0200

    Make recalc from csv work

--- resources/views/auctions/create.blade.php
+++ resources/views/auctions/create.blade.php
@@ -82,7 +82,9 @@
                     Zatwierdź
                 </button>
             @else
-                Uwaga!!! Masz już przetarg na swoim koncie zawierający jeden z tych produktów jeśli potrzebujesz zmienić swój przetarg zadzwoń pod numer 507 925 963
+                <div style="color: red">
+                    Uwaga!!! Masz już przetarg na swoim koncie zawierający jeden z tych produktów jeśli potrzebujesz zmienić swój przetarg zadzwoń pod numer 507 925 963
+                </div>
             @endif
             <div id="spinner" class="hidden">
                 <svg class="animate-spin h-5 w-5 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
