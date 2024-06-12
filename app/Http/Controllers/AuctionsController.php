3f660ce176856fdaa15fa83c4152050afc6421f2
Antoni Seba <antoniwoj@o2.pl>
2024-06-12 13:45:40 +0200

    Make recalc from csv work

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -555,17 +555,17 @@ class AuctionsController extends Controller
             );
 
             $item = $order->items()->where('order_id', $order->id)->where('product_id', $product->id)->first();
-            $item->gross_selling_price_commercial_unit = $offer?->basic_price_net * 1.23 ?? $product->gross_selling_price_commercial_unit;
+            $item->gross_selling_price_commercial_unit = ($offer?->basic_price_net * 1.23 ?? $product->gross_selling_price_commercial_unit) / $product->pagking->numbers_of_basic_commercial_units_in_pack;
             $item->net_selling_price_basic_unit = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit;
             $item->gross_selling_price_basic_unit = $offer?->basic_price_net * 1.23 ?? $product->gross_selling_price_commercial_unit;
 
             $item->net_purchase_price_basic_unit = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit;
-            $item->net_purchase_price_commercial_unit = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit / $product->pagking->numbers_of_basic_commercial_units_in_pack;
+            $item->net_purchase_price_commercial_unit = ($offer?->basic_price_net ?? $product->net_selling_price_commercial_unit) / $product->pagking->numbers_of_basic_commercial_units_in_pack;
 
-            $item->net_purchase_price_commercial_unit_after_discounts = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit / $product->pagking->numbers_of_basic_commercial_units_in_pack;
+            $item->net_purchase_price_commercial_unit_after_discounts = ($offer?->basic_price_net ?? $product->net_selling_price_commercial_unit) / $product->pagking->numbers_of_basic_commercial_units_in_pack;
             $item->net_purchase_price_basic_unit_after_discounts = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit;
-            $item->net_purchase_price_calculated_unit_after_discounts = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit / $product->pagking->numbers_of_basic_commercial_units_in_pack;
-            $item->net_purchase_price_aggregate_unit_after_discounts = $offer?->basic_price_net ?? $product->net_selling_price_commercial_unit / $product->pagking->numbers_of_basic_commercial_units_in_pack;
+            $item->net_purchase_price_calculated_unit_after_discounts = ($offer?->basic_price_net ?? $product->net_selling_price_commercial_unit) / $product->pagking->numbers_of_basic_commercial_units_in_pack;
+            $item->net_purchase_price_aggregate_unit_after_discounts = ($offer?->basic_price_net ?? $product->net_selling_price_commercial_unit) / $product->pagking->numbers_of_basic_commercial_units_in_pack;
 
             $item->save();
 
