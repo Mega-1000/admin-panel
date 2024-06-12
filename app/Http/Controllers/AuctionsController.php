8389a445a60597c24601028843492d63d2c219d2
Antoni Seba <antoniwoj@o2.pl>
2024-06-12 12:53:26 +0200

    Make recalc from csv work

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -555,8 +555,8 @@ class AuctionsController extends Controller
             );
 
             $item = $order->items()->where('order_id', $order->id)->where('product_id', $product->id)->first();
-            $item->gross_selling_price_commercial_unit = $offer?->basic_price_gross ?? $product->gross_selling_price_commercial_unit;
-            $item->saveQuietly();
+            $item->gross_selling_price_commercial_unit = $offer?->basic_price_net * 1.23 ?? $product->gross_selling_price_commercial_unit;
+            $item->save();
 
             Log::notice(($offer?->basic_price_net * 1.23 ?? $product->gross_selling_price_commercial_unit) . $item->id . $item->gross_selling_price_commercial_unit);
 
