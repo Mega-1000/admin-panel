7042fc96d7b55440c4f0b5cf746a97358c2e146f
Antoni Seba <antoniwoj@o2.pl>
2024-06-17 21:26:06 +0200

    Make recalc from csv work

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -426,7 +426,7 @@ class ProductsController extends Controller
         $product->load('opinions');
         $product->load('price');
 
-        $product->similarProducts = $product->price->products()->whereHas('children')->with('price')->get();
+        $product->similarProducts = $product->category?->products()->whereHas('children')->with('price')->get();
 
         $product->meanOpinion = $product->opinions->avg('rating') ?? 0;
 
