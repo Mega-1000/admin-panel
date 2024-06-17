618c007d2ad181f0d5e5c8608f3940084ae790dd
Antoni Seba <antoniwoj@o2.pl>
2024-06-17 07:17:51 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -6,7 +6,7 @@
 
         @foreach($order['files'] as $file)
             <a href="{{ route('orders.getFile', ['id' => $order['id'], 'file_id' => $file['hash']]) }}" target="_blank">
-                {{ str_split(6, $file['file_name'])}}
+                {{ str_split($file['file_name'] ?? 'brak',6)}}
             </a>
 
             <button onclick="getFilesList({{ $order['id'] }})">
