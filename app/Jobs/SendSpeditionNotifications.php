7902075eafe0139d41b2c4ae17ca81f56e5db7c7
Antoni Seba <antoniwoj@o2.pl>
2024-06-18 09:17:40 +0200

    Make recalc from csv work

--- app/Jobs/SendSpeditionNotifications.php
+++ app/Jobs/SendSpeditionNotifications.php
@@ -81,13 +81,13 @@ class SendSpeditionNotifications implements ShouldQueue
             $currentHour = date('H');
             $currentMinute = date('i');
 
-            if ($order->id == 85773) {
-                dd('test');
-            }
-
-            if (($currentHour == 7 && $currentMinute >= 0 && $currentMinute <= 30) || $currentHour >= 12) {
+            if (($currentHour == 9 && $currentMinute >= 0 && $currentMinute <= 30) || $currentHour >= 10) {
                 if ($fromDate->isPast() && $toDate->isFuture() && !Carbon::create($order->last_confirmation)->isToday() && !$order->special_data_filled && $order?->warehouse?->warehouse_email) {
                     $this->updateOrderLabels($order, [244]);
+
+                    if ($currentHour >= 10) {
+                        $this->updateOrderLabels($order, [270]);
+                    }
 
                     try {
                         Mailer::create()
