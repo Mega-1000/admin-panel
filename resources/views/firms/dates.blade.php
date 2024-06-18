cf16d6a9e4ae5c70c07b22252c134a630b46e1a4
Antoni Seba <antoniwoj@o2.pl>
2024-06-18 17:52:22 +0200

    Make recalc from csv work

--- resources/views/firms/dates.blade.php
+++ resources/views/firms/dates.blade.php
@@ -62,7 +62,7 @@
     </div>
 </div>
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
 <script>
