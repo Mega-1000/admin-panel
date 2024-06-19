fe830bc3eb61434378f3caac65bbfb1d07e53281
Antoni Seba <antoniwoj@o2.pl>
2024-06-19 12:58:36 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
@@ -45,7 +45,7 @@
         @endphp
         <div class="message-container">
             <span class="message-preview">{{ $firstFiveWords }}...</span>
-{{--            <span class="message-full">{{ $messageText }}</span>--}}
+            <span class="message-full">{{ $messageText }}</span>
         </div>
         - {{ \Carbon\Carbon::parse(explode('.', $message['created_at'])[0])->addHours(2) }}
         {{ $userType }}
