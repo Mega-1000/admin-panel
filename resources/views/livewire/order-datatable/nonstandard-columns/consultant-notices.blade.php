042143e34946f0d926c38d118325d0d25d98010f
Antoni Seba <antoniwoj@o2.pl>
2024-06-21 11:17:36 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/consultant-notices.blade.php
@@ -45,10 +45,10 @@
             $firstFiveWords = implode(' ', array_slice($words, 0, 5));
         @endphp
         <div class="message-container">
-            <span class="message-preview"     onmouseover="showMessageInformations('{{ $order['id'] }}')"
-                  onmouseout="hideMessageInformations('{{ $order['id'] }}')">{{ $firstFiveWords }}...</span>
+            <span class="message-preview"     onmouseover="showMessageInformations('{{ $order['created_at'] }}')"
+                  onmouseout="hideMessageInformations('{{ $order['created_at'] }}')">{{ $firstFiveWords }}...</span>
 
-            <div class="tooltip-phone-info"  id="tooltip-message-{{ $order['id'] }}" style="display: none">
+            <div class="tooltip-phone-info"  id="tooltip-message-{{ $order['created_at'] }}" style="display: none">
                 {{ $messageText }}
             </div>
         </div>
--- resources/views/order-datatable-index.blade.php
+++ resources/views/order-datatable-index.blade.php
@@ -356,7 +356,6 @@
     </script>
 
     <script>
-
         const showMessageInformations = (id) => {
             const phoneInformations = document.getElementById('tooltip-phone-info-' + id);
             phoneInformations.style.display = 'block';
