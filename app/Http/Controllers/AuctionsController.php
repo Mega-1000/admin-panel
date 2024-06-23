0e63d6ff81248d57be018af0bf2270ff06081561
Antoni Seba <antoniwoj@o2.pl>
2024-06-23 20:25:56 +0200

    Make recalc from csv work

--- app/Http/Controllers/AuctionsController.php
+++ app/Http/Controllers/AuctionsController.php
@@ -460,6 +460,7 @@ class AuctionsController extends Controller
             ->unique('product_name_supplier');
 
         foreach ($products as $product) {
+            dd($product->firm);
             LocationHelper::getAvaiabilityOfProductForZipCode($product, \request()->query('zipCode'));
         }
 
