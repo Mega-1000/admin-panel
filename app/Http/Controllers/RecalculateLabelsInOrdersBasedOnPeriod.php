4320d54acb955f8137821de4443b04195e25c801
Antoni Seba <antoniwoj@o2.pl>
2024-06-26 08:34:58 +0200

    Make recalc from csv work

--- app/Http/Controllers/RecalculateLabelsInOrdersBasedOnPeriod.php
+++ app/Http/Controllers/RecalculateLabelsInOrdersBasedOnPeriod.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Entities\Order;
 use App\Helpers\OrderDepositPaidCalculator;
 use App\Http\Requests\RecalculateLabelsInOrdersBasedOnPeriodRequest;
+use App\Jobs\DispatchLabelEventByNameJob;
 use App\Services\Label\AddLabelService;
 use App\Services\Label\RemoveLabelService;
 use App\Services\OrderPaymentLabelsService;
@@ -33,12 +34,26 @@ class RecalculateLabelsInOrdersBasedOnPeriod extends Controller
         $orders = $query->get();
 
         foreach ($orders as $order) {
-            if ($request->get('calculate-only-with-39')) {
-                continue;
+            if (count($order->payments)) {
+                if ($order->isPaymentRegulated()) {
+                    dispatch(new DispatchLabelEventByNameJob($order, "payment-equal-to-order-value"));
+                } else {
+                    if (!$order->labels->contains('id', 240)) {
+                        dispatch(new DispatchLabelEventByNameJob($order, "required-payment-before-unloading"));
+                    }
+                }
             }
-            $arr = [];
+
+            $hasMissingDeliveryAddressLabel = $order->labels()->where('label_id', 75)->get();
+
+            if (count($hasMissingDeliveryAddressLabel) > 0) {
+                if ($order->isDeliveryDataComplete()) {
+                    dispatch(new DispatchLabelEventByNameJob($order, "added-delivery-address"));
+                }
+            }
 
             $this->orderPaymentLabelsService->calculateLabels($order);
+            $arr = [];
 
             $additional_service = $order->additional_service_cost ?? 0;
             $additional_cod_cost = $order->additional_cash_on_delivery_cost ?? 0;
@@ -55,23 +70,18 @@ class RecalculateLabelsInOrdersBasedOnPeriod extends Controller
 
             $sumOfGrossValues = $totalProductPrice + $additional_service + $additional_cod_cost + $shipment_price_client;
 
-            if ($order->payments()->count()) {
-                return redirect()->back();
-            }
-
             if (
                 round(round($sumOfGrossValues, 2) + round($depositPaidData['returnedValue'], 2) - round($depositPaidData['balance'], 2) - round($depositPaidData['wtonValue'], 2) - round($depositPaidData['externalFirmValue'], 2)) == 0.0 &&
                 $order->payments->count() > 0
             ) {
                 $order = Order::find($order->id);
                 $LpArray = [];
-                RemoveLabelService::removeLabels($order, [39], $LpArray, [], Auth::user()->id);
+                RemoveLabelService::removeLabels($order, [39], $LpArray, [], Auth::user()?->id);
             } else {
                 if (!$order->labels->contains('id', 240)) {
                     AddLabelService::addLabels($order, [39], $arr, [], Auth::user()?->id);
                 }
             }
-
         }
 
         return redirect()->back();
