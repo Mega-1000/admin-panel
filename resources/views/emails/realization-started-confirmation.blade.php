8712d83522e9b0aafa16bc025a63c5e9ea68819a
Antoni Seba <antoniwoj@o2.pl>
2024-06-28 14:20:26 +0200

    Make recalc from csv work

--- resources/views/emails/realization-started-confirmation.blade.php
+++ resources/views/emails/realization-started-confirmation.blade.php
@@ -5,7 +5,7 @@ Zauważyliśmy, że zamówienie o numerze {{ $order->id }} zostało zatwierdzone
 <br>
 <br>
 Prosimy o opłacenie zamówienia pod linkiem
-<a href="https://admin.mega1000.pl/order-proform-pdf/{{ $order->id}}">
+<a href="https://mega1000.pl/payment/{{ $order->token}}">
     FAKTURA PROFORMA
 </a>
 <br>
