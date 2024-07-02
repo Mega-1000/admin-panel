ec8fad9a6dcc2ba898dfac1d63b0b8c40149cd68
Antoni Seba <antoniwoj@o2.pl>
2024-07-02 14:38:22 +0200

    Make recalc from csv work

--- resources/views/create-avisation-fast.blade.php
+++ resources/views/create-avisation-fast.blade.php
@@ -1,29 +1,44 @@
-<form action="{{ route('storeAvisation', $order->id) }}" method="POST">
-    @csrf
+@extends('layouts.datatable')
 
-    Kwota wpłaty
-    <input type="text" name="declared_sum">
-    <br>
-    <br>
+@section('app-header')
+    <h1 class="page-title">
+        <i class="voyager-move"></i> @lang('import.title')
+    </h1>
+@endsection
+
+@section('app-content')
+    <form action="{{ route('storeAvisation', $order->id) }}" method="POST">
+        @csrf
+
+        Kwota wpłaty
+        <input type="text" name="declared_sum">
+        <br>
+        <br>
 
 
-    Magazyn do awizacji
-    <input type="text" value="{{ $order->warehouse?->symbol }}" name="warehouse-symbol">
-    <br>
-    <br>
+        Dostępne magazyny:
+        @foreach($order->items->first()->manufacturer->warehouses as $warehouse)
+            {{ $warehouse->symbol }}
+        @endforeach
+        Magazyn do awizacji
+        <input type="text" value="{{ $order->warehouse?->symbol }}" name="warehouse-symbol">
+        <br>
+        <br>
 
-    Rodzaj transportu
-    <select>
-        <option value="1">
-            Transport Fabryczny
-        </option>
-        <option value="2">
-            Odbiór osobisty
-        </option>
-    </select>
+        Rodzaj transportu
+        <select>
+            <option value="1">
+                Transport Fabryczny
+            </option>
+            <option value="2">
+                Odbiór osobisty
+            </option>
+        </select>
+        <br>
 
-    <button class="btn btn-primary">
-        Zatwierdź
-    </button>
+        <button class="btn btn-primary">
+            Zatwierdź
+        </button>
 
-</form>
+    </form>
+@endsection
