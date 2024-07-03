a56ba3c01e64199e70443d107a07d6d540f4ff1a
Antoni Seba <antoniwoj@o2.pl>
2024-07-03 11:29:57 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -66,7 +66,7 @@
             $date = false;
 
             try {
-                $date = \Carbon\Carbon::create('last_confirmation')->isToday();
+                $date = \Carbon\Carbon::create($order['last_confirmation'])->isToday();
             } catch (\Exception $e) {
             }
         @endphp
