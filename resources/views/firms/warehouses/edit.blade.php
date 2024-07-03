3541a30e31ec5eb8ab9054f947ebd3666bf6528f
Antoni Seba <antoniwoj@o2.pl>
2024-07-03 09:36:20 +0200

    Make recalc from csv work

--- resources/views/firms/warehouses/edit.blade.php
+++ resources/views/firms/warehouses/edit.blade.php
@@ -62,6 +62,11 @@
                                            value="{{ $warehouse->warehouse_email ?? '' }}">
                                 </div>
                                 <div class="form-group">
+                                    <label for="shipment-after-pay-email">Email odziału zajmującego się logistyką po wpłacie</label>
+                                    <input type="email" class="form-control" id="shipment-after-pay-email" name="shipment-after-pay-email"
+                                           value="{{ $warehouse->shipment_after_pay_email ?? '' }}">
+                                </div>
+                                <div class="form-group">
                                     <label for="status">@lang('warehouses.form.status')</label>
                                     <select class="form-control text-uppercase" name="status">
                                         <option value="ACTIVE">@lang('warehouses.form.active')</option>
