e61e39ab6f98c858ea50b810539ebd9c4e57170d
Antoni Seba <antoniwoj@o2.pl>
2024-07-03 15:54:22 +0200

    Make recalc from csv work

--- resources/views/emails/payment-confirmation.blade.php
+++ resources/views/emails/payment-confirmation.blade.php
@@ -4,4 +4,12 @@ Dzień dobry,
 Pod następującym linkiem dostępne jest potwierdzenie przelewu wykonanego przez nas: <a href="{{ $confirmation->file_url }}"> {{ $confirmation->file_url }} </a>
 <br>
 <br>
-Prosimy o potwierdzenie otrzymania przelewu klikając <a href="{{ route('store-payment-confirmation-confirm', \App\Entities\Order::find($confirmation->order_id)->id) }}">TUTAJ</a>
+Prosimy o potwierdzenie otrzymania tej wiadomości klikając <a href="{{ route('store-payment-confirmation-confirm', \App\Entities\Order::find($confirmation->order_id)->id) }}">TUTAJ</a>
+<br>
+<br>
+W przypadku braku potwierdzenia odczytania tej wiadomości będziemy zmuszeni do ponownego wysyłania jej co 15 minut.
+<br>
+<br>
+Pozdrawiamy,
+<br>
+Zespół EPH Polska
