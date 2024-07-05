63b810726f86af3e0ce65f35eb0f5f39f70ca1b8
Antoni Seba <antoniwoj@o2.pl>
2024-07-05 21:36:17 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -422,8 +422,7 @@
                     </a>
 
                     @if(auth()->id())
-                        <a class="btn btn-secondary" href="https://admin.mega1000.pl/auctions/offer/create/
-                        {{ App\Entities\ChatAuctionFirm::where('firm_id', App\Entities\Firm::where('symbol', $symbol)->first()->id)->where('chat_auction_id', $order->chat->auctions->first()->id)->first()?->token }}">
+                        <a class="btn btn-secondary" href="https://admin.mega1000.pl/auctions/offer/create/{{ App\Entities\ChatAuctionFirm::where('firm_id', App\Entities\Firm::where('symbol', $symbol)->first()->id)->where('chat_auction_id', $order->chat->auctions->first()->id)->first()?->token }}">
                             Dodaj cenę jako ta firma
                         </a>
                     @endif
