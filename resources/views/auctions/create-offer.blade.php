536909d49f29f05d138bb1d9f61beac79548f30e
Antoni Seba <antoniwoj@o2.pl>
2024-07-07 11:43:24 +0200

    Make recalc from csv work

--- resources/views/auctions/create-offer.blade.php
+++ resources/views/auctions/create-offer.blade.php
@@ -270,7 +270,7 @@
     }).addTo(map);
 
     // Origin zip code
-    var originZipCode = "{{ \App\Helpers\LocationHelper::nearestWarehouse($chat_auction_firm->chatAuction->chat->order->customer, $chat_auction_firm->firm)->address->zip_code }}";
+    var originZipCode = "{{ \App\Helpers\LocationHelper::nearestWarehouse($chat_auction_firm->chatAuction->chat->order->customer, $chat_auction_firm->firm)->address->postal_code }}";
 
     // Destination zip code (from the existing data)
     var destZipCode = "{{ $chat_auction_firm->chatAuction->chat->order->addresses->first()->postal_code }}";
