c96800b895b09984f4307b62b83f1c7553298191
Antoni Seba <antoniwoj@o2.pl>
2024-07-08 09:58:17 +0200

    Make recalc from csv work

--- resources/views/chat/auction-end.blade.php
+++ resources/views/chat/auction-end.blade.php
@@ -162,7 +162,7 @@
                                 </div>
                             @else
                                 <table class="min-w-full divide-y divide-gray-200 ">
-                                    <thead class="bg-gray-50 text-white">
+                                    <thead class="bg-gray-50 text-white" style="color: white">
                                     <tr>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                             Ceny brutto za m3
