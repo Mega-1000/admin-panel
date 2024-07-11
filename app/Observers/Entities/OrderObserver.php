3cb65fbb9d1d0fb430dec24f71189317581031be
Antoni Seba <antoniwoj@o2.pl>
2024-07-11 08:49:07 +0200

    Make recalc from csv work

--- app/Observers/Entities/OrderObserver.php
+++ app/Observers/Entities/OrderObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers\Entities;
 
+use App\Entities\BuyingInvoice;
 use App\Entities\Order;
 use App\Entities\Status;
 use App\Facades\Mailer;
@@ -96,6 +97,30 @@ readonly class OrderObserver
     public function updated(Order $order): void
     {
         OrdersRecalculatorBasedOnPeriod::recalculateOrdersBasedOnPeriod($order);
+
+        $sumOfPurchase = 0;
+
+        foreach ($order->items as $item) {
+            $pricePurchase = $item['net_purchase_price_commercial_unit_after_discounts'] ?? 0;
+            $quantity = $item['quantity'] ?? 0;
+            $sumOfPurchase += floatval($pricePurchase) * intval($quantity);
+        }
+
+        $totalItemsCost = $sumOfPurchase * 1.23;
+        $transportCost = $order->shipment_price_for_us;
+
+        $totalItemsCost += $transportCost;
+
+        $totalGross = BuyingInvoice::where('order_id', $order->id)->sum('value');
+        $arr = [];
+
+        if ($order->labels->contains('id', 65) && $totalGross == round($totalItemsCost, 2)) {
+            AddLabelService::addLabels($order, [264], $arr, []);
+            RemoveLabelService::removeLabels($order, [263], $arr , [], auth()->id());
+        } else {
+            AddLabelService::addLabels($order, [263], $arr, []);
+            RemoveLabelService::removeLabels($order, [264], $arr , [], auth()->id());
+        }
     }
 
     public function labelsAttached(Order $order): void
