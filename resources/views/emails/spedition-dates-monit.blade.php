f7345b076594b1afccf42c0f9fe56feed7c8ed47
Antoni Seba <antoniwoj@o2.pl>
2024-07-15 10:13:47 +0200

    Make recalc from csv work

--- resources/views/emails/spedition-dates-monit.blade.php
+++ resources/views/emails/spedition-dates-monit.blade.php
@@ -4,10 +4,10 @@ Dzień dobry,
 Prosimy o informacje na temat statusu wysyłki zamówienia o numerze: {{ $order->id }}
 <br>
 <br>
-Jeśli zamówienie zostanie wysłane jutro prosimy o wypełnienie formularza pod linkiem: <a href="{{ route('shippingToday', $order->id) }}">{{ route('shippingToday', $order->id) }}</a>
+Jeśli zamówienie zostanie wysłane jutro prosimy o wypełnienie formularza pod linkiem: <a href="https://admin.mega1000.pl/orders/set-order-as-not-shipping-today/{{$order->id}}">{{ route('shippingToday', $order->id) }}</a>
 <br>
 <br>
-Jeśli zamówienie nie zostanie wysłane jutro kliknij na ten link: <a href="https://www.admin.mega1000.pl/orders/set-order-as-not-shipping-today/{{$order->id}}">{{ route('notShippingToday', $order->id) }}</a>
+Jeśli zamówienie nie zostanie wysłane jutro kliknij na ten link: <a href="https://admin.mega1000.pl/orders/set-order-as-not-shipping-today/{{$order->id}}">{{ route('notShippingToday', $order->id) }}</a>
 <br>
 <br>
 Jeśli jeszcze nie masz dokładnych informacji na temat wysyłki tego zamówienia prosimy zachować tę wiadomość i wybrać odpowiednią opcję w momencie otrzymania tej informacji (do godziny 13).
