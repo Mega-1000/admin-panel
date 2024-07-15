3722d6349fe9095839150e601d29806af0b1b662
Antoni Seba <antoniwoj@o2.pl>
2024-07-15 14:13:49 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/offer-balance.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/offer-balance.blade.php
@@ -6,4 +6,3 @@
 <p>WAC: {{ round($WAC, 2) }}</p>
 <p>ZP: {{ round($ZP, 2) }}</p>
 <p>BZO: {{ round($BZO, 2) }}</p>
-<p><a href="/admin/allegro-billing?order-id={{ $order['id'] }}" class="btn btn-primary" target="_blank">pokaz na liście</a></p>
