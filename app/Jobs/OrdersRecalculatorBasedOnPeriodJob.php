603c03fbd08433477fc09df56f0c96d44a067725
Antoni Seba <antoniwoj@o2.pl>
2024-07-15 14:46:28 +0200

    Make recalc from csv work

--- app/Jobs/OrdersRecalculatorBasedOnPeriodJob.php
+++ app/Jobs/OrdersRecalculatorBasedOnPeriodJob.php
@@ -4,9 +4,11 @@ namespace App\Jobs;
 
 use App\Entities\Order;
 use App\Helpers\OrdersRecalculatorBasedOnPeriod;
+use App\Services\Label\AddLabelService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -35,6 +37,17 @@ class OrdersRecalculatorBasedOnPeriodJob implements ShouldQueue
 
         foreach ($orders as $order) {
             OrdersRecalculatorBasedOnPeriod::recalculateOrdersBasedOnPeriod($order);
+        }
+
+        $orders = Order::where('approved_at', '<=', now()->subDays(5))->whereHas('labels', function ($q) {
+            $q->where('labels.id', 206);
+        })->get();
+
+        foreach ($orders as $order) {
+            if (!$order->labels->contains([286, 220, 5, 4, 195, 221])) {
+                $arr = [];
+                AddLabelService::addLabels($order, [286], $arr, []);
+            }
         }
     }
 }
