b77c3298d434fdabc3a9b6d542cd322d0569d33b
Antoni Seba <antoniwoj@o2.pl>
2024-07-16 20:23:00 +0200

    Make recalc from csv work

--- app/Services/CalculateSubjectInvoiceBilansLabels.php
+++ app/Services/CalculateSubjectInvoiceBilansLabels.php
@@ -18,7 +18,7 @@ class CalculateSubjectInvoiceBilansLabels {
         $orderValue = $order->getValue() + Orders::getOrderReturnGoods($order) - Orders::getSumOfWTONPayments($order);
         $arr = [];
 
-        dd(round($orderInvoiceValuesSum, 2), round($orderValue, 2),  $order->getValue());
+        dd(round($orderInvoiceValuesSum, 2), round($orderValue, 2),  $order->getValue(), Orders::getOrderReturnGoods($order), Orders::getSumOfWTONPayments($order));
         if (round($orderInvoiceValuesSum, 2) != round($orderValue, 2) && !$order->labels->contains(124)) {
             AddLabelService::addLabels($order, [231],$arr, []);
 
