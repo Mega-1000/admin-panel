62aa88636c48e1870af9f8e2997d829fe43d2d0a
Antoni Seba <antoniwoj@o2.pl>
2024-07-17 09:55:11 +0200

    Make recalc from csv work

--- app/Jobs/WarehouseDispatchPendingReminderJob.php
+++ app/Jobs/WarehouseDispatchPendingReminderJob.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Mail\WarehouseDispatchPendingReminderMail;
 use App\Repositories\OrderWarehouseNotificationRepository;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Carbon;
@@ -25,6 +24,7 @@ class WarehouseDispatchPendingReminderJob extends Job implements ShouldQueue
 
             if (!empty($warehousesToRemind)) {
                 foreach ($warehousesToRemind as $warehouseNotification) {
+                    dd($warehouseNotification);
                     if ($this->shouldNotifyWithEmail($warehouseNotification, $now)) {
                         Log::notice('Wysyłka powiadomienie dla zamówienia: ' . $warehouseNotification->order_id , ['line' => __LINE__, 'file' => __FILE__]);
                         dispatch_now(new OrderStatusChangedToDispatchNotificationJob($warehouseNotification->order_id));
