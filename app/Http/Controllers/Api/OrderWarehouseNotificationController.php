09f784a83fb864af28878dff0cf54262215be40e
Antoni Seba <antoniwoj@o2.pl>
2024-07-17 14:49:57 +0200

    Make recalc from csv work

--- app/Http/Controllers/Api/OrderWarehouseNotificationController.php
+++ app/Http/Controllers/Api/OrderWarehouseNotificationController.php
@@ -113,7 +113,7 @@ class OrderWarehouseNotificationController extends Controller
             $data['realization_date'] = $data['realization_date_from'];
             $data['possible_delay_days'] = Carbon::parse($data['realization_date_from'])->diffInDays(Carbon::parse($data['realization_date_to']));
             $notification = OrderWarehouseNotification::find($notificationId);
-            return response()->json($notification);
+            return response()->json($data);
             $notification->update($data);
 
             if (!empty($data['customer_notices'])) {
