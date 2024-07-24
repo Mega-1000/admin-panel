1d20462243ad8d265d463d6286dfc49067f31eec
Antoni Seba <antoniwoj@o2.pl>
2024-07-24 08:41:42 +0200

    Make recalc from csv work

--- resources/views/do_action.blade.php
+++ resources/views/do_action.blade.php
@@ -1,2 +1,5 @@
 <iframe src="{{ route('orders.edit', $order->id) }}" width="100%" height="1000px" frameborder="0"></iframe>
 <iframe src="/auctions/{{ $order->chat->auctions()->first()->id }}/end" width="100%" height="1000px" frameborder="0"></iframe>
+
+// get-bascet
+<iframe src="/admin/orders/{{ $order->id }}/get-basket" width="100%" height="1000px" frameborder="0"></iframe>
