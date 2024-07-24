e4b975e0d642768bc860da13ecbb8804fb856492
Antoni Seba <antoniwoj@o2.pl>
2024-07-24 20:23:58 +0200

    Make recalc from csv work

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3284,6 +3284,7 @@ class OrdersController extends Controller
             $labelId = 95;
         }
 
+        dd($order->labels->where('id', 265)->first());
 
         if (request()->get('notAbleToProcess')) {
             $not_able_to_handle_users = json_decode($order->not_able_to_handle_users);
