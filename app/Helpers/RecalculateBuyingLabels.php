98113a6dafc27caa3a5ae61a549254082fe69f7d
Antoni Seba <antoniwoj@o2.pl>
2024-07-25 17:53:18 +0200

    Make recalc from csv work

--- app/Helpers/RecalculateBuyingLabels.php
+++ app/Helpers/RecalculateBuyingLabels.php
@@ -17,8 +17,7 @@ class RecalculateBuyingLabels
         OrderPayment::where('created_at' , '>=', now()->firstOfMonth())
             ->whereHas('order', function ($q) {$q->whereHas('items.product', function ($q) {$q->where('variation_group', 'styropiany');});})->where('operation_type', 'Wpłata/wypłata bankowa')->get()->unique('order')->pluck('order.id');
 
-        // first day of month
-        if (empty(BuyingInvoice::where('order_id', $order->id)->first()) && $order->labels->contains('id', 65)) {
+        if (empty(BuyingInvoice::where('order_id', $order->id)->first()) && !$order->labels->contains('id', 65)) {
             return;
         }
 
