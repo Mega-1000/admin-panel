170dba37a8d3558b049ac877600931ac252e74ec
Antoni Seba <antoniwoj@o2.pl>
2024-07-26 09:52:05 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -226,10 +226,6 @@
     ->
     Awizacje obsługuje {{ $notification && $notification->employee_id && $notification->employee->is_performing_avization ? 'Pracownik' : 'Magazyn' }}
     ->
-    @php
-        $notification = \App\Entities\OrderWarehouseNotification::where('order_id', $or->id)->where('contact_person', '!=', null)->first();
-    @endphp
-
     Podano osobę kontaktową: {{ $notification->contact_person ?? '' }}
     telefon: {{ $notification?->contact_person_phone ?? '' }}
 
