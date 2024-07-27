7d87f13ff3b7ce5cbacd6fbfd4aa8e1fc2884015
Antoni Seba <antoniwoj@o2.pl>
2024-07-27 09:00:53 +0200

    Make recalc from csv work

--- resources/views/firms/show-order.blade.php
+++ resources/views/firms/show-order.blade.php
@@ -100,7 +100,7 @@
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
     // Origin zip code
