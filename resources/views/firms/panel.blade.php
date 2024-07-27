effaf51271e94989e5acf33cdb15c0476720b489
Antoni Seba <antoniwoj@o2.pl>
2024-07-27 11:38:25 +0200

    Make recalc from csv work

--- resources/views/firms/panel.blade.php
+++ resources/views/firms/panel.blade.php
@@ -72,6 +72,8 @@
                                     <div class="bg-red-100 border-l-4 border-yellow-500 text-yellow-700 p-3 mb-4">
                                         <p class="font-bold">
                                             PRZELEW ZOSTAŁ WYKONANY - prosimy o potwierdzenie otrzymujesz powiadomienia co 15 minut. kliknij
+                                            <br>
+                                            <a href="{{ route('store-payment-confirmation-confirm', $order->id) }}">TUTAJ</a>
                                         </p>
                                     </div>
                                 @endif
@@ -80,6 +82,52 @@
                                     <div class="bg-red-100 border-l-4 border-yellow-500 text-yellow-700 p-3 mb-4">
                                         <p class="font-bold">
                                             DATY SPEDYCJI ZOSTAŁY ZAKOŃCZONE - Prosimy o informacje czy zlecenie zostało wydane aktualnie otrzymujesz powiadomienia co 15 minut
+                                            <br>
+                                            Jeśli zamówienie juź wyjechało, prosimy o potwierdzenie tego faktu klikając w przycisk poniżej:
+                                            <a href="{{ rtrim(config('app.front_nuxt_url'), '/') . "/magazyn/awizacja/{$order->orderWarehouseNotifications->first()->id}/{$order->warehouse_id}/{$order->id}/wyslij-fakture" }}">
+                                                <button style="background-color: #4CAF50; /* Green */
+                                            border: none;
+                                            color: white;
+                                            text-align: center;
+                                            text-decoration: none;
+                                            display: inline-block;
+                                            font-size: 16px;
+                                            margin: 4px 2px;
+                                            cursor: pointer;">Towar został wydany</button>
+                                            </a>
+
+                                            <br>
+                                            <br>
+                                            Potrzebujesz przełożyć daty zamówienia? Skontaktuj się z klientem po czym zaaktualizuj daty pod tym linkiem:
+
+                                            @php
+                                                $lowestDistance = PHP_INT_MAX;
+                                                $company = $order->warehouse->firm;
+                                                $closestEmployee = null;
+
+                                                foreach ($company->employees as $employee) {
+                                                $employee->distance = App\Helpers\LocationHelper::getDistanceOfClientToEmployee($employee, $order->customer);
+
+                                                if ($employee->distance < $lowestDistance) {
+                                                        $lowestDistance = $employee->distance;
+                                                        $closestEmployee = $employee;
+                                                    }
+                                                }
+
+                                                if (!$closestEmployee) {
+                                                    $closestEmployee = $company->employees->first();
+                                                }
+
+                                                App\Services\MessageService::createNewCustomerOrEmployee($order->chat, new Illuminate\Http\Request(['type' => 'Employee']), $closestEmployee);
+
+
+                                                $token = app(\App\Helpers\MessagesHelper::class)->getChatToken(
+                                                    $order->id,
+                                                    $closestEmployee->id,
+                                                    'e',
+                                                );
+                                            @endphp
+                                            <a href="https://admin.mega1000.pl/chat/{{ $token }}">Zmień daty dostawy</a>
                                         </p>
                                     </div>
                                 @endif
@@ -88,6 +136,11 @@
                                     <div class="bg-red-100 border-l-4 border-yellow-500 text-yellow-700 p-3 mb-4">
                                         <p class="font-bold">
                                             NIE OTRZYMALIŚMY INFORMACJI CZY ZAMÓWIENIE WYJEDZIE JUTRO - Prosimy o informacje czy zlecenie zostanie wydane jutro aktualnie otrzymujesz powiadomienia co 15 minut
+                                            <br>
+                                            Jeśli zamówienie zostanie wysłane jutro prosimy o wypełnienie formularza pod linkiem: <a href="https://admin.mega1000.pl/orders/set-order-as-shipping-today/{{$order->id}}">{{ route('shippingToday', $order->id) }}</a>
+                                            <br>
+                                            <br>
+                                            Jeśli zamówienie nie zostanie wysłane jutro kliknij na ten link: <a href="https://admin.mega1000.pl/orders/set-order-as-not-shipping-today/{{$order->id}}">{{ route('notShippingToday', $order->id) }}</a>
                                         </p>
                                     </div>
                                 @endif
