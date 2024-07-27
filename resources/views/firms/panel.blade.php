a6d546f8b460c946e165c280f901b5227917ddc5
Antoni Seba <antoniwoj@o2.pl>
2024-07-27 11:34:14 +0200

    Make recalc from csv work

--- resources/views/firms/panel.blade.php
+++ resources/views/firms/panel.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body class="bg-gray-100 min-h-screen">
 <nav class="bg-white shadow-md">
@@ -46,9 +47,11 @@
                                             <br>
                                             Otrzymujesz powiadomienia co 15 minut. kliknij
 
-                                            <a href="
-{{ rtrim(config('app.front_nuxt_url'), "/") . "/zamowienie/mozliwe-do-realizacji/brak-danych/{$order->id}" }}
-                                            " target="__blank">
+                                            <a
+                                                href="https://new.mega1000.pl/magazyn/awizacja/34655/1431333/86398"
+                                                target="__blank"
+                                                class="btn btn-primary"
+                                            >
                                                 tutaj
                                             </a>
 
