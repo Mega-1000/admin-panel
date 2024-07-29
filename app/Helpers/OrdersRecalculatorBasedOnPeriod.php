3aa120e2e2b977d0644e810cddc15084715578c0
Antoni Seba <antoniwoj@o2.pl>
2024-07-29 19:34:27 +0200

    Make recalc from csv work

--- app/Helpers/OrdersRecalculatorBasedOnPeriod.php
+++ app/Helpers/OrdersRecalculatorBasedOnPeriod.php
@@ -51,7 +51,6 @@ class OrdersRecalculatorBasedOnPeriod
         $sumOfGrossValues = $totalProductPrice + $additional_service + $additional_cod_cost + $shipment_price_client;
 
         $payments = OrderPayment::where('order_id', $order->id)->where('declared_sum', '!=', null)->where('status', null)->orWhere('status', 'Deklaracja wpłaty')->where('promise_date', '>', now())->get()->sum('amount');
-        dd($payments);
 
         if ($payments != 0) {
             AddLabelService::addLabels($order, [240], $arr, [], Auth::user()?->id);
