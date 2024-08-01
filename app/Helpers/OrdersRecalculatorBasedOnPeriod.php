ab5cc587efe06925168787b93bef60e41fe3d1b3
Antoni Seba <antoniwoj@o2.pl>
2024-08-01 07:13:03 +0300

    Make recalc from csv work

--- app/Helpers/OrdersRecalculatorBasedOnPeriod.php
+++ app/Helpers/OrdersRecalculatorBasedOnPeriod.php
@@ -57,7 +57,9 @@ class OrdersRecalculatorBasedOnPeriod
         } else {
             RemoveLabelService::removeLabels($order, [240], $arr, [], Auth::user()?->id);
         }
+
         if (OrderPayment::where('order_id', $order->id)->where('declared_sum', '!=', null)->whereIn('status', [null, 'Deklaracja wpłaty'])->where('promise_date', '>', now())->get()->sum('declared_sum') == 0) {
+            dd('koej');
             $order->labels()->detach(240);
         } else {
             if (!$order->labels->contains('id', 240)) {
