9a92a732ab3ff1145b7daeb1075bfb65be07f122
Antoni Seba <antoniwoj@o2.pl>
2024-08-01 08:34:42 +0300

    Make recalc from csv work

--- app/Helpers/OrdersRecalculatorBasedOnPeriod.php
+++ app/Helpers/OrdersRecalculatorBasedOnPeriod.php
@@ -14,6 +14,7 @@ class OrdersRecalculatorBasedOnPeriod
 {
     public static function recalculateOrdersBasedOnPeriod($order): void
     {
+        dd('okej');
         $order->labels()->detach(240);
         $order->labels()->detach(39);
     }
