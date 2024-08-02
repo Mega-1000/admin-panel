9fd81195bfde578452fe8f3dd3e47d08359de739
Antoni Seba <antoniwoj@o2.pl>
2024-08-02 21:49:25 +0300

    Make recalc from csv work

--- app/Helpers/OrderBilansCalculator.php
+++ app/Helpers/OrderBilansCalculator.php
@@ -63,7 +63,7 @@ class OrderBilansCalculator
         $totalOfDeclaredPayments = $order->payments()->where('operation_type', null)->sum('declared_sum');
 
 
-        return $order->getSumOfGrossValues() - $bilans + $returnedValue - $WPFZ - $kwonPayments + $totalOfDeclaredPayments;
+        return $order->getSumOfGrossValues() - $bilans - $returnedValue - $WPFZ - $kwonPayments + $totalOfDeclaredPayments;
     }
 
 }
