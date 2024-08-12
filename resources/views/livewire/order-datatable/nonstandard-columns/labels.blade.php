f24d8566cdc74cf758e0cde297c4c0557d53e437
Antoni Seba <antoniwoj@o2.pl>
2024-08-12 12:21:02 +0200

    Make recalc from csv work

--- resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
+++ resources/views/livewire/order-datatable/nonstandard-columns/labels.blade.php
@@ -140,8 +140,10 @@
 
     @foreach($or->dates()?->first()?->toArray() ?? [] as $k => $date)
         @if(array_key_exists($k, $dateTranslations))
-            {{ $dateTranslations[$k] ?? '' }}: {{ isset($date) ? \Carbon\Carbon::parse($date)->timezone('Europe/Warsaw')->format('m-dH:i') : 'Brak' }}
-            <br>
+            <div style="width: 100px">
+                {{ $dateTranslations[$k] ?? '' }}: {{ isset($date) ? \Carbon\Carbon::parse($date)->timezone('Europe/Warsaw')->format('m-d H:i') : 'Brak' }}
+                <br>
+            </div>
         @endif
     @endforeach
 
