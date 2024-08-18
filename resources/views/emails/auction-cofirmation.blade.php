bcbcb2bcc7fd3972245723eea17b9ed76e719373
Antoni Seba <antoniwoj@o2.pl>
2024-08-18 13:37:03 +0200

    Make recalc from csv work

--- resources/views/emails/auction-cofirmation.blade.php
+++ resources/views/emails/auction-cofirmation.blade.php
@@ -1,16 +1,30 @@
-Dzień dobry,
-<br>
-<br>
-Na twoim koncie został stworzony przetarg na zapytanie styropianowe.
-<br>
-<br>
-Zarządzać swoimi zapytaniami możesz na swoim koncie pod linkiem: <a href="https://mega1000.pl/account">https://mega1000.pl/account</a> po zalogowaniu na email (hasło to twój numer telefonu).
-<br>
-<br>
-Tabelkę cen proponowanych przez firmy znajdziesz pod linkiem: <a href="https://admin.mega1000.pl/auctions/{{ $auction->id }}/end">https://admin.mega1000.pl/auctions/{{ $auction->id }}/end</a> zmienia się ona na żywo.
-<br>
-<br>
-<br>
-Z pozdrowieniami
-<br>
-Zespół EPH POLSKA
+<div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
+    <p style="font-size: 18px; margin-bottom: 20px;">Dzień dobry,</p>
+
+    <p style="margin-bottom: 20px;">
+        Na twoim koncie został stworzony przetarg na zapytanie styropianowe.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Zarządzać swoimi zapytaniami możesz na swoim koncie pod linkiem:
+        <a href="https://mega1000.pl/account"
+           style="color: #0073e6; text-decoration: none;">
+            https://mega1000.pl/account
+        </a>
+        po zalogowaniu na email (hasło to twój numer telefonu).
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Tabelkę cen proponowanych przez firmy znajdziesz pod linkiem:
+        <a href="https://admin.mega1000.pl/auctions/{{ $auction->id }}/end"
+           style="color: #0073e6; text-decoration: none;">
+            https://admin.mega1000.pl/auctions/{{ $auction->id }}/end
+        </a>.
+        Zmienia się ona na żywo.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Z pozdrowieniami,<br>
+        Zespół EPH POLSKA
+    </p>
+</div>
