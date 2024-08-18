e11ba53b51cad097b8172dcd25a8c6a2c43f8238
Antoni Seba <antoniwoj@o2.pl>
2024-08-18 13:36:59 +0200

    Make recalc from csv work

--- resources/views/emails/new-styro-offer.blade.php
+++ resources/views/emails/new-styro-offer.blade.php
@@ -1,18 +1,31 @@
-Dzień dobry,
-<br>
-<br>
-Zauważyliśmy, że stworzyli państwo zapytanie ofertowe na styropian.
-<br>
-<br>
-Chcielibyśmy poinformować, że założyliśmy Państwu konto w naszym serwisie. Na które można się zalogować pod adresem:  <a
-    href="https://mega1000.pl/account?credentials={{$order->customer->login}}:{{$order->customer->phone}}"> https://mega1000.pl/account </a> po zalogowaniu się za pomocą swojego e-maila i numeru telefonu jako hasła.
-<br>
-<br>
-Możecie tam Państwo zarządzać swoimi zapytaniami i zamówieniami, przejść do chatu, dokonywać edycji, pobrać proformę, stworzyć przetarg i wykonać wiele innych akcji.
-<br>
-<br>
-W przypadku wszelkich pytań jesteśmy do Państwa dyspozycji pod numerem: 507 925 963 7 dni w tygodniu od 7 do 23.
-<br>
-<br>
-Z pozdrowieniami <br>
-Zespół EPH Polska
+<div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
+    <p style="font-size: 18px; margin-bottom: 20px;">Dzień dobry,</p>
+
+    <p style="margin-bottom: 20px;">
+        Zauważyliśmy, że stworzyli Państwo zapytanie ofertowe na styropian.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Chcielibyśmy poinformować, że założyliśmy Państwu konto w naszym serwisie, na które można się zalogować pod adresem:
+        <a href="https://mega1000.pl/account?credentials={{$order->customer->login}}:{{$order->customer->phone}}"
+           style="color: #0073e6; text-decoration: none;">
+            https://mega1000.pl/account
+        </a>
+        po zalogowaniu się za pomocą swojego e-maila i numeru telefonu jako hasła.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Możecie tam Państwo zarządzać swoimi zapytaniami i zamówieniami, przejść do chatu, dokonywać edycji, pobrać proformę,
+        stworzyć przetarg i wykonać wiele innych akcji.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        W przypadku wszelkich pytań jesteśmy do Państwa dyspozycji pod numerem:
+        <strong style="color: #0073e6;">507 925 963</strong>, 7 dni w tygodniu od 7 do 23.
+    </p>
+
+    <p style="margin-bottom: 20px;">
+        Z pozdrowieniami,<br>
+        Zespół EPH Polska
+    </p>
+</div>
