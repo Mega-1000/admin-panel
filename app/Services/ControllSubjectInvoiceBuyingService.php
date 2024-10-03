2e334004b753ab4486ea714cbaaa092bf37a07e8
Antoni Seba <antoniwoj@o2.pl>
2024-10-03 16:47:17 +0200

    looks

--- app/Services/ControllSubjectInvoiceBuyingService.php
+++ app/Services/ControllSubjectInvoiceBuyingService.php
@@ -20,7 +20,7 @@ class ControllSubjectInvoiceBuyingService
     public function handle(array $data): void
     {
         foreach ($data as $orderNotes) {
-            dd($orderNotes['notes'] == 86564);
+            dd($orderNotes->notes == 86564);
             $this->handleSingle($orderNotes);
         }
 
