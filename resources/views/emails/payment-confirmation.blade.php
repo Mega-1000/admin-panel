27945529b44b50e3185f820617b6eeded7a58e50
Antoni Seba <antoniwoj@o2.pl>
2025-01-30 18:11:08 +0100

    Update payment-confirmation.blade.php

--- resources/views/emails/payment-confirmation.blade.php
+++ resources/views/emails/payment-confirmation.blade.php
@@ -3,24 +3,6 @@ Dzień dobry,
 @php
 $order = \App\Entities\Order::find($confirmation->order_id);
 @endphp
-<p>
-    UWAGA !!<br>
-        Jeśli chcesz udostępnić ten moduł do innego działu twojej firmy to skopiuj link i wyślij do odpowiedniej osoby
-    <a
-        href="https://admin.mega1000.pl/firm-panel-actions/{{ \App\Entities\Firm::where('symbol', $order->items->first()->product->manufacturer)->first()->id }}"
-        style="background-color: #4CAF50; /* Green */
-        border: none;
-        color: white;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        margin: 4px 2px;
-        cursor: pointer;
-        border-radius: 12px;
-        padding: 10px 24px;"
-    >PANEL WSZYSTKICH ZAMÓWIEN DO WASZEJ FIRMY</a>
-</p>
 <br>
 Pod następującym linkiem dostępne jest potwierdzenie przelewu wykonanego przez nas: <a href="{{ $confirmation->file_url }}"> {{ $confirmation->file_url }} </a> do oferty o numerze: {{ $confirmation->order_id }}.
 <br>
