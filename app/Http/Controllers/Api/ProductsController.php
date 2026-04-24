f2dd0eea606a12895fb40915d00039fe8ac8cf46
Bartosz Woszczak <biuro@prosofts.pl>
2026-04-24 02:20:55 +0200

    Fix issue

--- app/Http/Controllers/Api/ProductsController.php
+++ app/Http/Controllers/Api/ProductsController.php
@@ -198,20 +198,14 @@ class ProductsController extends Controller
 
     public function getHiddenProducts(GetHiddenProductsRequest $request): JsonResponse
     {
-        $parent = Product::with(['children.price', 'children.packing'])
-            ->where('id', (int)$request->product)->first();
-//        $parent = Product::with(['children' => function ($q) {
-//            $q->join('product_prices', 'products.id', '=', 'product_prices.product_id')
-//                ->join('product_packings', 'products.id', '=', 'product_packings.product_id')
-//                ->select([
-//                    'products.*',
-//                    'product_prices.*',
-//                    'product_packings.*',
-//                ])
-//                ->orderBy('products.priority')
-//                ->orderBy('products.name')
-//                ->distinct();
-//        }])->find((int)$request->product);
+        $parent = Product::with(['children' => function ($q) {
+                $q->select('product_prices.*', 'product_packings.*', 'products.*');
+                $q->join('product_prices', 'product.id', '=', 'product_prices.product_id');
+                $q->join('product_packings', 'product.id', '=', 'product_packings.product_id');
+                $q->orderBy('priority');
+                $q->orderBy('name');
+            }])
+            ->find((int)$request->product);
 
         return response()->json($parent?->children ?? []);
     }
